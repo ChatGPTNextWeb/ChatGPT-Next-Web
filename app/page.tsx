76f851bfa6ab3d5f9c310eaa78ae0e82ef938b2b
Yidadaa <yidadaa@qq.com>
2023-03-14 00:27:45 +0800

    feat: add analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { Home } from "./components/home";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <Analytics />
+    </>
+  );
 }
