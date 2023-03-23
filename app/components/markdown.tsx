22652a8a13fc1a55e3b4249ec2715306f33e65a7
Yifei Zhang <yidadaa@qq.com>
2023-03-23 11:28:07 +0000

    fix: #5 crash if code block cannot be highlighted

--- app/components/markdown.tsx
+++ app/components/markdown.tsx
@@ -2,13 +2,19 @@ import ReactMarkdown from "react-markdown";
 import "katex/dist/katex.min.css";
 import RemarkMath from "remark-math";
 import RehypeKatex from "rehype-katex";
-import RemarkGfm from 'remark-gfm'
-import RehypePrsim from 'rehype-prism-plus'
+import RemarkGfm from "remark-gfm";
+import RehypePrsim from "rehype-prism-plus";
 
 export function Markdown(props: { content: string }) {
-    return (
-        <ReactMarkdown remarkPlugins={[RemarkMath, RemarkGfm]} rehypePlugins={[RehypeKatex, RehypePrsim]}>
-            {props.content}
-        </ReactMarkdown>
-    );
+  return (
+    <ReactMarkdown
+      remarkPlugins={[RemarkMath, RemarkGfm]}
+      rehypePlugins={[
+        RehypeKatex,
+        [RehypePrsim, { ignoreMissing: true }],
+      ]}
+    >
+      {props.content}
+    </ReactMarkdown>
+  );
 }
