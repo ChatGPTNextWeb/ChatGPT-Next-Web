df66eef919a3eda0569c94b7ab79523aa3957968
Yifei Zhang <yidadaa@qq.com>
2023-03-26 11:58:25 +0000

    feat: support using user api key

--- app/store/access.ts
+++ app/store/access.ts
@@ -4,7 +4,9 @@ import { queryMeta } from "../utils";
 
 export interface AccessControlStore {
   accessCode: string;
+  token: string;
 
+  updateToken: (_: string) => void;
   updateCode: (_: string) => void;
   enabledAccessControl: () => boolean;
 }
@@ -14,12 +16,16 @@ export const ACCESS_KEY = "access-control";
 export const useAccessStore = create<AccessControlStore>()(
   persist(
     (set, get) => ({
+      token: "",
       accessCode: "",
       enabledAccessControl() {
         return queryMeta("access") === "enabled";
       },
       updateCode(code: string) {
         set((state) => ({ accessCode: code }));
+      },
+      updateToken(token: string) {
+        set((state) => ({ token }));
       },
     }),
     {
