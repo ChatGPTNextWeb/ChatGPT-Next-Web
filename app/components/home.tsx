5f2a49394ce4c8156fccbc60e0dcffbf8bc78399
houpe <houpe@qq.com>
2023-03-26 22:00:45 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -536,11 +536,7 @@ export function Home() {
                 }}
               />
             </div>
-            <div className={styles["sidebar-action"]}>
-              <a href={REPO_URL} target="_blank">
-                <IconButton icon={<GithubIcon />} />
-              </a>
-            </div>
+            
           </div>
           <div>
             <IconButton
