600377854459a55cf592655ef356e52913de7c9c
woogle666 <129281830+woogle666@users.noreply.github.com>
2023-03-30 00:42:11 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,10 @@ try {
 }
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "AI Chat",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "AI Chat",
     statusBarStyle: "black-translucent",
   },
   themeColor: "#fafafa"
