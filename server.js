e3a7137bf8de7778b76815b71ba1e260ea6c5200
Zhe Li <lizhe@lizhe.org>
2023-03-31 03:03:03 +0800

    add azure app service support

---
+++ server.js
@@ -0,0 +1,16 @@
+const { createServer } = require("http");
+const next = require("next");
+
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
