00aaba2429b3735134fc715120fb5bce6606d3b3
lushevol <lushevol@outlook.com>
2023-03-30 17:02:19 +0800

    fix: fix SSR no window error.

--- app/components/voice.tsx
+++ app/components/voice.tsx
@@ -1,24 +1,30 @@
 // implement a voice component by speech synthesis to read the text with the voice of the user's choice
 import * as React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IconButton } from "./button";
 import VoiceIcon from "../icons/voice.svg";
 
-const voices = window.speechSynthesis.getVoices();
 const speak = ({
   text,
   voice,
 }: {
   text: string;
-  voice?: SpeechSynthesisVoice;
+  voice: SpeechSynthesisVoice;
 }) => {
-  if (!text) return;
+  if (!window || !text) return;
   const utterance = new SpeechSynthesisUtterance(text);
-  utterance.voice = voice || voices[0];
+  utterance.voice = voice;
   window.speechSynthesis.speak(utterance);
 };
 export function Voice(props: { text: string }) {
+  const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([]);
   const [voice, setVoice] = useState<SpeechSynthesisVoice | null>(null);
+
+  useEffect(() => {
+    const voices = window.speechSynthesis.getVoices();
+    setVoices(voices);
+    setVoice(voices[0]);
+  }, []);
 
   return props.text ? (
     <div>
