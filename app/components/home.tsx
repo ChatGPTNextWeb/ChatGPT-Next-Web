2cc23a44975f944e73cbc894b051bb93955e3c94
latorc <65785354+latorc@users.noreply.github.com>
2023-04-03 11:13:27 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -111,9 +111,9 @@ export function Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>ChatGPT Next Clandoom</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            May TKs wisdom be with you.
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
