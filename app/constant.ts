6bdbc9543e84007389ec4ef0ae312a722925ff87
yorkeking <34937020+yorkeking@users.noreply.github.com>
2023-04-03 23:30:36 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
 export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
+export const REPO = "LeiChat-人工智能";//"ChatGPT-Next-Web";
 export const REPO_URL = `http://leichat.top/#/`;  //`https://github.com/${OWNER}/${REPO}`
 export const UPDATE_URL = `${REPO_URL}#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
