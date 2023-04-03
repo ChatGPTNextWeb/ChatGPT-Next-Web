dbe112b1c347e20d508341ae0aeaa39b9c2b4136
mykunfree <90499251+mykunfree@users.noreply.github.com>
2023-04-03 17:48:24 +0800

    Update tw.ts

--- app/locales/tw.ts
+++ app/locales/tw.ts
@@ -10,7 +10,7 @@ const tw: LocaleType = {
     ChatItemCount: (count: number) => `${count} 條對話`,
   },
   Chat: {
-    SubTitle: (count: number) => `您已經與 ChatGPT 進行了 ${count} 條對話`,
+    SubTitle: (count: number) => `進行了 ${count} 條對話`,
     Actions: {
       ChatList: "查看消息列表",
       CompressedHistory: "查看壓縮後的歷史 Prompt",
