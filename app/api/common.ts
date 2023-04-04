8a1b2aee27f7406a51c4c42999e67a6fce806a58
yangmao <yangmao@126.com>
2023-04-04 14:40:27 +0800

    改成自己的域名

--- app/api/common.ts
+++ app/api/common.ts
@@ -1,6 +1,6 @@
 import { NextRequest } from "next/server";
 
-const OPENAI_URL = "api.openai.com";
+const OPENAI_URL = "c.6530.cn";
 const DEFAULT_PROTOCOL = "https";
 const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
 const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
