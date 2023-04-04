1e539117498a18ffe23e87e27448a0b3a46be4f9
Tangser tang <tangser@qq.com>
2023-04-04 04:58:56 +0000

    ..

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -3,7 +3,8 @@ import { SubmitKey } from "../store/app";
 const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: "现在是未授权状态，请在设置页填写授权码。",
+    Unauthorized:
+      "现在是未授权状态，请在设置页填写访问密码。密码提示:公司门牌号",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
