061b93606f8442e934b071f1459cbe37f52b01af
Dashan_IZ <78125220+Dashan-IZ@users.noreply.github.com>
2023-04-06 20:02:59 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -118,7 +118,7 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>ChatGPT for Dashan</div>
           <div className={styles["sidebar-sub-title"]}>
             Build your own AI assistant.
           </div>
