42b0d837d9e0edfd3accac8de5a6ce4a25206097
Fuck U <47294782+Pessimisticc@users.noreply.github.com>
2023-04-06 12:22:04 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -121,7 +121,7 @@ function _Home() {
         <div className={styles["sidebar-header"]}>
           <div className={styles["sidebar-title"]}>ChatGPT Next</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+             Build your own AI assistant. <a href="http://www.afunc.cn/">我的博客</a>
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
