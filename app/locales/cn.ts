4899d8ca0ea9e0c121e7b7a853a06f9a523cab2c
mracat <38930499+mracat@users.noreply.github.com>
2023-04-10 13:33:12 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -3,7 +3,7 @@ import { SubmitKey } from "../store/app";
 const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: "现在是未授权状态，请点击左下角设置按钮输入访问密码。",
+    Unauthorized: "现在是未授权状态，请点击左下角设置按钮输入访问密码。如需购买普通 KEY或者PLUS KEY请联系留言。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
