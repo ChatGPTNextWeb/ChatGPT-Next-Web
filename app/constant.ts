f556d2b7cca84752a95d8cc251cfc31f51058d0b
hutao01-miku <118782661+hutao01-miku@users.noreply.github.com>
2023-04-10 22:19:42 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,6 +1,6 @@
 export const OWNER = "Yidadaa";
 export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
+export const REPO_URL = `https://gi0.cn/LGf`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
