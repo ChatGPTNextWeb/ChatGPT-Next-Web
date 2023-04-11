b4c3263390bd1b10999006fad1732c50273f0fa7
user0650 <7190674+user0650@users.noreply.github.com>
2023-04-11 16:35:55 +0000

    remove github button

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -206,11 +206,11 @@ function _Home() {
                 shadow
               />
             </div>
-            <div className={styles["sidebar-action"]}>
+            {/* <div className={styles["sidebar-action"]}>
               <a href={REPO_URL} target="_blank">
                 <IconButton icon={<GithubIcon />} shadow />
               </a>
-            </div>
+            </div> */}
           </div>
           <div>
             <IconButton
