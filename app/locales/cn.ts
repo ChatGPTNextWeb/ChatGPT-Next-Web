591f0a1c45cd1b3d3f532ff7462e441a8ddd01d3
AlexYuf <113398791+AlexYuf@users.noreply.github.com>
2023-04-12 20:13:00 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -3,7 +3,7 @@ import { SubmitKey } from "../store/app";
 const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: "现在是未授权状态，请私信【飞哥的书】获取授权码吧。",
+    Unauthorized: "现在是未授权状态，请私信小红书【飞哥的书】免费获取授权码吧。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
