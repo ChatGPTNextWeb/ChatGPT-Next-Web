5742c0db346c8dbc8e2f5cb7fa82febabbf90976
AlexYuf <113398791+AlexYuf@users.noreply.github.com>
2023-04-12 20:37:25 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,6 +1,6 @@
 export const OWNER = "Yidadaa";
 export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `www.123chat.top`;
+export const REPO_URL = `https://www.123chat.top`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
