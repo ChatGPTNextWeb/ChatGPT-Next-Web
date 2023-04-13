fcc29526ba88443f02f0d71da3a1805dfe065e21
Yifei Zhang <yidadaa@qq.com>
2023-04-13 11:05:36 +0800

    Update README_CN.md

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -204,11 +204,7 @@ function _Home() {
                 shadow
               />
             </div>
-            <div className={styles["sidebar-action"]}>
-              <a href={REPO_URL} target="_blank">
-                <IconButton icon={<GithubIcon />} shadow />
-              </a>
-            </div>
+
           </div>
           <div>
             <IconButton
