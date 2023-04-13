6ef4f90fbfde9b4ffd8ccaf9aba196a31ab51847
kongque001 <121564313+kongque001@users.noreply.github.com>
2023-04-13 20:16:30 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -3,7 +3,7 @@ import { SubmitKey } from "../store/app";
 const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: "现在是未授权状态，请点击左下角设置按钮输入访问密码。",
+    Unauthorized: "现在是未授权状态，请点联系xxx获取密码。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
