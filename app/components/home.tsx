635b5b91b8ab84481a054f6fafe1e596b56db0b0
ngzhenghui94 <ngzhenghui94@hotmail.com>
2023-04-14 09:10:02 +0000

    to repo

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -167,9 +167,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>OpenAI Model</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            Your personal AI assistant.
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
