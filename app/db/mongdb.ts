d0abf899b5baad6c08d6bf17720efa1304f9f5ef
xuzhenjun <xuzhenjun@910app.com>
2023-04-14 14:50:40 +0800

    test

--- app/db/mongdb.ts
+++ app/db/mongdb.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI as string; // your mongodb connection string
+const uri = process.env.MONGODB_URI as string; //your mongodb connection string
 const options = {};
 
 declare global {
