43ab8ca0dc1fdbfc0c4a33da38db5ca02e0d15d1
crypticatul <work.atulsingh@gmail.com>
2023-04-16 03:37:30 +0530

    First
    Logo + Text

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -169,9 +169,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>Chat Cognitiev</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            Your Own Ultimate AI Friend .
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
