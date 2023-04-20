5b3f3ad62b254f2e35c8d0ad8805aab50832983b
www9song <www9song@163.com>
2023-04-20 20:35:31 +0800

    Update app.ts

--- app/store/app.ts
+++ app/store/app.ts
@@ -147,7 +147,7 @@ const DEFAULT_CONFIG: ChatConfig = {
   disablePromptHint: false,
 
   modelConfig: {
-    model: "gpt-3.5-turbo",
+    model: "gpt-4",
     temperature: 1,
     max_tokens: 2000,
     presence_penalty: 0,
