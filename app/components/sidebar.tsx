5bd938188ddebda3d00d34c884932d2d231ddf4b
chen <1154760273@qq.com>
2023-04-21 20:43:38 +0800

    缩短zhaoshengmu

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -88,8 +88,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGPT Next</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
-                      ------power by zhaoshengmu
+          Build your own AI assistant. ---zhaoshengmu
         </div>
         <div className={styles["sidebar-logo"]}>
           <ChatGptIcon />
