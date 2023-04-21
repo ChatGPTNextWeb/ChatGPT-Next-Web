3bef1292f3d6b9cd49d548892f7174eafdf7f4c2
chen <1154760273@qq.com>
2023-04-21 20:32:49 +0800

    修改为Enter提交

--- app/store/app.ts
+++ app/store/app.ts
@@ -139,7 +139,7 @@ const DEFAULT_CONFIG: ChatConfig = {
   historyMessageCount: 4,
   compressMessageLengthThreshold: 1000,
   sendBotMessages: true as boolean,
-  submitKey: SubmitKey.CtrlEnter as SubmitKey,
+  submitKey: SubmitKey.Enter as SubmitKey,
   avatar: "1f603",
   fontSize: 14,
   theme: Theme.Auto as Theme,
