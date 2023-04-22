e2f5ad86cb50e74a6da624f7435feb7e9e1bc2b9
xuyang.li <68420148+LiXuYangISZ@users.noreply.github.com>
2023-04-22 21:31:01 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -3,7 +3,7 @@ import { SubmitKey } from "../store/config";
 const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: "现在是未授权状态，请点击左下角设置按钮输入访问密码。",
+    Unauthorized: "现在是未授权状态，请点击左下角设置按钮输入访问密码1111111111111111111111111。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
