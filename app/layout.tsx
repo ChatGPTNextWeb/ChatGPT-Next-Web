64a5364344818d20c00991588bbbdc1d68053681
ErShui <131098780+EeShui@users.noreply.github.com>
2023-04-23 07:10:39 +0000

    Pending changes exported from your codespace

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "小珂珂GPT",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "小珂珂GPT",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
