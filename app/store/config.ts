73eac1072fa177c4c8e33601d70bdb3952ba8042
zouzhongy <853794216@qq.com>
2023-04-24 19:58:08 +0800

    添加text-davinci-003

--- app/store/config.ts
+++ app/store/config.ts
@@ -73,6 +73,10 @@ export const ALL_MODELS = [
     name: "gpt-3.5-turbo-0301",
     available: true,
   },
+  {
+    name: "text-davinci-003",
+    available: true,
+  },
 ] as const;
 
 export type ModelType = (typeof ALL_MODELS)[number]["name"];
