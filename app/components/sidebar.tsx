4a64deec5f9c636ea68490d38aab819cc12476ff
tianjunjie <jack.tian@71360.com>
2023-04-24 10:40:19 +0800

    11

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -90,7 +90,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT</div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
         </div>
