6efad41fe2453cebcefd4202c17a8debf0ba43e0
宇 路 <1270308266@qq.com>
2023-04-25 12:09:39 +0800

    联系方式

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -92,7 +92,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGPT Next</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+        联系方式+Q:1270308266
         </div>
         <div className={styles["sidebar-logo"]}>
           <ChatGptIcon />
