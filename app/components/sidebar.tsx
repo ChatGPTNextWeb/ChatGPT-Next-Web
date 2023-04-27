cdf0408fcbbdc14d455b07dec5a857c3acf64daf
chenshuiguqq <42374492+chenshuiguqq@users.noreply.github.com>
2023-04-27 22:57:30 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -90,7 +90,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>Gu's ChatGPT</div>
+        <div className={styles["sidebar-title"]}>Gu&apos;s ChatGPT</div>
         <div className={styles["sidebar-sub-title"]}>
           Just try your any Question!
         </div>
