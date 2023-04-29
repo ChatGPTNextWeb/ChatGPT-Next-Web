0f606da9c40050941cdd8f855cb84c669e961be5
雲霧 <123147018+yunwuu@users.noreply.github.com>
2023-04-29 17:37:48 +0800

    revert

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,20 +42,6 @@ export default function RootLayout({
           rel="stylesheet"
         ></link>
         <script src="/serviceWorkerRegister.js" defer></script>
-        <script>
-          var _paq = window._paq = window._paq || [];
-          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-          _paq.push(['trackPageView']);
-          _paq.push(['enableLinkTracking']);
-          (function() {
-            var u="//analytics.yunwuu.com/";
-            _paq.push(['setTrackerUrl', u+'matomo.php']);
-            _paq.push(['setSiteId', '1']);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-          })();
-        </script>
-
       </head>
       <body>{children}</body>
     </html>
