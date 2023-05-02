e06425e5aba820e87a5971f3bcaf3d482e8c154a
bathfire <>
2023-05-02 21:50:16 +0800

    测试2下！

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -96,7 +96,7 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>柳叶</div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "个人",
+  description: "个人助理.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "理解万岁！",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
