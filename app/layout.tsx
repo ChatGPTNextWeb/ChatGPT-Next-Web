c877a579048ec0c5f5b230a4330d0c3f0c970c08
vtea <959996@gmail.com>
2023-05-04 11:25:49 +0800

    Enable Web Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,23 +47,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-import { Analytics } from '@vercel/analytics/react';
- 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
-}
