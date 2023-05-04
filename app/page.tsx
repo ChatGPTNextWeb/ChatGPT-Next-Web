6512b28463e0f13410240fc1b51dc3b4094b6307
vtea <959996@gmail.com>
2023-05-04 11:28:07 +0800

    Enable Web Analytics

--- app/page.tsx
+++ app/page.tsx
@@ -14,3 +14,21 @@ export default async function App() {
     </>
   );
 }
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
