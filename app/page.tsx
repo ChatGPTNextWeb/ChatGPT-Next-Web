bdaaa76abbc5683bbba51fb2e1f108faa50d676e
vtea <959996@gmail.com>
2023-05-04 11:31:38 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
-}
 
 import { Home } from "./components/home";
 
