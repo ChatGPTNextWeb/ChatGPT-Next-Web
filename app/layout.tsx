64dc7d679d4cf50b1a15da9411ee2deacfac5561
vtea <959996@gmail.com>
2023-05-07 06:38:14 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,8 +43,16 @@ export default function RootLayout({
         ></link>
         <script src="/serviceWorkerRegister.js" defer></script>
         
+        
+      <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','G-MFC66KNHXH');`}} />
+    </Head>
+    <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-MFC66KNHXH" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} 
 
-      </head>
+      
       <body>{children}</body>
     </html>
   );
