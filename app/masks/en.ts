a2027616b6d336796f6ccb9a91cef5d0897a611b
ZhaoLiu <liuzhao1225@gmail.com>
2023-05-08 10:18:51 +0800

    fix error

--- app/masks/en.ts
+++ app/masks/en.ts
@@ -30,7 +30,7 @@ export const EN_MASKS: BuiltinMask[] = [
       },
     ],
     modelConfig: {
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       temperature: 1,
       max_tokens: 2000,
       presence_penalty: 0,
