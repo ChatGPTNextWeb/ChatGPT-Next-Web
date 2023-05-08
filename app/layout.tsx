ab2358fa42630c38cc7b1bbd5332e4125f5afd64
Lapis <70219915+shizhiyun@users.noreply.github.com>
2023-05-08 16:55:51 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "闻声合音",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "闻声合音",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
