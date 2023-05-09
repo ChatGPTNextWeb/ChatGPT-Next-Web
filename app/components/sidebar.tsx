0c4be68b2ed9c169cc87e1d22e5923ac3d5359a3
Duang Cheng <longcheng@microsoft.com>
2023-05-09 15:13:27 +0800

    Revert "feat: change the text of the private version."
    This reverts commit cc9fd7cb64d0d1cd7d045708353c3c06b53d9bed.

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -98,7 +98,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGPT Next</div>
         <div className={styles["sidebar-sub-title"]}>
-          Your own OpenAI / AzureAI assistant.
+          Build your own AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
