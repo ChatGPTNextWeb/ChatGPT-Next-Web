bf6190cd838b16cbdfe6b24cc5ad4f36249d96d3
vtea <959996@gmail.com>
2023-05-10 03:32:50 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,7 +117,7 @@ export function SideBar(props: { className?: string }) {
           icon={<PluginIcon />}
           text={shouldNarrow ? undefined : Locale.Plugin.Name}
           className={styles["sidebar-bar-button"]}
-          onClick={() => window.open("https://prm.aiprm.top", "_blank")}
+          onClick={() => window.open("https://aiprm.cn2.ltd", "_blank")}
           shadow
         />
       </div>
