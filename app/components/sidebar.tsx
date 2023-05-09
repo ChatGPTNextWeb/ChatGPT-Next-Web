cc9fd7cb64d0d1cd7d045708353c3c06b53d9bed
Duang Cheng <longcheng@microsoft.com>
2023-05-09 13:06:26 +0800

    feat: change the text of the private version.

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -98,7 +98,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGPT Next</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Your own OpenAI / AzureAI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
