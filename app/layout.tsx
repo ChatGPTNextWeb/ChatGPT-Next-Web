cb2d62ae4f50f11a4645930a7a8987207238ba02
Hima Alaya <derek_sun_ok@163.com>
2023-05-10 16:36:47 +0800

    feat 名称修改

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "易聊 Eazy-Chat",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "易聊 Eazy-Chat",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
