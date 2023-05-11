4ac0aadd9077df82e8356e768421046f97316882
599153574@qq.com <ganxing@ganxingdeMacBook-Air.local>
2023-05-12 07:51:04 +0800

    add french translation -增加法语翻译

--- app/locales/de.ts
+++ app/locales/de.ts
@@ -76,6 +76,7 @@ const de: LocaleType = {
         cn: "简体中文",
         en: "English",
         tw: "繁體中文",
+        fr: "Français",
         es: "Español",
         it: "Italiano",
         tr: "Türkçe",
