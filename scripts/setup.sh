3a45f97da1153d12b15fbf1920259640688a1230
liuluyanglly <blingblinglly@163.com>
2023-05-12 10:37:09 +0800

    12

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -26,32 +26,32 @@ case "$(uname -s)" in
 esac
 
 # Check if needed dependencies are installed and install if necessary
-if ! command -v node >/dev/null || ! command -v git >/dev/null || ! command -v yarn >/dev/null; then
-  case "$(uname -s)" in
-    Linux)
-      if [[ "$(cat /etc/*-release | grep '^ID=')" = "ID=ubuntu" ]]; then
-        sudo apt-get update
-        sudo apt-get -y install nodejs git yarn
-      elif [[ "$(cat /etc/*-release | grep '^ID=')" = "ID=centos" ]]; then
-        sudo yum -y install epel-release
-        sudo yum -y install nodejs git yarn
-      elif [[ "$(cat /etc/*-release | grep '^ID=')" = "ID=amzn" ]]; then
-        sudo yum -y install epel-release
-        sudo yum -y install nodejs git yarn
-      elif [[ "$(cat /etc/*-release | grep '^ID=')" = "ID=arch" ]]; then
-        sudo pacman -Syu -y
-        sudo pacman -S -y nodejs git yarn
-      else
-        echo "Unsupported Linux distribution"
-        exit 1
-      fi
-      ;;
-    Darwin)
-      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-      brew install node git yarn
-      ;;
-  esac
-fi
+#if ! command -v node >/dev/null || ! command -v git >/dev/null || ! command -v yarn >/dev/null; then
+#  case "$(uname -s)" in
+#    Linux)
+#      if [[ "$(cat /etc/*-release | grep '^ID=')" = "ID=ubuntu" ]]; then
+#        sudo apt-get update
+#        sudo apt-get -y install nodejs git yarn
+#      elif [[ "$(cat /etc/*-release | grep '^ID=')" = "ID=centos" ]]; then
+#        sudo yum -y install epel-release
+#        sudo yum -y install nodejs git yarn
+#      elif [[ "$(cat /etc/*-release | grep '^ID=')" = "ID=\"amzn\"" ]]; then
+#        sudo yum -y install epel-release
+#        sudo yum -y install nodejs git yarn
+#      elif [[ "$(cat /etc/*-release | grep '^ID=')" = "ID=arch" ]]; then
+#        sudo pacman -Syu -y
+#        sudo pacman -S -y nodejs git yarn
+#      else
+#        echo "Unsupported Linux distribution"
+#        exit 1
+#      fi
+#      ;;
+#    Darwin)
+#      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#      brew install node git yarn
+#      ;;
+#  esac
+#fi
 
 # Clone the repository and install dependencies
 git clone https://github.com/liuluyanglly/ChatGPT-Next-Web
