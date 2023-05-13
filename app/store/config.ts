9e858a661dc3bd72f3542eac49152b0cf6cc443d
liuluyang <blingblinglly@163.com>
2023-05-13 15:46:14 +0800

    AI

--- app/store/config.ts
+++ app/store/config.ts
@@ -56,26 +56,26 @@ export const ALL_MODELS = [
     name: "gpt-4",
     available: ENABLE_GPT4,
   },
-  {
-    name: "gpt-4-0314",
-    available: ENABLE_GPT4,
-  },
+  // {
+  //   name: "gpt-4-0314",
+  //   available: ENABLE_GPT4,
+  // },
   {
     name: "gpt-4-32k",
     available: ENABLE_GPT4,
   },
-  {
-    name: "gpt-4-32k-0314",
-    available: ENABLE_GPT4,
-  },
+  // {
+  //   name: "gpt-4-32k-0314",
+  //   available: ENABLE_GPT4,
+  // },
   {
     name: "gpt-3.5-turbo",
     available: true,
   },
-  {
-    name: "gpt-3.5-turbo-0301",
-    available: true,
-  },
+  // {
+  //   name: "gpt-3.5-turbo-0301",
+  //   available: true,
+  // },
   {
     name: "qwen-v1", // 通义千问
     available: false,
