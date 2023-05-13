206fbdf6c4075900df6b6bec8435a953922b470b
qiangweizhang <57748866+qiangweizhang@users.noreply.github.com>
2023-05-13 19:01:18 +0800

    增加广告

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,10 +119,13 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>元话数藏 ChatGPT</div>
+        <div className={styles["sidebar-title"]}>元话数藏</div>
         <div className={styles["sidebar-sub-title"]}>
-          http://metakina.com. 一天上线，低至一万。
-          http://defiweb.cn. 全公链生态解决方案。
+          元话数藏SaaS（https://metakina.com）
+          价格低至三万;一天上线; 免上链费用; 免gas费，免支付接入费; 带二级市场，自定义UI: 阿里云waf防黑机制: 阿里云k8s分布
+          式部署支持高并发;可对接到元数网盯链: 人工智能生成NFT (藏品) :ChatGPT编写藏品故事: 10多种营销套路玩法;版权认证接口;
+          区块链技术服务（ https://defiweb.cn）
+          链游、元宇宙、DeFi、NFT、Swap、钱包、公链、DAPP、挖矿系统、小程序系统等各类定制开发，强大团队技术保障，高效有质量!
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
