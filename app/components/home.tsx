1a7a09bdbe1075e055e20a5433307d080783089c
qiangweizhang <57748866+qiangweizhang@users.noreply.github.com>
2023-05-13 18:51:23 +0800

    code back

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -4,31 +4,30 @@ require("../polyfill");
 
 import { useState, useEffect } from "react";
 
-import { IconButton } from "./button";
 import styles from "./home.module.scss";
 
-import SettingsIcon from "../icons/settings.svg";
-import GithubIcon from "../icons/github.svg";
-import ChatGptIcon from "../icons/chatgpt.svg";
-
 import BotIcon from "../icons/bot.svg";
-import AddIcon from "../icons/add.svg";
 import LoadingIcon from "../icons/three-dots.svg";
-import CloseIcon from "../icons/close.svg";
 
-import { useChatStore } from "../store";
-import { isMobileScreen } from "../utils";
-import Locale from "../locales";
-import { ChatList } from "./chat-list";
-import { Chat } from "./chat";
+import { getCSSVar, useMobileScreen } from "../utils";
 
 import dynamic from "next/dynamic";
-import { REPO_URL } from "../constant";
+import { Path, SlotID } from "../constant";
 import { ErrorBoundary } from "./error";
+
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { SideBar } from "./sidebar";
+import { useAppConfig } from "../store/config";
+import { useMaskStore } from "../store/mask";
 
 export function Loading(props: { noLogo?: boolean }) {
   return (
-    <div className={styles["loading-content"]}>
+    <div className={styles["loading-content"] + " no-dark"}>
       {!props.noLogo && <BotIcon />}
       <LoadingIcon />
     </div>
@@ -39,8 +38,20 @@ const Settings = dynamic(async () => (await import("./settings")).Settings, {
   loading: () => <Loading noLogo />,
 });
 
-function useSwitchTheme() {
-  const config = useChatStore((state) => state.config);
+const Chat = dynamic(async () => (await import("./chat")).Chat, {
+  loading: () => <Loading noLogo />,
+});
+
+const NewChat = dynamic(async () => (await import("./new-chat")).NewChat, {
+  loading: () => <Loading noLogo />,
+});
+
+const MaskPage = dynamic(async () => (await import("./mask")).MaskPage, {
+  loading: () => <Loading noLogo />,
+});
+
+export function useSwitchTheme() {
+  const config = useAppConfig();
 
   useEffect(() => {
     document.body.classList.remove("light");
@@ -63,9 +74,7 @@ function useSwitchTheme() {
       metaDescriptionDark?.setAttribute("content", "#151515");
       metaDescriptionLight?.setAttribute("content", "#fafafa");
     } else {
-      const themeColor = getComputedStyle(document.body)
-        .getPropertyValue("--theme-color")
-        .trim();
+      const themeColor = getCSSVar("--theme-color");
       metaDescriptionDark?.setAttribute("content", themeColor);
       metaDescriptionLight?.setAttribute("content", themeColor);
     }
@@ -82,155 +91,50 @@ const useHasHydrated = () => {
   return hasHydrated;
 };
 
-function _Home() {
-  const [createNewSession, currentIndex, removeSession] = useChatStore(
-    (state) => [
-      state.newSession,
-      state.currentSessionIndex,
-      state.removeSession,
-    ],
-  );
-  const loading = !useHasHydrated();
-  const [showSideBar, setShowSideBar] = useState(true);
-
-  // setting
-  const [openSettings, setOpenSettings] = useState(false);
-  const config = useChatStore((state) => state.config);
-
-  useSwitchTheme();
-
-  if (loading) {
-    return <Loading />;
-  }
+function Screen() {
+  const config = useAppConfig();
+  const location = useLocation();
+  const isHome = location.pathname === Path.Home;
+  const isMobileScreen = useMobileScreen();
 
   return (
     <div
-      className={`${
-        config.tightBorder && !isMobileScreen()
-          ? styles["tight-container"]
-          : styles.container
-      }`}
+      className={
+        styles.container +
+        ` ${
+          config.tightBorder && !isMobileScreen
+            ? styles["tight-container"]
+            : styles.container
+        }`
+      }
     >
-      <div
-        className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
-      >
-        <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}> 
-            元话数藏专用ChatGPT
-          </div>
-          <div className={styles["sidebar-sub-title"]}>
-            <h2>平台特点</h2>
-            <h4>
-              价格低至三万;<br/>
-              一天上线数藏平台;<br/>
-              成本低，免上链费用;<br/>
-              免gas费，免支付接入费;<br/>
-              带二级市场，自定义UI;<br/>
-              阿里云waf防黑机制;<br/>
-              阿里云k8s分布式部署支持高并发;<br/>
-              可对接到元数网，盯链;<br/>
-              人工智能生成NFT（藏品）;<br/>
-              ChatGPT编写藏品故事;<br/>
-              10多种营销套路玩法;<br/>
-            </h4>
-            <h2>部分案例</h2>
-            <h4>
-            <a href="https://www.aonem.art" target="_blank">一梦数藏</a><br/>
-            <a href="https://shiqishucang.metakina.com" target="_blank">书旗数藏UI</a><br/>
-            <a href="https://lingtai.art" target="_blank">灵台数藏</a><br/>
-            <a href="https://www.yuejiemeta.com" target="_blank">超越数藏</a><br/>
-            <a href="https://www.bashibacs.com" target="_blank">八十八数仓</a><br/>
-            <a href="http://lufan.metakina.com" target="_blank">卢凡数藏</a><br/>
-            <a href="https://yh.juzhoulian.cn" target="_blank">橘子数藏</a><br/>
-            <a href="https://huaxuangeshucang.com" target="_blank">华轩数藏</a><br/>
-            <a href="http://chuangyuyishu-demo.metakina.com" target="_blank">川渝数藏</a><br/>
-            <a href="http://sdjkys-demo.metakina.com" target="_blank">丹麦数藏</a><br/>
-            </h4>
-          </div>
-          <div className={styles["sidebar-sub-title"]}>
-          <h2>联系地址：</h2> <h4>  <a href="http://metakina.com" target="_blank">http://metakina.com</a><br/> </h4>
-          </div>
-          <div className={styles["sidebar-logo"]}>
-            <ChatGptIcon />
-          </div>
-        </div>
+      <SideBar className={isHome ? styles["sidebar-show"] : ""} />
 
-        <div
-          className={styles["sidebar-body"]}
-          onClick={() => {
-            setOpenSettings(false);
-            setShowSideBar(false);
-          }}
-        >
-          <ChatList />
-        </div>
-
-        <div className={styles["sidebar-tail"]}>
-          <div className={styles["sidebar-actions"]}>
-            <div className={styles["sidebar-action"] + " " + styles.mobile}>
-              <IconButton
-                icon={<CloseIcon />}
-                onClick={() => {
-                  if (confirm(Locale.Home.DeleteChat)) {
-                    removeSession(currentIndex);
-                  }
-                }}
-              />
-            </div>
-            <div className={styles["sidebar-action"]}>
-              <IconButton
-                icon={<SettingsIcon />}
-                onClick={() => {
-                  setOpenSettings(true);
-                  setShowSideBar(false);
-                }}
-                shadow
-              />
-            </div>
-            <div className={styles["sidebar-action"]}>
-              <a href="https://metakina.com" target="_blank">
-                <IconButton icon={<GithubIcon />} shadow />
-              </a>
-            </div>
-          </div>
-          <div>
-            <IconButton
-              icon={<AddIcon />}
-              text={Locale.Home.NewChat}
-              onClick={() => {
-                createNewSession();
-                setShowSideBar(false);
-              }}
-              shadow
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className={styles["window-content"]}>
-        {openSettings ? (
-          <Settings
-            closeSettings={() => {
-              setOpenSettings(false);
-              setShowSideBar(true);
-            }}
-          />
-        ) : (
-          <Chat
-            key="chat"
-            showSideBar={() => setShowSideBar(true)}
-            sideBarShowing={showSideBar}
-          />
-        )}
+      <div className={styles["window-content"]} id={SlotID.AppBody}>
+        <Routes>
+          <Route path={Path.Home} element={<Chat />} />
+          <Route path={Path.NewChat} element={<NewChat />} />
+          <Route path={Path.Masks} element={<MaskPage />} />
+          <Route path={Path.Chat} element={<Chat />} />
+          <Route path={Path.Settings} element={<Settings />} />
+        </Routes>
       </div>
     </div>
   );
 }
 
 export function Home() {
+  useSwitchTheme();
+
+  if (!useHasHydrated()) {
+    return <Loading />;
+  }
+
   return (
     <ErrorBoundary>
-      <_Home></_Home>
+      <Router>
+        <Screen />
+      </Router>
     </ErrorBoundary>
   );
 }
