dc599e30231f3383dabaaf708a332c0130545464
xqgzs <122903349+xqgzs@users.noreply.github.com>
2023-05-14 02:07:15 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "DawnGPT",
+  description: "Your personal DawnGPT Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "DawnGPT",
     statusBarStyle: "default",
   },
 };
