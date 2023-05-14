d7db6535a5e62f0ba2454eb939a37957940afd0b
fstcm136 <102739619+fstcm136@users.noreply.github.com>
2023-05-14 15:08:27 +0800

    Revert "Fix: click the prompt button to hide hints when it's already shown"

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -795,12 +795,6 @@ export function Chat() {
           scrollToBottom={scrollToBottom}
           hitBottom={hitBottom}
           showPromptHints={() => {
-            // Click again to close
-            if (promptHints.length > 0) {
-              setPromptHints([]);
-              return;
-            }
-
             inputRef.current?.focus();
             setUserInput("/");
             onSearch("");
