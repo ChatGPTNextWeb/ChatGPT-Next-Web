25828a8771544ae0eaa6f3dd1c7f629803b55e51
zebing-xuan <xuanzebing.bingo@gmail.com>
2023-05-16 11:15:22 +0800

    update config

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -2,21 +2,24 @@ import { NextResponse } from "next/server";
 
 import { getServerSideConfig } from "../../config/server";
 
-const serverConfig = getServerSideConfig();
-
 // Danger! Don not write any secret value here!
 // 警告！不要在这里写入任何敏感信息！
-const DANGER_CONFIG = {
-  needCode: serverConfig.needCode,
-  hideUserApiKey: serverConfig.hideUserApiKey,
-  enableGPT4: serverConfig.enableGPT4,
-};
 
 declare global {
-  type DangerConfig = typeof DANGER_CONFIG;
+  type DangerConfig = {
+    needCode: boolean;
+    hideUserApiKey: boolean;
+    enableGPT4: boolean;
+  };
 }
 
 async function handle() {
+  const serverConfig = getServerSideConfig();
+  const DANGER_CONFIG = {
+    needCode: serverConfig.needCode,
+    hideUserApiKey: serverConfig.hideUserApiKey,
+    enableGPT4: serverConfig.enableGPT4,
+  };
   return NextResponse.json(DANGER_CONFIG);
 }
 
