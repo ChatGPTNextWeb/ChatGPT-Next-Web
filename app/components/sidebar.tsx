e4169838c86fc8c263cf6061c9b09601c16f181a
youzouge <129656392+youzouge@users.noreply.github.com>
2023-05-16 17:14:28 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>PGY ChatGPT</div>
         <div className={styles["sidebar-sub-title"]}>
-          PGYTECH AI BOT.
+          PGYTECH AI 仅内部交流使用.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
