ccd488f96f13567e3a18e69866707a6f4286f0cd
Limc <i@limc.me>
2023-05-17 22:42:02 +0800

    fix: bug

--- public/serviceWorkerRegister.js
+++ public/serviceWorkerRegister.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/serviceWorker.js').then(function (registration) {
+    navigator.serviceWorker.register('./serviceWorker.js').then(function (registration) {
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function (err) {
       console.error('ServiceWorker registration failed: ', err);
