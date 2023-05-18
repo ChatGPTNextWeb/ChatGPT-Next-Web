f88c8c7b80d8b4ae5fbe737281aec15457b9cb5a
crypticatul <88734298+crypticatul@users.noreply.github.com>
2023-05-19 00:39:35 +0530

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>Cognitiev PRO</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Your Ultimate AI Sidekick
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
