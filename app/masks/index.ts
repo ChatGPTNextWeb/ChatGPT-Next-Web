a84742c69a6efadaa0e0e7ab6163ac4f05a87ba5
crypticatul <88734298+crypticatul@users.noreply.github.com>
2023-05-19 00:27:04 +0530

    Update index.ts

--- app/masks/index.ts
+++ app/masks/index.ts
@@ -1,5 +1,5 @@
 import { Mask } from "../store/mask";
-import { CN_MASKS } from "./cn";
+
 import { EN_MASKS } from "./en";
 
 import { type BuiltinMask } from "./typing";
@@ -21,6 +21,6 @@ export const BUILTIN_MASK_STORE = {
   },
 };
 
-export const BUILTIN_MASKS: Mask[] = [...CN_MASKS, ...EN_MASKS].map((m) =>
+export const BUILTIN_MASKS: Mask[] = [...EN_MASKS].map((m) =>
   BUILTIN_MASK_STORE.add(m),
 );
