f6c268dc1e3afad9448801f47bcec8b4cc81ef91
ShengYan, Zhang <yan.code@gmail.com>
2023-05-19 10:39:34 +0800

    fix: allow to import a single mask

--- app/components/mask.tsx
+++ app/components/mask.tsx
@@ -256,6 +256,11 @@ export function MaskPage() {
               maskStore.create(mask);
             }
           }
+          return;
+        }
+        //if the content is a single mask.
+        if (importMasks.name) {
+          maskStore.create(importMasks);
         }
       } catch {}
     });
