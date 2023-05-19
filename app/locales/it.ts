b1ba3df989781b557f8963a83a5c285b65ef8ecc
Yidadaa <yidadaa@qq.com>
2023-05-20 00:39:52 +0800

    feat: close #1478 new chat use global config as default

--- app/locales/it.ts
+++ app/locales/it.ts
@@ -71,21 +71,6 @@ const it: LocaleType = {
     Lang: {
       Name: "Language", // ATTENTION: if you wanna add a new translation, please do not translate this value, leave it as `Language`
       All: "Tutte le lingue",
-      Options: {
-        cn: "简体中文",
-        en: "English",
-        tw: "繁體中文",
-        fr: "Français",
-        es: "Español",
-        it: "Italiano",
-        tr: "Türkçe",
-        jp: "日本語",
-        de: "Deutsch",
-        vi: "Tiếng Việt",
-        ru: "Русский",
-        cs: "Čeština",
-        ko: "한국어",
-      },
     },
     Avatar: "Avatar",
     FontSize: {
