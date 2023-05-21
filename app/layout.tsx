ea21524cadb44f404dd6e554319fe84632935cdb
zebing-xuan <xuanzebing.bingo@gmail.com>
2023-05-21 09:32:53 +0800

    rename title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "IP Detect",
+  description: "Check your IP.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "IP Detect",
     statusBarStyle: "default",
   },
 };
