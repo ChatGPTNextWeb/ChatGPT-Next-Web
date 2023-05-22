8d9f08f01267dcebf2f1943ea68d68b7bcd9132d
baoge01 <15108041500@163.com>
2023-05-22 22:02:22 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,10 +1,10 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `${REPO_URL}#keep-updated`;
-export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
-export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
+export const OWNER = "松子GPT";
+ export const REPO = "松子GPT";
+ export const REPO_URL = `https://ifrf.cn`;
+ export const ISSUE_URL = `https://ifrf.cn`;
+ export const UPDATE_URL = `https://ifrf.cn`;
+ export const FETCH_COMMIT_URL = `https://ifrf.cn`;
+ export const FETCH_TAG_URL = `https://ifrf.cn`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export enum Path {
