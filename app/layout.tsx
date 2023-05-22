697f080213d5fc2eb5f424a43246267340ec03c1
baoge01 <15108041500@163.com>
2023-05-22 20:23:09 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "松子GPT",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
