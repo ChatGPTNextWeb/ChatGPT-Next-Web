ca5535941147f6fe64a5b6da2afcc9ff8899ee04
baoge01 <15108041500@163.com>
2023-05-22 20:43:26 +0800

    Update es.ts

--- app/locales/es.ts
+++ app/locales/es.ts
@@ -11,7 +11,7 @@ const es: LocaleType = {
     ChatItemCount: (count: number) => `${count} mensajes`,
   },
   Chat: {
-    SubTitle: (count: number) => `${count} mensajes con ChatGPT`,
+    SubTitle: (count: number) => `${count} mensajes con 松子GPT`,
     Actions: {
       ChatList: "Ir a la lista de chats",
       CompressedHistory: "Historial de memoria comprimido",
@@ -41,7 +41,7 @@ const es: LocaleType = {
     Copy: "Copiar todo",
     Download: "Descargar",
     MessageFromYou: "Mensaje de ti",
-    MessageFromChatGPT: "Mensaje de ChatGPT",
+    MessageFromChatGPT: "Mensaje de 松子GPT",
   },
   Memory: {
     Title: "Historial de memoria",
