98f43fb679463222ce887ea8ba629b6b5c6b3f73
sudoxin <76290251+Jonxinxin@users.noreply.github.com>
2023-05-23 16:15:14 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -176,7 +176,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "有什么可以帮你的吗，请尽情吩咐我！有问题请发送邮件到[imnibaba@outlook.com](mailto:imnibaba@outlook.com)😊",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
