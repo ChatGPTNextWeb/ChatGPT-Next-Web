56cfe0a83e07c8ac7d70dee59f88b69cc2890970
baoge01 <21059169+baoge01@users.noreply.github.com>
2023-05-23 10:08:26 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -1,10 +1,10 @@
 import { SubmitKey } from "../store/config";
 
 const cn = {
-  WIP: "该功能仍在开发中……",
+  WIP: "ing……",
   Error: {
     Unauthorized:
-      "前往公众号：**[松子说网](https://picloud-1302482767.cos.ap-nanjing.myqcloud.com/2023/05/12/645e38958e1bb.jpg)** 回复 **密码** 免费获取密码，然后点击左下角[设置](/#/settings)按钮输入密码后解锁**免费使用**。\n\n **提问：** 能免费用 GPT-4 吗？\n 如果觉得好用，也可以适当打赏，帮助我们可持续发展![图片](https://aaaa-1257161492.cos.ap-beijing.myqcloud.com/IMG_5119%2820230523-095916%29.JPG)",
+      "前往公众号：**[松子说网](https://aaaa-1257161492.cos.ap-beijing.myqcloud.com/QQ%E6%88%AA%E5%9B%BE20230523095241.png)** 回复 **密码** 免费获取密码，然后点击左下角[设置](/#/settings)按钮输入密码后解锁**免费使用**。\n\n  如果觉得好用，也可以适当打赏，帮助我们可持续发展![图片](https://aaaa-1257161492.cos.ap-beijing.myqcloud.com/IMG_5119%2820230523-095916%29.JPG)",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
