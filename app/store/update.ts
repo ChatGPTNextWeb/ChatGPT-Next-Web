63982ea73132b91d2adbd308e2a6b86bf4f76d9b
baoge01 <21059169+baoge01@users.noreply.github.com>
2023-05-23 11:30:04 +0800

    Update update.ts

--- app/store/update.ts
+++ app/store/update.ts
@@ -17,21 +17,8 @@ export interface UpdateStore {
   updateUsage: (force?: boolean) => Promise<void>;
 }
 
-function queryMeta(key: string, defaultValue?: string): string {
-  let ret: string;
-  if (document) {
-    const meta = document.head.querySelector(
-      `meta[name='${key}']`,
-    ) as HTMLMetaElement;
-    ret = meta?.content ?? "";
-  } else {
-    ret = defaultValue ?? "";
-  }
 
-  return ret;
-}
-
-const ONE_MINUTE = 60 * 1000;
+const ONE_MINUTE = 10000;
 
 export const useUpdateStore = create<UpdateStore>()(
   persist(
@@ -44,26 +31,8 @@ export const useUpdateStore = create<UpdateStore>()(
       version: "unknown",
 
       async getLatestVersion(force = false) {
-        set(() => ({ version: queryMeta("version") ?? "unknown" }));
-
-        const overTenMins = Date.now() - get().lastUpdate > 10 * ONE_MINUTE;
-        if (!force && !overTenMins) return;
-
-        set(() => ({
-          lastUpdate: Date.now(),
-        }));
-
-        try {
-          const data = await (await fetch(FETCH_COMMIT_URL)).json();
-          const remoteCommitTime = data[0].commit.committer.date;
-          const remoteId = new Date(remoteCommitTime).getTime().toString();
-          set(() => ({
-            remoteVersion: remoteId,
-          }));
-          console.log("[Got Upstream] ", remoteId);
-        } catch (error) {
-          console.error("[Fetch Upstream Commit Id]", error);
-        }
+        // Version checking functionality is disabled.
+        return;
       },
 
       async updateUsage(force = false) {
