d02037da6905374b2947f9acef7f04e04400f2f2
mmwong1984 <103017331+mmwong1984@users.noreply.github.com>
2023-05-24 21:14:39 +0800

    Update emoji.tsx

--- app/components/emoji.tsx
+++ app/components/emoji.tsx
@@ -8,6 +8,7 @@ import { ModelType } from "../store";
 
 import BotIcon from "../icons/bot.svg";
 import BlackBotIcon from "../icons/black-bot.svg";
+import ClaudeIcon from "../icons/claude.svg";
 
 export function getEmojiUrl(unified: string, style: EmojiStyle) {
   return `https://cdn.staticfile.org/emoji-datasource-apple/14.0.0/img/${style}/64/${unified}.png`;
