b7fe1f6f9f515b8fd35807a1509f3146c3466356
mmwong1984 <103017331+mmwong1984@users.noreply.github.com>
2023-05-24 17:28:24 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -69,10 +69,6 @@ export const ALL_MODELS = [
     available: true,
   },
   {
-    name: "qwen-v1", // 通义千问
-    available: false,
-  },
-  {
     name: "ernie", // 文心一言
     available: false,
   },
