1621c3481a6389957fdb36dc1231aeb3cdd8ca1c
Saurav Kumar <sauravkhdoolia@gmail.com>
2023-05-27 15:12:03 +0530

    fix: next still not allowing to set cookie
    Signed-off-by: Saurav Kumar <sauravkhdoolia@gmail.com>

--- app/store/theme.server.ts
+++ app/store/theme.server.ts
@@ -2,8 +2,10 @@
 import { cookies } from "next/headers";
 
 export async function changeTheme(theme: string) {
-  "use server";
+  ("use server");
 
   const cookieStore = cookies();
+
+  // @ts-ignore
   cookieStore.set("theme", theme);
 }
