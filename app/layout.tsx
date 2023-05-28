e4f82a9859cf3d98b6f7111628bf8758b8dab25c
Henry Huang <84128702+bravohenry@users.noreply.github.com>
2023-05-28 11:00:24 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Bravo Chat",
+  description: "powered by OpenAI",
   viewport: {
     width: "device-width",
     initialScale: 1,
