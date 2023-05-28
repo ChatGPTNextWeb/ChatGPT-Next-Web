826b6639e386a3e67f82e01ec74c953bb79ed69a
snf <132287412+snownightflower@users.noreply.github.com>
2023-05-29 02:01:25 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from cel/analytics/react";
+import { Analytics } from "@vercel/analytics/react";
 
 import { Home } from "./components/home";
 
@@ -14,29 +14,3 @@ export default async function App() {
     </>
   );
 }
-
-import Head from 'next/head'
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        {/* Replace this with your Google Analytics script */}
-        <script>
-          // <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-3XG47CPH77"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-3XG47CPH77');
-</script>
-        </script>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
-}
-
-export default MyApp
