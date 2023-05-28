051cc4818c74c4b3136f34c27c370f8c110ee2e2
snf <132287412+snownightflower@users.noreply.github.com>
2023-05-29 02:40:30 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,31 +10,7 @@ export default async function App() {
   return (
     <>
       <Home />
-      import { Analytics } from "@vercel/analytics/react";
-import { Home } from "./components/home";
-import { getServerSideConfig } from "./config/server";
-import Head from 'next/head';
-
-const serverConfig = getServerSideConfig();
-
-export default function App() {
-  return (
-    <>
-      <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3XG47CPH77"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-3XG47CPH77');
-          `
-        }}>
-        </script>
-      </Head>
-      <Home />
       {serverConfig?.isVercel && <Analytics />}
     </>
   );
 }
-
