503f40155eda3a783460bd0f3532c8aee9b35250
snf <132287412+snownightflower@users.noreply.github.com>
2023-05-29 02:27:26 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
-import Head from 'next/head'
+import { Analytics } from "@vercel/analytics/react";
+
+import { Home } from "./components/home";
+
+import { getServerSideConfig } from "./config/server";
+
+const serverConfig = getServerSideConfig();
+
+export default async function App() {
+  return (
+    <>
+      <Home />
+      import { Analytics } from "@vercel/analytics/react";
 import { Home } from "./components/home";
 import { getServerSideConfig } from "./config/server";
+import Head from 'next/head';
 
 const serverConfig = getServerSideConfig();
 
@@ -23,20 +36,6 @@ export default function App() {
     </>
   );
 }
-
-
-import { Analytics } from "@vercel/analytics/react";
-
-import { Home } from "./components/home";
-
-import { getServerSideConfig } from "./config/server";
-
-const serverConfig = getServerSideConfig();
-
-export default async function App() {
-  return (
-    <>
-      <Home />
       {serverConfig?.isVercel && <Analytics />}
     </>
   );
