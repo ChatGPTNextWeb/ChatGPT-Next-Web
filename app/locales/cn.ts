f3e2b4fa56bcdaae039e4bfc9f52378aa62cce20
YuukiTriple <62502859+YuukiTriple@users.noreply.github.com>
2023-05-28 21:32:07 +0800

    Update cn.ts
    文本修改

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -176,7 +176,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "有什么可以帮你的吗（公用20美金key，用完了自备）",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
