356692ee09f49abbe0fb4fc8daf4455775064c27
mmwong1984 <103017331+mmwong1984@users.noreply.github.com>
2023-05-29 11:24:36 +0800

    Update settings.tsx

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -491,8 +491,33 @@ export function Settings() {
                 }}
               />
             </ListItem>
-          ) : null}
-        
+          ) : null}        
+         
+          <ListItem
+            title={Locale.Settings.Usage.Title}
+            subTitle={
+              showUsage
+                ? loadingUsage
+                  ? Locale.Settings.Usage.IsChecking
+                  : Locale.Settings.Usage.SubTitle(
+                      usage?.used ?? "Unlimited",
+                      usage?.subscription ?? "Unlimited Use",
+                    )
+                : Locale.Settings.Usage.NoAccess
+            }
+          >
+            {!showUsage || loadingUsage ? (
+              <div />
+            ) : (
+              <IconButton
+                icon={<ResetIcon></ResetIcon>}
+                text={Locale.Settings.Usage.Check}
+                onClick={() => checkUsage(false)}
+              />
+            )}
+          </ListItem>
+        </List>
+
         <List>
           <ListItem
             title={Locale.Settings.Prompt.Disable.Title}
