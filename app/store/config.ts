94ee7e7ec89d2aa8fa792099f8c46bed1de797a6
mmwong1984 <103017331+mmwong1984@users.noreply.github.com>
2023-05-29 13:45:48 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -17,7 +17,7 @@ export enum Theme {
 }
 
 export const DEFAULT_CONFIG = {
-  submitKey: SubmitKey.CtrlEnter as SubmitKey,
+  submitKey: SubmitKey.Enter as SubmitKey,
   avatar: "1f603",
   fontSize: 14,
   theme: Theme.Auto as Theme,
