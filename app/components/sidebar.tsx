d47f3d5595a2fd5d717bd7865e7bb5c2216785de
ZDX-VVB V587 <zgc277526225@gmail.com>
2023-06-01 15:16:13 +0000

    -- 标题

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,10 +119,8 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
-        <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
-        </div>
+        <div className={styles["sidebar-title"]}>智能AI助手</div>
+        <div className={styles["sidebar-sub-title"]}>问鼎九洲信息科技.</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
