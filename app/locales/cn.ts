af0abf448a278936cc878f906cc2dac79d41f994
snf <132287412+snownightflower@users.noreply.github.com>
2023-06-03 00:50:50 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,10 +4,8 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-      "访问密码不正确或为空，请点击👉[设置](/#/settings)页输入正确的访问密码，或者填入您自己/或购买的 OpenAI API Ke
-     👉[领取您的ai使用指南全集](https://bw54wz1lzj.feishu.cn/docx/DNofd1v9KoK9XLxs2pjciOpInUf)
-     👉[查看免责声明](https://bw54wz1lzj.feishu.cn/docx/KyYhdXEnuospwbxzhUPcixOSnS7)           
-     👉[联系客服](https://bw54wz1lzj.feishu.cn/docx/IisZd35bwophDexgpjscMUgTnhe)",},
+      "访问密码不正确或为空，请点击👉[设置](/#/settings)页输入正确的访问密码，或者填入您自己/或购买的 OpenAI API Key。👉[领取您的ai使用指南全集](https://bw54wz1lzj.feishu.cn/docx/DNofd1v9KoK9XLxs2pjciOpInUf)👉[查看免责声明](https://bw54wz1lzj.feishu.cn/docx/KyYhdXEnuospwbxzhUPcixOSnS7) 👉[联系客服](https://bw54wz1lzj.feishu.cn/docx/IisZd35bwophDexgpjscMUgTnhe)点击👉[领取属于您强大且私人独享的ai助理](https://bw54wz1lzj.feishu.cn/docx/DNofd1v9KoK9XLxs2pjciOpInUf)",
+  },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
   },
@@ -178,9 +176,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "我是您的强大且私人独享的AI助理,有什么可以帮你的嘛? 
-        请点击👉[属于您的ai使用教程全集](https://bw54wz1lzj.feishu.cn/docx/DNofd1v9KoK9XLxs2pjciOpInUf)
-             👉[查看免责声明](https://bw54wz1lzj.feishu.cn/docx/KyYhdXEnuospwbxzhUPcixOSnS7)",
+    BotHello: "我是您的强大且私人独享的AI助理,有什么可以帮你的嘛?怎么使用 请点击👉[属于您的ai启发指南教程](https://bw54wz1lzj.feishu.cn/docx/DNofd1v9KoK9XLxs2pjciOpInUf) ",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
