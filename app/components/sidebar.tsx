09c2e0c5741ae69419d8e336a1f309e5e3734066
crypticatul <88734298+crypticatul@users.noreply.github.com>
2023-06-03 15:34:05 +0530

    Update sidebar.tsx Bug Fix Subscription management

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -188,7 +188,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <Link to="/app/account/plans">
+            <Link to="../account/plans">
               <IconButton icon={<PayIcon />} shadow />
             </Link>
           </div>
