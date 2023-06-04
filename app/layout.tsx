fe11da0f84bf51df1366622008494e87e42cab0f
星尘 <73565648+xmxxc@users.noreply.github.com>
2023-06-04 10:19:52 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "XMX网络——CHATGPT FOR XMX",
+  description: "XMX,XMX网络，XMXAI,ChatGPT,Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "XMX网络——CHATGPT FOR XMX",
     statusBarStyle: "default",
   },
 };
