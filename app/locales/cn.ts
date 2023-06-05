1b6eba6f52e11b3436879f68211c9a84a16d77ba
2764776697 <81808845+2764776697@users.noreply.github.com>
2023-06-05 18:18:47 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -86,7 +86,7 @@ const cn = {
       ConfirmClearAll: "确认清除所有数据？",
     },
     Lang: {
-      Name: "Language", // ATTENTION: if you wanna add a new translation, please do not translate this value, leave it as `Language`
+      Name: "语言", // ATTENTION: if you wanna add a new translation, please do not translate this value, leave it as `Language`
       All: "所有语言",
     },
     Avatar: "头像",
@@ -157,7 +157,7 @@ const cn = {
     },
     AccessCode: {
       Title: "访问密码",
-      SubTitle: "管理员已开启加密访问",
+      SubTitle: "输入访问密码即可免费使用",
       Placeholder: "请输入访问密码",
     },
     Model: "模型 (model)",
@@ -241,8 +241,8 @@ const cn = {
     Skip: "直接开始",
     NotShow: "不再展示",
     ConfirmNoShow: "确认禁用？禁用后可以随时在设置中重新启用。",
-    Title: "挑选一个面具",
-    SubTitle: "现在开始，与面具背后的灵魂思维碰撞",
+    Title: "挑选一个对话场景",
+    SubTitle: "现在开始，与对话场景背后的灵魂思维碰撞",
     More: "查看全部",
   },
 
