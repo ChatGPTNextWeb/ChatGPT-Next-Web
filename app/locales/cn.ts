14fba3c614ceab4a5757ccaa04c4813087f25e22
2764776697 <81808845+2764776697@users.noreply.github.com>
2023-06-05 18:06:57 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,7 +4,8 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-      "点击网页左下角设置按钮，找到访问密码填：666888 即可免费使用<br>如果机器人回答卡顿，说明使用人数较多，请前往：ka.mofaxi.cn 购买自己的独立API Key。",
+      "点击网页左下角设置按钮，找到访问密码填：666888 即可免费使用
+    如果机器人回答卡顿，说明使用人数较多，请前往：ka.mofaxi.cn 购买自己的独立API Key。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
