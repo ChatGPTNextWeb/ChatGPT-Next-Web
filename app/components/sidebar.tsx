4b0b5449bb1a53dfcff108fca455806758df4b90
crypticatul <88734298+crypticatul@users.noreply.github.com>
2023-06-05 23:53:10 +0530

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -183,7 +183,7 @@ export function SideBar(props: { className?: string }) {
             />
           </div> */}
           <div className={styles["sidebar-action"]}>
-            <Link href='/settings'>
+            <Link href='/Settings'>
               <IconButton icon={<SettingsIcon />} shadow />
             </Link>
           </div>
