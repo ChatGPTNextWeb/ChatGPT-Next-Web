e1827d51ad9bdae2f5681d666fdc03cf23c8b618
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-06-05 17:09:51 +0200

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGPT Next</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          AdEx Partners - secure local UI for OpenAI API
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
