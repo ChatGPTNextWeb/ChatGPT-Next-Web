b098f2769ec96dc329b21b6400d43c4dbeeb3d49
beliker <1251683316@qq.com>
2023-06-09 17:44:46 +0800

    main test1.1

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -438,7 +438,7 @@ export const useChatStore = create<ChatStore>()(
           api.llm.chat({
             messages: topicMessages,
             config: {
-              model: "gpt-3.5-turbo-MAX",
+              model: "gpt-3.5-turbo",
             },
             onFinish(message) {
               get().updateCurrentSession(
--- app/store/config.ts
+++ app/store/config.ts
@@ -30,7 +30,7 @@ export const DEFAULT_CONFIG = {
   dontShowMaskSplashScreen: false, // dont show splash screen when create chat
 
   modelConfig: {
-    model: "gpt-3.5-turbo-MAX" as ModelType,
+    model: "gpt-3.5-turbo" as ModelType,
     temperature: 0.5,
     max_tokens: 2000,
     presence_penalty: 0,
@@ -67,10 +67,6 @@ export const ALL_MODELS = [
   {
     name: "gpt-4-32k-0314",
     available: ENABLE_GPT4,
-  },
-  {
-    name: "gpt-3.5-turbo-MAX",
-    available: true,
   },
   {
     name: "gpt-3.5-turbo",
