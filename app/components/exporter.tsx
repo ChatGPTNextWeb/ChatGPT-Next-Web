20016e045ec211b317457cea08ae8f14115e6f00
KangHui <KangHui@szmtc.com.cn>
2023-06-10 22:40:07 +0800

    更新标题

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -437,7 +437,7 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>GMTC ChatGPT</div>
             <div className={styles["sub-title"]}>
               github.com/Yidadaa/ChatGPT-Next-Web
             </div>
