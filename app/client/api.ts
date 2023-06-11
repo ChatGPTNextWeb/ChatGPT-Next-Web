9d536e3cf2016eb2178763436ad6027760cb66b9
FocusedFidgeter <128767209+FocusedFidgeter@users.noreply.github.com>
2023-06-11 15:21:27 +0000

    Rebranding

--- app/client/api.ts
+++ app/client/api.ts
@@ -64,12 +64,9 @@ export class ClientApi {
       .concat([
         {
           from: "human",
-          value:
-            "Share from [ChatGPT Next Web]: https://github.com/Yidadaa/ChatGPT-Next-Web",
+          value: "Share from [Atomic Assistant]: https://AtomicAssistant.pro",
         },
       ]);
-    // 敬告二开开发者们，为了开源大模型的发展，请不要修改上述消息，此消息用于后续数据清洗使用
-    // Please do not modify this message
 
     console.log("[Share]", msgs);
     const res = await fetch("/sharegpt", {
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Atomic Assistant",
+  description: 'Atom Assists by wearing "Masks".',
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Atomic Assistant",
     statusBarStyle: "default",
   },
 };
