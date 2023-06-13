e978388236a8d9e0057fa7bb3e1b46270a76a527
AI探险家 <aizpy@BitFort.local>
2023-06-13 22:58:58 +0800

    Update slogan

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>Aizpy</div>
         <div className={styles["sidebar-sub-title"]}>
-          Chat with your own AI assistant.
+          Your personal AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
