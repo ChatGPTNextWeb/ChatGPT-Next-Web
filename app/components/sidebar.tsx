2a95a7f881c19e493e8f6c1d9d10f46342694fdf
RoyZ-CSGO <Roy@DMR.gg>
2023-06-13 18:44:06 +0800

    SUBTITLE EDIT

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>[DMR] ChatGPT Next</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          AI For Everyone.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
@@ -174,7 +174,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank">
+            <a href="https://github.com/DMR-Team/" target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
