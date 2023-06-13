6da9671af72496a38755b39c353ed78d898713c9
AI探险家 <aizpy@BitFort.local>
2023-06-14 00:45:53 +0800

    update icon

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 
 import styles from "./home.module.scss";
 
-import BotIcon from "../icons/bot.svg";
+import BotIcon from "../icons/chatgpt.svg";
 import LoadingIcon from "../icons/three-dots.svg";
 
 import { getCSSVar, useMobileScreen } from "../utils";
