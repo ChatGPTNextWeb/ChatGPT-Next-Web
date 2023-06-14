fe0d4815806fc6ac592d6cb3ff61c581175796e1
yuhan-feng <55166473+yuhan-feng@users.noreply.github.com>
2023-06-14 13:23:53 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -5,11 +5,11 @@ const cn = {
   Error: {
     Unauthorized: [
       "#**当前状态为未授权！**",
-      "**[前往公众号](https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU5Mzc4MTM1Nw==)**",
+      "**[前往公众号柴造DOGC](https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU5Mzc4MTM1Nw==)**",
       "回复 **密码** 免费获取密码，然后点击左下角[设置](/#/settings)按钮输入密码后解锁**免费使用**。",
       "**声明：** 本站点为免费站点，请合理合法使用",
       "**保证：** 不会收集任何信息",
-      "![图片](/wechaty.webp)",
+      { "image": "![图片](/wechaty.webp)" },
     ]
   },
   Auth: {
@@ -210,12 +210,14 @@ const cn = {
 
  **声明：** 本站点为免费站点，请合理合法使用
  **保证：** 不会收集任何信息
-
+ 
  常见问题与使用说明点击查看 **[腾讯文档链接](腾讯文档链接)** 
 
 卡顿或无响应？可能是用户量太大， [申请独立账号](chat.zhai-zhai.com)
 
-欢迎扔出需求[黄柴柴客服](https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU5Mzc4MTM1Nw==)`, // 这里是第 172 行
+欢迎扔出需求[黄柴柴客服](https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU5Mzc4MTM1Nw==)`
+   
+   "![图片](/wechaty.webp)", // 这里是第 172 行
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
