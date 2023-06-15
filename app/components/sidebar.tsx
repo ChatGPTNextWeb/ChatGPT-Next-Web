660f8bbdabab60eed41bdf8457ea6d0239e8d551
sunnyjoyboy <sunnyjoyboys@gmail.com>
2023-06-15 22:12:26 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>Sky.bingchat</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          vx公众号：sky轻创AI；vx客服:chatGPTdidi
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
