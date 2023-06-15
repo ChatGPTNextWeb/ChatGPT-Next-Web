cf1a0c2ba8d0e4fd1b527bef8d2fd3d6309e096a
aizpy <aizpy@outlook.com>
2023-06-15 10:28:23 +0800

    update icon

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -6,7 +6,7 @@ import { Path } from "../constant";
 import { useAccessStore } from "../store";
 import Locale from "../locales";
 
-import BotIcon from "../icons/bot.svg";
+import BotIcon from "../icons/bot.png";
 
 export function AuthPage() {
   const navigate = useNavigate();
