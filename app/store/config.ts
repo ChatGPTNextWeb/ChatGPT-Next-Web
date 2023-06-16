e2839fe1bff1bbc0aa79b338441f249df8344e2f
rizaust <34341804+rizaust@users.noreply.github.com>
2023-06-16 11:50:06 +0800

    更新最新gpt模型

--- app/store/config.ts
+++ app/store/config.ts
@@ -57,6 +57,10 @@ export const ALL_MODELS = [
     available: ENABLE_GPT4,
   },
   {
+    name: "gpt-4-0314",
+    available: ENABLE_GPT4,
+  },
+  {
     name: "gpt-4-0613",
     available: ENABLE_GPT4,
   },
@@ -69,11 +73,23 @@ export const ALL_MODELS = [
     available: ENABLE_GPT4,
   },
   {
+    name: "gpt-4-32k-0613",
+    available: ENABLE_GPT4,
+  },
+  {
     name: "gpt-3.5-turbo",
     available: true,
   },
   {
+    name: "gpt-3.5-turbo-0314",
+    available: true,
+  },
+  {
     name: "gpt-3.5-turbo-0613",
+    available: true,
+  },
+  {
+    name: "gpt-3.5-turbo-16k",
     available: true,
   },
   {
