241cba805719f0710ad122c7f6ff51a2d1e5c3e5
Joshua <7747985+yeenbean@users.noreply.github.com>
2023-06-15 22:27:14 -0700

    Apply the most objectively horrendous branding ever

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -120,11 +120,9 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT Next<sup>[y]</sup>
         </div>
-        <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
-        </div>
+        <div className={styles["sidebar-sub-title"]}>are ya #yeening son?</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
