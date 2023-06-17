a42d65890a856ff343cee0b1111fecc25e8e0d58
Joshua <7747985+yeenbean@users.noreply.github.com>
2023-06-16 19:08:53 -0700

    Update dependencies for Ubuntu worker

--- scripts/setup-ubuntu-2204-ci.sh
+++ scripts/setup-ubuntu-2204-ci.sh
@@ -23,7 +23,7 @@ sudo apt install -y libwebkit2gtk-4.0-dev \
     wget \
     libssl-dev \
     libgtk-3-dev \
-    libayatana-appindicator3-dev \
+    libappindicator3-dev \
     librsvg2-dev
     
 # install AppImage dependencies
