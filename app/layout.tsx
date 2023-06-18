870840621d113523ab756a3ea9db986cc9aa188d
baohanbu <136401006+baohanbu@users.noreply.github.com>
2023-06-18 12:27:44 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "MiaoMiaoChat",
+  description: "Your personal MiaoMiao Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -17,7 +17,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "MiaoMiaoChat",
     statusBarStyle: "default",
   },
 };
