22e1364beda2595ea527ba78cc1e2481b57c3c35
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-06-21 13:20:09 +0200

    Update redirect.js

--- public/redirect.js
+++ public/redirect.js
@@ -1,5 +1,5 @@
 if (window.location.hostname === "adexgpt.vercel.app") {
     var currentPath = window.location.pathname + window.location.search + window.location.hash;
     var newUrl = "https://adexgpt.adexpartners.com" + currentPath;
-    window.location.href = newUrl;
+//    window.location.href = newUrl;
 }
