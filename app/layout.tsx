5e090d96ade0ff1e271f0e27e1522745da6241c9
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-06-21 12:35:18 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
+        <script src="/redirect.js"></script>
       </head>
       <body>{children}</body>
     </html>
