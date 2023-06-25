7207cf6c7afe4c5a4664764fc72156e295494b10
Yidadaa <yidadaa@qq.com>
2023-06-26 01:27:36 +0800

    feat: align chat page title center on mobile screen

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -18,7 +18,7 @@ const en: LocaleType = {
     ChatItemCount: (count: number) => `${count} messages`,
   },
   Chat: {
-    SubTitle: (count: number) => `${count} messages with ChatGPT`,
+    SubTitle: (count: number) => `${count} messages`,
     Actions: {
       ChatList: "Go To Chat List",
       CompressedHistory: "Compressed History Memory Prompt",
