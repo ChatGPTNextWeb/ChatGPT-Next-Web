737f866c04a34cb8b9fbf12c10cee99e6328a574
TOZI <48510108+tozmart@users.noreply.github.com>
2023-06-25 16:32:25 +0800

    Update layout.tsx
    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ININPOP Chat",
+  description: "Your personal ININPOP Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -17,7 +17,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ININPOP Chat",
     statusBarStyle: "default",
   },
 };
