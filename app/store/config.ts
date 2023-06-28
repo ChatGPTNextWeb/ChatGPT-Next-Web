1054ee468cc5893823f093a5ec44d6e817dbd40a
chen <1154760273@qq.com>
2023-06-28 19:43:55 +0800

    恢复上次去除的多余模型

--- app/store/config.ts
+++ app/store/config.ts
@@ -52,30 +52,30 @@ export type ModelConfig = ChatConfig["modelConfig"];
 const ENABLE_GPT4 = true;
 
 export const ALL_MODELS = [
-  // {
-  //   name: "gpt-4",
-  //   available: ENABLE_GPT4,
-  // },
-  // {
-  //   name: "gpt-4-0314",
-  //   available: ENABLE_GPT4,
-  // },
-  // {
-  //   name: "gpt-4-32k",
-  //   available: ENABLE_GPT4,
-  // },
-  // {
-  //   name: "gpt-4-32k-0314",
-  //   available: ENABLE_GPT4,
-  // },
+  {
+    name: "gpt-4(4不可用)",
+    available: ENABLE_GPT4,
+  },
+  {
+    name: "gpt-4-0314",
+    available: ENABLE_GPT4,
+  },
+  {
+    name: "gpt-4-32k",
+    available: ENABLE_GPT4,
+  },
+  {
+    name: "gpt-4-32k-0314",
+    available: ENABLE_GPT4,
+  },
   {
     name: "gpt-3.5-turbo",
     available: true,
   },
-  // {
-  //   name: "gpt-3.5-turbo-0301",
-  //   available: true,
-  // },
+  {
+    name: "gpt-3.5-turbo-0301",
+    available: true,
+  },
   {
     name: "gpt-3.5-turbo-16k-0613",
     available: true,
