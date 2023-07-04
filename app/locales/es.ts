9cd9a3eb4fb65b78ff8121219d856fb417564e6e
Marcus Schiesser <mail@marcusschiesser.de>
2023-07-05 00:07:26 +0700

    feat: add setting to hide builtin masks

--- app/locales/es.ts
+++ app/locales/es.ts
@@ -87,8 +87,14 @@ const es: PartialLocaleType = {
       SubTitle: "Preview markdown in bubble",
     },
     Mask: {
-      Title: "Mask Splash Screen",
-      SubTitle: "Show a mask splash screen before starting new chat",
+      Splash: {
+        Title: "Mask Splash Screen",
+        SubTitle: "Show a mask splash screen before starting new chat",
+      },
+      Builtin: {
+        Title: "Show Builtin Masks",
+        SubTitle: "Show builtin masks in mask list",
+      },
     },
     Prompt: {
       Disable: {
