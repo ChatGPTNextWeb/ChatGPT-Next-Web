98614a89ae28c6f483fa7a2a34f6c3bae75fe8a6
crypticatul <88734298+crypticatul@users.noreply.github.com>
2023-07-07 00:20:22 +0530

    Update page.tsx

--- app/plans/page.tsx
+++ app/plans/page.tsx
@@ -110,13 +110,13 @@ const Plans = () => {
             <div className={styles.plan}>
               <p className={styles.type}>Manage Subscription</p>
              
-               <h2 className={styles.price}> 💰 </h2>
+               <h2 className={styles.price}> Manage 💳 </h2>
               <Link href='/account/plans'>
                 <a>
                   <IconButton
                   bordered
                   className={styles.button}
-                  text="MANAGE💲"
+                  text="Subscription 🤑"
                   />
                 </a>
               </Link>
