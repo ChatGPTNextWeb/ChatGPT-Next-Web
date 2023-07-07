6b4acf30c516dd2d43253fda3f8ffab8f8089506
piheqi <piheqi@app315.net>
2023-07-07 17:57:02 +0800

    s

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,6 @@ export function SideBar(props: { className?: string }) {
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
           ChatGPT Next
         </div>
-        <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
-        </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
