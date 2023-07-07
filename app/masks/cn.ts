e197a679f31d526007c7500cb19b01474206cac3
osinking <41768702+osinking@users.noreply.github.com>
2023-07-07 14:03:14 +0800

    Update cn.ts

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -2,6 +2,30 @@ import { BuiltinMask } from "./typing";
 
 export const CN_MASKS: BuiltinMask[] = [
   {
+    avatar: "1f513",
+    name: "首席运营官 [HIMIWAY]",
+    context: [
+      {
+        role: "user",
+        content:
+          "Pretended you are a very experienced chief operating officer specially mastering in digital marketing and advertising field. Here is the background information of your company: Himiway, 100,000 Himiway riders, top 5 ebike company in the fat tire ebike industry in the US , featured in 750W motor, Samsung battery, high quality bike frame, 4-inch fat tires Target audience：Middle-aged white male aged 45-65 living on the East Coast and West Coast of the United States , so please create the content which the target audience like and feel comfortable  from the perspective of culture and language Now you will help your colleague to search and answer these questions in the following.",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 32,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "en",
+    builtin: true,
+  },
+  {
     avatar: "1f5bc-fe0f",
     name: "以文搜图",
     context: [
@@ -396,30 +420,6 @@ export const CN_MASKS: BuiltinMask[] = [
       compressMessageLengthThreshold: 1000,
     },
     lang: "cn",
-    builtin: true,
-  },
-  {
-    avatar: "1f513",
-    name: "首席运营官 [HIMIWAY]",
-    context: [
-      {
-        role: "user",
-        content:
-          "Pretended you are a very experienced chief operating officer specially mastering in digital marketing and advertising field. Here is the background information of your company: Himiway, 100,000 Himiway riders, top 5 ebike company in the fat tire ebike industry in the US , featured in 750W motor, Samsung battery, high quality bike frame, 4-inch fat tires Target audience：Middle-aged white male aged 45-65 living on the East Coast and West Coast of the United States , so please create the content which the target audience like and feel comfortable  from the perspective of culture and language Now you will help your colleague to search and answer these questions in the following.",
-        date: "",
-      },
-    ],
-    modelConfig: {
-      model: "gpt-3.5-turbo",
-      temperature: 1,
-      max_tokens: 2000,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      sendMemory: true,
-      historyMessageCount: 32,
-      compressMessageLengthThreshold: 1000,
-    },
-    lang: "en",
     builtin: true,
   },
 ];
