28cb42e85a2e0badc100ae39c4656e422c956db3
crypticatul <88734298+crypticatul@users.noreply.github.com>
2023-07-09 08:45:10 +0530

    Update page.tsx

--- app/plans/page.tsx
+++ app/plans/page.tsx
@@ -27,13 +27,13 @@ const Plans = () => {
       <div className={styles.container}>
 
         <div className={styles.plans}>
-            <div className={styles.plan}>
+            
           <h2 className={styles.price}>SUBSCRIBE</h2>
-          <p className={styles.type}>Please ALLOW POP-UP to go through Payments</p>
-              </div>
+          
+              
             </div>
         <div className={styles.planContainer}>
-          
+          <p className={styles.type}>Please ALLOW POP-UP to go through Payments</p>
           <div className={styles.plans}>
             <div className={styles.plan}>
               <p className={styles.type}>Monthly</p>
