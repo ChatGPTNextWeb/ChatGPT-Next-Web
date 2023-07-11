19b243e7b234eb77eac9ce33d05b3c46ffe59059
hwtlpu <132579572+hwtlpu@users.noreply.github.com>
2023-07-12 00:29:39 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,10 +117,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          人工智能机器人
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          GPT-4 商店haoduo.us
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
