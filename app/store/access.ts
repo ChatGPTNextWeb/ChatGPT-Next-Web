d02935a2266db6b399b5ab9e29214c08973cd201
CZB <1182733571@qq.com>
2023-07-12 18:14:46 +0800

    取消自动同步

--- app/store/access.ts
+++ app/store/access.ts
@@ -27,7 +27,9 @@ export interface AccessControlStore {
 let fetchState = 0; // 0 not fetch, 1 fetching, 2 done
 
 const DEFAULT_OPENAI_URL =
-  getClientConfig()?.buildMode === "export" ? DEFAULT_API_HOST : "/api/openai/";
+  getClientConfig()?.buildMode === "export"
+    ? DEFAULT_API_HOST
+    : "https://api.akm.pw/";
 console.log("[API] default openai url", DEFAULT_OPENAI_URL);
 
 export const useAccessStore = create<AccessControlStore>()(
