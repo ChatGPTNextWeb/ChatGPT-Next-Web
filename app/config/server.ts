00fcd79c17f7d36f21b77b8e2bcfa5c34de8aa36
silence-tin <130728888+silence-tin@users.noreply.github.com>
2023-07-16 14:25:40 +0800

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -36,9 +36,10 @@ export const getServerSideConfig = () => {
       "[Server Config] you are importing a nodejs-only module outside of nodejs",
     );
   }
-
+const apiKeys=(process.env.OPENAI_API_KEY ?? ").split(',')
+const apiKey=apiKeys.at(Math.floor(Math.random()* apiKeys.length))??"
   return {
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKeys,
     code: process.env.CODE,
     codes: ACCESS_CODES,
     needCode: ACCESS_CODES.size > 0,
