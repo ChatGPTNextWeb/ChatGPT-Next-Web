ef398c366e3d9745a83e0441c05f0c72f13a9224
silence-tin <130728888+silence-tin@users.noreply.github.com>
2023-07-16 16:20:39 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,57 +1,11 @@
-import React, { useState } from 'react';
 import "./styles/globals.scss";
 import "./styles/markdown.scss";
 import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
+import AccessOverlay from './AccessOverlay.client';
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-  },
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "#fafafa" },
-    { media: "(prefers-color-scheme: dark)", color: "#151515" },
-  ],
-  appleWebApp: {
-    title: "ChatGPT Next Web",
-    statusBarStyle: "default",
-  },
-};
-
-const AccessOverlay = ({ children }) => {
-  const [password, setPassword] = useState("");
-  const [accessGranted, setAccessGranted] = useState(false);
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handlePasswordSubmit = (event) => {
-    event.preventDefault();
-    if (password === "admin") {
-      setAccessGranted(true);
-    } else {
-      alert("Wrong password. Please try again.");
-      setPassword("");
-    }
-  };
-
-  if (accessGranted) {
-    return children;
-  } else {
-    return (
-      <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.5)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <form onSubmit={handlePasswordSubmit}>
-          <input type="password" value={password} onChange={handlePasswordChange} placeholder="Enter password" />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
-  }
+  // existing metadata...
 };
 
 export default function RootLayout({
@@ -62,9 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="config" content={JSON.stringify(getClientConfig())} />
-        <link rel="manifest" href="/site.webmanifest"></link>
-        <script src="/serviceWorkerRegister.js" defer></script>
+        {/* existing head elements */}
       </head>
       <body>
         <AccessOverlay>{children}</AccessOverlay>
