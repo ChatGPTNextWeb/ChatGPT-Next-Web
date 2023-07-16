4c931d19856ef846452e18fada501a5676eeec9c
silence-tin <130728888+silence-tin@users.noreply.github.com>
2023-07-16 16:18:51 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @next/next/no-page-custom-font */
+import React, { useState } from 'react';
 import "./styles/globals.scss";
 import "./styles/markdown.scss";
 import "./styles/highlight.scss";
@@ -22,6 +22,38 @@ export const metadata = {
   },
 };
 
+const AccessOverlay = ({ children }) => {
+  const [password, setPassword] = useState("");
+  const [accessGranted, setAccessGranted] = useState(false);
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handlePasswordSubmit = (event) => {
+    event.preventDefault();
+    if (password === "admin") {
+      setAccessGranted(true);
+    } else {
+      alert("Wrong password. Please try again.");
+      setPassword("");
+    }
+  };
+
+  if (accessGranted) {
+    return children;
+  } else {
+    return (
+      <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.5)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <form onSubmit={handlePasswordSubmit}>
+          <input type="password" value={password} onChange={handlePasswordChange} placeholder="Enter password" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -34,7 +66,9 @@ export default function RootLayout({
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
       </head>
-      <body>{children}</body>
+      <body>
+        <AccessOverlay>{children}</AccessOverlay>
+      </body>
     </html>
   );
 }
