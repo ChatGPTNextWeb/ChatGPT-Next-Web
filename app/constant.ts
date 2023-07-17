d9a0e7b59e9126afcc1c8aede1cdaf61ec679bcd
crypticatul <88734298+crypticatul@users.noreply.github.com>
2023-07-17 19:57:04 +0530

    Update constant.ts Crypticatul

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,5 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
+export const OWNER = "crypticatul";
+export const REPO = "Chat-Cognitiev";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
