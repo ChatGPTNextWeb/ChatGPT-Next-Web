8592efa39e88b199ad7987b9b6d17d8583794529
Apple fish <247580004@qq.com>
2023-07-19 14:19:41 +0800

    Fix UI

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,10 +118,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          AI 助手
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Enjoy!
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
