d2e4cbbfb9dc5151bdbbcd427bc3ed2fea903815
JJTools <125969063+JJTools@users.noreply.github.com>
2023-07-20 11:30:56 +0800

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -36,7 +36,7 @@ export const getServerSideConfig = () => {
       "[Server Config] you are importing a nodejs-only module outside of nodejs",
     );
   }
-  const apikeys = (process.env.OPENAI_API_KEY ?? ).split(',')
+  const apikeys = (process.env.OPENAI_API_KEY ?? '').split(',')
   const apikey = apikeys.at(Math.floor(Math.random() * apikeys.length)) ?? ''
   return {
     apikey,
