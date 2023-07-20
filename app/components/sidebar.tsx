d796dae17063dffc5803cdd5c07737dc7f39b561
tangshengjie <tang.shengjie@zlingsmart.com>
2023-07-20 13:24:35 +0800

    modify error

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
           YueLink ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          "免费3.5版本ChatGPT"
+          YueLink ChatGPT
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
