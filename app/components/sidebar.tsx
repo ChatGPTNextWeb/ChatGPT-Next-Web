013c247616d92a8cb9a06acb6dcac3626334652a
YIMING LIU <46246070+Think2twice@users.noreply.github.com>
2023-07-21 15:44:19 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,7 +118,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          Lucid Home
         </div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
