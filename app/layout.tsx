e2e9eb1ebe3687ba5b23a60d183e1c6ecb587284
wcoee <132860485+wcoee@users.noreply.github.com>
2023-07-22 19:44:38 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT",
+  description: "xvua.xvua.chat",
   viewport: {
     width: "device-width",
     initialScale: 1,
