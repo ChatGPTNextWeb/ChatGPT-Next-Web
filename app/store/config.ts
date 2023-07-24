b9f8d13eb81f517c0528fbb31bfe6ed3c92faa2f
YaKun <liuyk@eagersoft.cn>
2023-07-24 17:22:13 +0800

    设置默认提交按钮为Ctrl

--- app/store/config.ts
+++ app/store/config.ts
@@ -21,7 +21,7 @@ export enum Theme {
 }
 
 export const DEFAULT_CONFIG = {
-  submitKey: SubmitKey.CtrlEnter as SubmitKey,
+  submitKey: SubmitKey.Enter as SubmitKey,
   avatar: "1f603",
   fontSize: 14,
   theme: Theme.Auto as Theme,
