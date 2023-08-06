0089ab4c5c36b1f2b72f5661f1c6540c82f5f235
YIMING LIU <46246070+Think2twice@users.noreply.github.com>
2023-08-06 23:58:01 +0800

    Update sidebar.tsx
    反卷总署

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,7 +118,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          Lucid Home
+          反 卷 总 署
         </div>
         <div className={styles["sidebar-sub-title"]}>
           反卷总署,让别人无路可卷
