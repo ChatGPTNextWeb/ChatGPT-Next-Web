263dc458248ec9a605024ab0248eefc948963bb6
TrymVei <tve@variant.no>
2023-08-18 09:09:54 +0200

    feat: Cards are now clickable links
    (cherry picked from commit 2ed2d6a77a2d7b19013d80f2d98967878a225b3b)

--- app/components/employeeCard.tsx
+++ app/components/employeeCard.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import styles from "./employeeCard.module.scss";
+import Link from "next/link";
 
 export default function EmployeeCard(props: {
   employeeName: string;
   employeeRole: string;
   employeeImageSrc: string;
+  href: string;
 }) {
   return (
-    <div className={styles["employeeCard"]}>
-      <div>
-        <h2>{props.employeeName}</h2>
-        <p>{props.employeeRole}</p>
+    <Link className={styles["link"]} href={props.href}>
+      <div className={styles["employeeCard"]}>
+        <div>
+          <h2>{props.employeeName}</h2>
+          <p>{props.employeeRole}</p>
+        </div>
+        <img
+          src={props.employeeImageSrc}
+          alt={`bilde av ${props.employeeName}`}
+        />
       </div>
-      <img
-        src={props.employeeImageSrc}
-        alt={`bilde av ${props.employeeName}`}
-      />
-    </div>
+    </Link>
   );
 }
