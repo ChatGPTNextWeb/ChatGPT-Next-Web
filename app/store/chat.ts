d29b053ee58613383957ee506e6a37b53811453d
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-08-18 15:24:04 +0200

    gpt-3.5-turbo-16k for chat summaries

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -501,7 +501,7 @@ export const useChatStore = create<ChatStore>()(
           api.llm.chat({
             messages: topicMessages,
             config: {
-              model: "gpt-3.5-turbo",
+              model: "gpt-3.5-turbo-16k",
             },
             onFinish(message) {
               get().updateCurrentSession(
