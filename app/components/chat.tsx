8430cbb25da9ef75104f28fef0dfdb66c8ebeb8d
seiry <4397354+seiry@users.noreply.github.com>
2023-08-20 15:19:20 +0800

    fix _

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -599,7 +599,7 @@ export function EditMessageModal(props: { onClose: () => void }) {
   );
 }
 
-function _Chat() {
+function OriginChat() {
   type RenderMessage = ChatMessage & { preview?: boolean };
 
   const chatStore = useChatStore();
@@ -1304,5 +1304,5 @@ function _Chat() {
 export function Chat() {
   const chatStore = useChatStore();
   const sessionIndex = chatStore.currentSessionIndex;
-  return <_Chat key={sessionIndex}></_Chat>;
+  return <OriginChat key={sessionIndex}></OriginChat>;
 }
