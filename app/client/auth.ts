054c9d61e0ed130fb317d731d4f072156a79c39c
seiry <4397354+seiry@users.noreply.github.com>
2023-08-27 17:36:32 +0800

    gotoLogin

--- app/client/auth.ts
+++ app/client/auth.ts
@@ -1,10 +1,14 @@
-'use client'
+"use client";
 
-import {parse} from 'cookie'
-export const useJWTCookieAuthCheck = ()=>{
-  if(!document?.cookie) return false
-  const cookies = parse(document.cookie)
-  if(!cookies.CF_Authorization){
-    location.href = '/api/config'
+import { parse } from "cookie";
+export const useJWTCookieAuthCheck = () => {
+  if (!document?.cookie) return false;
+  const cookies = parse(document.cookie);
+  if (!cookies.CF_Authorization) {
+    gotoLogin()
   }
-}
+};
+
+export const gotoLogin = () => {
+  location.href = "/api/config";
+};
