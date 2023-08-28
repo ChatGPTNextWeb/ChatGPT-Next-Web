4182b1cc3aa390959a844da429fed4e12cd5afba
seiry <4397354+seiry@users.noreply.github.com>
2023-08-29 00:35:42 +0800

    add  a login backdoor

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -28,6 +28,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useMobileScreen } from "../utils";
 import dynamic from "next/dynamic";
 import { showConfirm, showToast } from "./ui-lib";
+import { gotoLogin } from "../client/auth";
 
 const ChatList = dynamic(async () => (await import("./chat-list")).ChatList, {
   loading: () => null,
@@ -120,7 +121,7 @@ export function SideBar(props: { className?: string }) {
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
           ChatGPT Next
         </div>
-        <div className={styles["sidebar-sub-title"]}>
+        <div className={styles["sidebar-sub-title"]} onClick={gotoLogin}>
           Build your own AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
