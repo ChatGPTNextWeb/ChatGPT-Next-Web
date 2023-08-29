44e1f1a6f1b56d45b1d9dbd0fdeee06203bca5be
hymhao <141479619+hymhao@users.noreply.github.com>
2023-08-29 14:41:39 +0800

    Update sidebar.tsx
    http://www.ggem666.com

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -174,7 +174,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
+            <a href={http://www.ggem666.com} target="_blank" rel="noopener noreferrer">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
