0e3ffe910c358e1cf73b6439d22e696582b67abc
woshizyc <48084908+woshizyc@users.noreply.github.com>
2023-09-02 10:22:17 +0800

    Update sidebar.tsx
    修改名称

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,10 +117,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          小橙子
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          就是一个小橙子.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
