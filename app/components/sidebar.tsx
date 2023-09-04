4a5b43a983929ef574e4f989a3fb379d4e3dedc5
Clifton Hills <qq1010905238@163.com>
2023-09-04 21:44:18 +0800

    Update sidebar.tsx
    个性化

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,10 +117,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          lsl ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          lsl专版,请节省使用哦,带宽有限.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
