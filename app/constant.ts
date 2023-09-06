151e1b4be1fd9e8737f5cf28221c1cb7c5654bef
gedune <hxz707950194@gmail.com>
2023-09-06 17:32:10 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -72,44 +72,16 @@ export const DEFAULT_MODELS = [
     available: true,
   },
   {
-    name: "gpt-4-0314",
-    available: false,
-  },
-  {
-    name: "gpt-4-0613",
-    available: false,
-  },
-  {
     name: "gpt-4-32k",
     available: true,
-  },
-  {
-    name: "gpt-4-32k-0314",
-    available: false,
-  },
-  {
-    name: "gpt-4-32k-0613",
-    available: false,
   },
   {
     name: "gpt-3.5-turbo",
     available: true,
   },
   {
-    name: "gpt-3.5-turbo-0301",
-    available: false,
-  },
-  {
-    name: "gpt-3.5-turbo-0613",
-    available: false,
-  },
-  {
     name: "gpt-3.5-turbo-16k",
     available: true,
-  },
-  {
-    name: "gpt-3.5-turbo-16k-0613",
-    available: false,
   },
 ] as const;
 
