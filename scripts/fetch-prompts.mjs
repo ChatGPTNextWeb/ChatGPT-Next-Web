086f7ce72fbbd8472559942dcd4b3460a134c582
姚嘉豪 <109867712+jaieler@users.noreply.github.com>
2023-09-08 15:21:55 +0800

    Update fetch-prompts.mjs

--- scripts/fetch-prompts.mjs
+++ scripts/fetch-prompts.mjs
@@ -55,7 +55,7 @@ async function fetchEN() {
           .filter((v) => v[0] && v[1]),
       );
   } catch (error) {
-    console.error("[Fetch] failed to fetch en prompts", error);
+    console.error("String contains non ISO-8859-1 code point.", error);
     return [];
   }
 }
