05231a70136188b891e024fee505ae6581b00721
weq wqeqwe <2631599879@qq.com>
2023-09-08 10:43:59 +0000

    modified:   app/masks/cn.ts 	modified:   app/masks/en.ts 	modified:   app/store/config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -45,7 +45,7 @@ export const DEFAULT_CONFIG = {
     presence_penalty: 0,
     frequency_penalty: 0,
     sendMemory: true,
-    historyMessageCount: 4,
+    historyMessageCount: 0,
     compressMessageLengthThreshold: 1000,
     enableInjectSystemPrompts: true,
     template: DEFAULT_INPUT_TEMPLATE,
