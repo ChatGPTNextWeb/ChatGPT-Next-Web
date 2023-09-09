6640dc5fb77fd3abb7f1d1d2cc9efdd7e657b3f4
B0zal <me@btz.pm>
2023-09-09 19:34:33 +0700

    [+] Enhancements to Commit Message Handling and Co-Author
    - Enhanced the `getBuildConfig()` function to support multiple co-authors in commit messages.
    - Introduced an array called `coAuthors` to store multiple co-authors if present.
    - Split the `coAuthorLine` by "<" to separate multiple co-authors and assign them to the `coAuthors` array.
    - Updated the `coAuthored` variable to check if the `coAuthors` array has any elements, indicating the presence of co-authors.
    - Modified the code to store each line of the commit message as a separate entry in the `messages` array, regardless of the presence of co-authors.
    - Excluded empty lines from the `messages` array to ensure only non-empty lines are stored.
    - Updated the `commitMessageObj` to include the `Co-Authored-By` property with the `coAuthors` array if co-authors are present.

--- app/config/build.ts
+++ app/config/build.ts
@@ -22,13 +22,58 @@ export const getBuildConfig = () => {
         .execSync('git log --pretty=format:"%H" -n 1')
         .toString()
         .trim();
+      const commitMessage: string = childProcess
+        .execSync('git log --pretty=format:"%B" -n 1')
+        .toString()
+        .trim();
+      const Author: string = childProcess
+        .execSync('git log --pretty=format:"%an" -n 1')
+        .toString()
+        .trim();
+      const coAuthorLine: string = childProcess
+        .execSync('git log --format="%h %(trailers:key=Co-authored-by)" -n 1')
+        .toString()
+        .trim();
+      const coAuthorMatch: RegExpMatchArray | null = coAuthorLine.match(
+        /Co-Authored-By:\s*(.*)/,
+      );
+      const coAuthors: string[] = coAuthorMatch
+        ? coAuthorMatch[1].trim().split("<").map(author => author.trim())
+        : [];
 
-      return { commitDate, commitHash };
+      const coAuthored: boolean = coAuthors.length > 0; // if there are co-authors, set to true
+
+      if (!commitMessage) {
+        console.warn("[Build Config] No commit message available.");
+      }
+
+      const [title, ...messages] = commitMessage.split("\n");
+
+      const uniqueMessages = messages
+        .filter((message) => !message.startsWith("Co-Authored-By:"))
+        .filter((message) => message.trim() !== ""); // Exclude empty lines
+
+      const commitMessageObj = {
+        title: title || "No title",
+        messages: uniqueMessages.length > 0 ? uniqueMessages : undefined,
+        "Co-Authored-By": coAuthors.length > 0 ? coAuthors : undefined,
+      };
+
+      return {
+        commitDate,
+        commitHash,
+        commitMessage: commitMessageObj,
+        Author,
+        coAuthored,
+      };
     } catch (e) {
       console.error("[Build Config] No git or not from git repo.");
       return {
         commitDate: "unknown",
         commitHash: "unknown",
+        commitMessage: { title: "unknown", messages: undefined, "Co-Authored-By": undefined },
+        Author: "unknown",
+        coAuthored: false,
       };
     }
   })();
