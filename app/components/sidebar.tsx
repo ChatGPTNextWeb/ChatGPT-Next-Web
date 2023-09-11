91f4cb87389ee4a2b17c1ec3af90188cedb1b565
smileinwind deng <smileinwind@foxmail.com>
2023-09-11 10:16:27 +0000

    sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT By DengSuper
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          知者不惑，仁者不忧，勇者不惧。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
