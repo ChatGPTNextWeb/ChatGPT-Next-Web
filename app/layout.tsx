83b65d10f1441ed80665faf7e59e0aba33b359d0
smileinwind deng <smileinwind@foxmail.com>
2023-09-11 10:10:39 +0000

    myself

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT By DengSuper",
+  description: "知者不惑，仁者不忧，勇者不惧。",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ChatGPT By DengSuper",
     statusBarStyle: "default",
   },
 };
