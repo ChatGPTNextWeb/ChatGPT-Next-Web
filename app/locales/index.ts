6e9491fd85af60568bf7968fea9dba2e7cf6a2a9
yjding <yjding@live.cn>
2023-09-24 17:21:28 -0400

    Update index.ts

--- app/locales/index.ts
+++ app/locales/index.ts
@@ -65,9 +65,9 @@ export const ALL_LANG_OPTIONS: Record<Lang, string> = {
 };
 
 const LANG_KEY = "lang";
-const DEFAULT_LANG = "en";
+const DEFAULT_LANG = "cn";
 
-const fallbackLang = en;
+const fallbackLang = cn;
 const targetLang = ALL_LANGS[getLang()] as LocaleType;
 
 // if target lang missing some fields, it will use fallback lang string
