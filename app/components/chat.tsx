6a61fe57763e96e3725566543d2f1fc5222ce69e
Algorithm5838 <108630393+Algorithm5838@users.noreply.github.com>
2023-09-26 20:45:09 +0300

    Update chat.tsx

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1155,7 +1155,13 @@ function _Chat() {
                       {isUser ? (
                         <Avatar avatar={config.avatar} />
                       ) : (
-                        <MaskAvatar mask={session.mask} />
+                        <>
+                          {["system", "context"].includes(message.role) ? (
+                            <Avatar avatar="2699-fe0f" />
+                          ) : (
+                            <MaskAvatar mask={session.mask} />
+                          )}
+                        </>
                       )}
                     </div>
 
