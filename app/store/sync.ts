6f6e417e51a43c4895d2a87a5f949c01b5cec3bd
H0llyW00dzZ <priv8@btz.pm>
2023-09-29 01:27:40 +0700

    CloudSync [Module Redist]
    [+] chore(sync.ts): update redist object properties in DEFAULT_SYNC_STATE
    
    - Change username property to port
    - Change apiKey property to username
    - Add key property with value of STORAGE_KEY
    - Add password property with empty string value

--- app/store/sync.ts
+++ app/store/sync.ts
@@ -41,8 +41,10 @@ const DEFAULT_SYNC_STATE = {
 
   redist: {
     endpoint: "",
-    username: STORAGE_KEY,
-    apiKey: "",
+    port: "",
+    username: "",
+    key: STORAGE_KEY,
+    password: "",
   },
 
   lastSyncTime: 0,
