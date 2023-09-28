5f011b2e458fc0411f0a9cdf653787c89908b2cb
H0llyW00dzZ <priv8@btz.pm>
2023-09-29 01:56:03 +0700

    CloudSync [Module Redist]
    [+] fix(redist.ts): fix variable name from `config.username` to `config.key`
    [+] feat(redist.ts): add support for Redis connection using `ioredis` library

--- app/utils/cloud/redist.ts
+++ app/utils/cloud/redist.ts
@@ -1,58 +1,57 @@
 import { STORAGE_KEY } from "@/app/constant";
 import { SyncStore } from "@/app/store/sync";
-import { corsFetch } from "../cors";
-import { chunks } from "../format";
+import Redis, { RedisOptions } from 'ioredis';
 
 export type RedistConfig = SyncStore["redist"];
 export type RedistClient = ReturnType<typeof createRedistClient>;
 
 export function createRedistClient(store: SyncStore) {
   const config = store.redist;
-  const storeKey = config.username.length === 0 ? STORAGE_KEY : config.username;
+  const storeKey = config.key.length === 0 ? STORAGE_KEY : config.key;
   const chunkCountKey = `${storeKey}-chunk-count`;
   const chunkIndexKey = (i: number) => `${storeKey}-chunk-${i}`;
 
-  const proxyUrl =
-    store.useProxy && store.proxyUrl.length > 0 ? store.proxyUrl : undefined;
+  const redisOptions: RedisOptions = {
+    host: config.endpoint,
+    port: config.port,
+    password: config.password,
+    tls: {
+      rejectUnauthorized: false, // Disable certificate verification
+    },
+  };
+
+  const redisClient = new Redis(redisOptions);
 
   return {
     async check() {
       try {
-        const res = await corsFetch(this.path(`get/${storeKey}`), {
-          method: "GET",
-          headers: this.headers(),
-          proxyUrl,
-        });
-        console.log("[Redist] check", res.status, res.statusText);
-        return [200].includes(res.status);
+        const result = await redisClient.ping();
+        console.log('[Redist] check', result);
+        return true;
       } catch (e) {
-        console.error("[Redist] failed to check", e);
+        console.error('[Redist] failed to check', e);
+        return false;
       }
-      return false;
     },
 
     async redisGet(key: string) {
-      const res = await corsFetch(this.path(`get/${key}`), {
-        method: "GET",
-        headers: this.headers(),
-        proxyUrl,
-      });
-
-      console.log("[Redist] get key =", key, res.status, res.statusText);
-      const resJson = (await res.json()) as { result: string };
-
-      return resJson.result;
+      try {
+        const result = await redisClient.get(key);
+        console.log('[Redist] get key =', key, result);
+        return result;
+      } catch (e) {
+        console.error('[Redist] failed to get key =', key, e);
+        return null;
+      }
     },
 
     async redisSet(key: string, value: string) {
-      const res = await corsFetch(this.path(`set/${key}`), {
-        method: "POST",
-        headers: this.headers(),
-        body: value,
-        proxyUrl,
-      });
-
-      console.log("[Redist] set key =", key, res.status, res.statusText);
+      try {
+        await redisClient.set(key, value);
+        console.log('[Redist] set key =', key);
+      } catch (e) {
+        console.error('[Redist] failed to set key =', key, e);
+      }
     },
 
     async get() {
@@ -60,41 +59,23 @@ export function createRedistClient(store: SyncStore) {
       if (!Number.isInteger(chunkCount)) return;
 
       const chunks = await Promise.all(
-        new Array(chunkCount)
-          .fill(0)
-          .map((_, i) => this.redisGet(chunkIndexKey(i)))
+        new Array(chunkCount).fill(0).map((_, i) => this.redisGet(chunkIndexKey(i)))
       );
-      console.log("[Redist] get full chunks", chunks);
-      return chunks.join("");
+      console.log('[Redist] get full chunks', chunks);
+      return chunks.join('');
     },
 
     async set(_: string, value: string) {
-
+      const chunkSize = 100000; // Adjust the chunk size as needed
+      const chunks = value.match(new RegExp(`.{1,${chunkSize}}`, 'g')) || [];
       let index = 0;
-      for await (const chunk of chunks(value)) {
+
+      for await (const chunk of chunks) {
         await this.redisSet(chunkIndexKey(index), chunk);
         index += 1;
       }
+
       await this.redisSet(chunkCountKey, index.toString());
-    },
-
-    headers() {
-      return {
-        Authorization: `Bearer ${config.apiKey}`,
-      };
-    },
-    path(path: string) {
-      let url = config.endpoint;
-
-      if (!url.endsWith("/")) {
-        url += "/";
-      }
-
-      if (path.startsWith("/")) {
-        path = path.slice(1);
-      }
-
-      return url + path;
     },
   };
 }
