c6930e1149fc86f47edf88d8e437972a3607a852
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-10-02 17:15:43 +0200

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -55,7 +55,7 @@ export const UNFINISHED_INPUT = (id: string) => "unfinished-input-" + id;
 
 export const STORAGE_KEY = "chatgpt-next-web";
 
-export const REQUEST_TIMEOUT_MS = 60000;
+export const REQUEST_TIMEOUT_MS = 80000;
 
 export const EXPORT_MESSAGE_CLASS_NAME = "export-markdown";
 
@@ -68,7 +68,7 @@ export const OpenaiPath = {
 
 export const DEFAULT_INPUT_TEMPLATE = `{{input}}`; // input / time / model / lang
 export const DEFAULT_SYSTEM_TEMPLATE = `
-You are ChatGPT, a large language model trained by OpenAI.
+You are GPT, a large language model (LLM) trained by OpenAI.
 Knowledge cutoff: 2021-09
 Current model: {{model}}
 Current time: {{time}}`;
