1505372e204d8363ec9c67a61d004806de81c200
KeithHello <hongji.li@act1.co.jp>
2023-10-04 18:08:29 +0900

    Change log
    -   config.ts
        -   line 72: remove type confirmation of x as x always has type 'number'
        -   line 135: remove the redundant local variable
    -   chat.ts
        -   delete the unused import

--- app/store/config.ts
+++ app/store/config.ts
@@ -46,13 +46,13 @@ export const DEFAULT_CONFIG = {
 
   modelConfig: {
     model: "gpt-3.5-turbo" as ModelType,
-    temperature: 0.6,
+    temperature: 0.5,
     top_p: 1,
     max_tokens: 2000,
     presence_penalty: 0,
     frequency_penalty: 0,
     sendMemory: true,
-    historyMessageCount: 1,
+    historyMessageCount: 4,
     compressMessageLengthThreshold: 1000,
     enableInjectSystemPrompts: true,
     template: DEFAULT_INPUT_TEMPLATE,
