a11f1a6f2277ec1ca9b3557b98a057560d4fb802
julian-hu <55130144+julian-hu@users.noreply.github.com>
2023-10-10 21:05:17 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -310,7 +310,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "当前可用模型，gpt-3.5-turbo，gpt-3.5-turbo-16k，gpt-4，gpt-4-32k",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
