1ab56c7ea232fe6a07b5277997abe341ab6e4e2a
Tanishka Garg <115347881+gargtanishka895@users.noreply.github.com>
2023-10-16 11:50:10 +0530

    Update error.tsx
    In this updated code: 
    
    Errors are logged to a server or service using the logErrorToServer function (you should implement this function to suit your needs).
    The page is only refreshed if the user explicitly clicks the "Refresh Page" button, giving them more control.
    A user-friendly message is displayed in case of an error.
    
    i want to make a note to make you remind that implement the logErrorToServer function to log errors to a backend service, as it's not included in the code.

--- app/components/error.tsx
+++ app/components/error.tsx
@@ -6,67 +6,47 @@ import { ISSUE_URL } from "../constant";
 import Locale from "../locales";
 import { showConfirm } from "./ui-lib";
 import { useSyncStore } from "../store/sync";
+import { logErrorToServer } from "./logging"; // Add a logging utility
 
 interface IErrorBoundaryState {
   hasError: boolean;
-  error: Error | null;
-  info: React.ErrorInfo | null;
 }
 
 export class ErrorBoundary extends React.Component<any, IErrorBoundaryState> {
   constructor(props: any) {
     super(props);
-    this.state = { hasError: false, error: null, info: null };
+    this.state = { hasError: false };
   }
 
-  componentDidCatch(error: Error, info: React.ErrorInfo) {
-    // Update state with error details
-    this.setState({ hasError: true, error, info });
-  }
+  componentDidCatch(error: Error) {
+    // Log the error to a server or service
+    logErrorToServer(error);
 
-  clearAndSaveData() {
-    try {
-      useSyncStore.getState().export();
-    } finally {
-      localStorage.clear();
-      location.reload();
-    }
+    // Set the state to indicate that an error occurred
+    this.setState({ hasError: true });
   }
 
   render() {
     if (this.state.hasError) {
-      // Render error message
+      // Render an error message and options
       return (
         <div className="error">
           <h2>Oops, something went wrong!</h2>
-          <pre>
-            <code>{this.state.error?.toString()}</code>
-            <code>{this.state.info?.componentStack}</code>
-          </pre>
-
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <a href={ISSUE_URL} className="report">
-              <IconButton
-                text="Report This Error"
-                icon={<GithubIcon />}
-                bordered
-              />
-            </a>
-            <IconButton
-              icon={<ResetIcon />}
-              text="Clear All Data"
-              onClick={async () => {
-                if (await showConfirm(Locale.Settings.Danger.Reset.Confirm)) {
-                  this.clearAndSaveData();
-                }
-              }}
-              bordered
-            />
-          </div>
+          <p>We apologize for the inconvenience. Please try refreshing the page.</p>
+          <a href={ISSUE_URL} className="report">
+            <IconButton text="Report This Error" icon={<GithubIcon />} bordered />
+          </a>
+          <IconButton
+            icon={<ResetIcon />}
+            text="Refresh Page"
+            onClick={() => window.location.reload()}
+            bordered
+          />
         </div>
       );
     }
-    // if no error occurred, render children
+
+    // If no error occurred, render children
     return this.props.children;
   }
 }
