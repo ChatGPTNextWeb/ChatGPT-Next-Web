ae8d00322ea99b517f4c129e48c863ec34025881
sky1988102 <130644826+sky1988102@users.noreply.github.com>
2023-10-18 14:28:28 +0800

    Update sidebar.tsx
    修改侧边栏副标题

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -148,7 +148,7 @@ export function SideBar(props: { className?: string }) {
           AI助手GPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          浩雀优选微信公众号左下角菜单栏购买Key使用.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
