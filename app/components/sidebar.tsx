814f07f56fed9a744215ae82360b8aa6f9739d8a
Ben11029 <107007336+Ben11029@users.noreply.github.com>
2023-10-22 14:06:28 -0700

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -148,7 +148,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT Next
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          天心海月🌕，真幻了无🌝！
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
