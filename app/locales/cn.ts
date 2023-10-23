dc3ac3e958b4939199126917c71acec3240fa5e9
Ben11029 <107007336+Ben11029@users.noreply.github.com>
2023-10-23 01:03:52 -0700

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -310,7 +310,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "主子，请发话！",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
