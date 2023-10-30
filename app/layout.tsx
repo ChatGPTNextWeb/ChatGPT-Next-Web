4fa53e6eac1f4212bcaf516ffd90b9215f7ab552
LeeMonths <liwenfu@months.top>
2023-10-30 22:58:24 +0800

    Update layout.tsx 20231030

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Month ChatGPT",
+  description: "months.top GPT",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Month ChatGPT",
     statusBarStyle: "default",
   },
 };
