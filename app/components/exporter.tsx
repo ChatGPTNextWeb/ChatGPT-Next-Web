d66dd35863aeaf524fb3af92db1a1a81a1af910c
LeeMonths <liwenfu@months.top>
2023-10-30 23:05:38 +0800

    Update exporter.tsx 20231030

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -514,9 +514,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>Month ChatGPT</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              github.com/lywnko/ChatGPT-Next-Web
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
