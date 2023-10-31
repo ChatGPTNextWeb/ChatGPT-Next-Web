f89b8032c82047f27bc0eb84eedc4e5fe9647b5a
Yu <951379969@qq.com>
2023-10-31 17:33:45 +0800

    modified:   app/components/sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -205,12 +205,7 @@ export function SideBar(props: { className?: string }) {
             <Link to={Path.Settings}>
               <IconButton icon={<SettingsIcon />} shadow />
             </Link>
-          </div>
-          <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank">
-              <IconButton icon={<GithubIcon />} shadow />
-            </a>
-          </div> */}
+          </div> 
         </div>
         <div>
           <IconButton
