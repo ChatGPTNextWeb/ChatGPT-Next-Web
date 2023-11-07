46008069b1327dcfb88bf281d16ccc770cde25fe
Zeeland <zeeland4work@gmail.com>
2023-11-07 17:12:09 +0000

    feat: enlarge max tokens for gpt4-turbo

--- app/components/model-config.tsx
+++ app/components/model-config.tsx
@@ -76,7 +76,7 @@ export function ModelConfigList(props: {
         <input
           type="number"
           min={100}
-          max={100000}
+          max={1000000}
           value={props.modelConfig.max_tokens}
           onChange={(e) =>
             props.updateConfig(
