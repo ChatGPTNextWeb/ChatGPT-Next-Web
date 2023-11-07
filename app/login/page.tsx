c77bedf43273e9d5947d9061ab6e7e180eeda24b
JuliusMoehring <44407680+JuliusMoehring@users.noreply.github.com>
2023-11-07 18:44:30 +0100

    Add auto login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,45 +1,22 @@
 "use client";
 
 import { signIn, useSession } from "next-auth/react";
-import Link from "next/link";
-import { IconButton } from "../components/button";
 
-import styles from "./login.module.scss";
+import { useEffect } from "react";
+import { Loading } from "../components/home";
 
 export default function Login() {
   const session = useSession();
 
-  const handleLogin = async () => {
-    signIn("azure-ad", {
-      callbackUrl: "/",
-    });
-  };
+  useEffect(() => {
+    const handleLogin = async () => {
+      signIn("azure-ad", {
+        callbackUrl: "/",
+      });
+    };
 
-  return (
-    <div className={styles["container"]}>
-      <h1 className={styles["headline"]}>Welcome to AdEx GPT</h1>
+    handleLogin();
+  }, []);
 
-      <p className={styles["subline"]}>
-        Before you can start using AdEx GPT you have to sign in using your AdEx
-        account. After a successful sign in you will be redirected to the chat.
-      </p>
-
-      {session.status === "authenticated" ? (
-        <Link href="/" className={styles["sign-in-button"]}>
-          <IconButton
-            text="Go to Chat"
-            type="primary"
-            className={styles["sign-in-button"]}
-          />
-        </Link>
-      ) : (
-        <IconButton
-          text="Login"
-          type="primary"
-          className={styles["sign-in-button"]}
-          onClick={handleLogin}
-        />
-      )}
-    </div>
-  );
+  return <Loading />;
 }
