64e894512249c171ab82da6c7ea973039fd7f494
Buer <42402987+MartialBE@users.noreply.github.com>
2023-11-08 18:33:45 +0800

    🐛 fix azure openai 'choices' parameter is empty (#60)

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -162,7 +162,7 @@ export class ChatGPTApi implements LLMApi {
             const text = msg.data;
             try {
               const json = JSON.parse(text);
-              const delta = json.choices[0].delta.content;
+              const delta = json.choices[0]?.delta.content;
               if (delta) {
                 responseText += delta;
                 options.onUpdate?.(responseText, delta);
