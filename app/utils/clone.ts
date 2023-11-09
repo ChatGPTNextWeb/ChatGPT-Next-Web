b7ffca031ebda555c373783820056541307ceba0
Yidadaa <yidadaa@qq.com>
2023-11-10 02:43:30 +0800

    feat: close #935 add azure support

--- app/utils/clone.ts
+++ app/utils/clone.ts
@@ -1,3 +1,10 @@
 export function deepClone<T>(obj: T) {
   return JSON.parse(JSON.stringify(obj));
 }
+
+export function ensure<T extends object>(
+  obj: T,
+  keys: Array<[keyof T][number]>,
+) {
+  return keys.every((k) => obj[k] !== undefined && obj[k] !== null);
+}
