c9b62875f19671342957c5aeaf0d22beea15f3b4
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-11-09 12:22:53 +0100

    gpt-4

--- app/store/config.ts
+++ app/store/config.ts
@@ -46,7 +46,7 @@ export const DEFAULT_CONFIG = {
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
-    model: "gpt-4-1106-preview" as ModelType,
+    model: "gpt-4" as ModelType,
     temperature: 0.5,
     top_p: 1,
     max_tokens: 8000,
