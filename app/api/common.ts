e4b0c481208fb96655e2f4bb601532ad9ff987f9
akira-cn <akira.cn@gmail.com>
2023-11-10 20:38:53 +0800

    fix: remove the content-encoding header

--- app/api/common.ts
+++ app/api/common.ts
@@ -106,6 +106,12 @@ export async function requestOpenai(req: NextRequest) {
     // to disable nginx buffering
     newHeaders.set("X-Accel-Buffering", "no");
 
+    // The latest version of the OpenAI API forced the content-encoding to be "br" in json response
+    // So if the streaming is disabled, we need to remove the content-encoding header
+    // But vercel uses gzip to compress the response
+    // So we need to remove the content-encoding header
+    newHeaders.delete("content-encoding");
+
     return new Response(res.body, {
       status: res.status,
       statusText: res.statusText,
