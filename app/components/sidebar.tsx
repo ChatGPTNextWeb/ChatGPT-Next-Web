965b722c48e03d24249e18dad88e62281dceaf10
ChenJinShui <42457310+ChenJinShui@users.noreply.github.com>
2023-11-13 22:08:10 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          AI不会颠覆人,但会替代工具人
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          从来不是AI替代人,而是会使用AI的人替代不会使用AI的人
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
@@ -217,7 +217,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
+            <a href="http://chenjinshui.top" target="_blank" rel="noopener noreferrer">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
