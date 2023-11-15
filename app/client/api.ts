49be48d6e40ebb164da5cde5336ab8d01d8c15ea
djkko <39113015+djkko@users.noreply.github.com>
2023-11-15 10:58:50 +0800

    修改分享 和提示信息

--- app/client/api.ts
+++ app/client/api.ts
@@ -88,14 +88,14 @@ export class ClientApi {
       .map((m) => ({
         from: m.role === "user" ? "human" : "gpt",
         value: m.content,
-      }))
-      .concat([
-        {
-          from: "human",
-          value:
-            "Share from [ChatGPT Next Web]: https://github.com/Yidadaa/ChatGPT-Next-Web",
-        },
-      ]);
+      }));
+      // .concat([
+      //   {
+      //     from: "human",
+      //     value:
+      //       "Share from [ChatGPT Next Web]: https://github.com/Yidadaa/ChatGPT-Next-Web",
+      //   },
+      // ]);
     // 敬告二开开发者们，为了开源大模型的发展，请不要修改上述消息，此消息用于后续数据清洗使用
     // Please do not modify this message
 
--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -8,7 +8,7 @@ const cn = {
   Error: {
     Unauthorized: isApp
       ? "检测到无效 API Key，请前往[设置](/#/settings)页检查 API Key 是否配置正确。"
-      : "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码，或者在[设置](/#/settings)页填入你自己的 OpenAI API Key。",
+      : "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码",
   },
   Auth: {
     Title: "需要密码",
