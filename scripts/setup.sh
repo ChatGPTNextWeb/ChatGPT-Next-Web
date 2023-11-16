aa8ec0a9dfb6213622a9f5d770eb740c0946f56d
Hk-Gosuto <yominogosuto@gmail.com>
2023-11-16 12:47:48 +0800

    chore: update project source

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -54,8 +54,8 @@ if ! command -v node >/dev/null || ! command -v git >/dev/null || ! command -v y
 fi
 
 # Clone the repository and install dependencies
-git clone https://github.com/Yidadaa/ChatGPT-Next-Web
-cd ChatGPT-Next-Web
+git clone https://github.com/Hk-Gosuto/ChatGPT-Next-Web-LangChain
+cd ChatGPT-Next-Web-LangChain
 yarn install
 
 # Prompt user for environment variables
