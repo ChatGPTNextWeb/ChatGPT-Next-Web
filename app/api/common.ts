f85ac00012710c3c777eae64cbfdc3f1ce9f6830
Erika <78210559+Eric6286@users.noreply.github.com>
2023-11-19 14:04:39 +0800

    Update common.ts

--- app/api/common.ts
+++ app/api/common.ts
@@ -1,6 +1,6 @@
 import { NextRequest } from "next/server";
 
-export const OPENAI_URL = "https://api.huiyan-ai.com";
+export const OPENAI_URL = "api.openai.com";
 const DEFAULT_PROTOCOL = "https";
 const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
 const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
