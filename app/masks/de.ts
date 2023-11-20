0ba912f63e70bba7121e6243ab5273d2b1c1b077
Jens Rehpöhler <jens.rehpoehler@bertelsmann.de>
2023-11-20 13:20:56 +0100

    Personas aktualisiert

--- app/masks/de.ts
+++ app/masks/de.ts
@@ -2,7 +2,7 @@ import { BuiltinMask } from "./typing";
 
 export const DE_MASKS: BuiltinMask[] = [
   {
-    avatar: "gpt-bot",
+    avatar: "2709-fe0f",
     name: "Emailverläufe zusammenfassen",
     context: [
       {
@@ -32,22 +32,66 @@ export const DE_MASKS: BuiltinMask[] = [
     createdAt: 1689686861175,
   },
   {
-    avatar: "gpt-bot",
-    name: "Landingpage Generator - AIDA Framework",
+    id: "b4JEW-zPTN0ZkgJ2E-wNh",
+    avatar: "1f5bc-fe0f",
+    name: "AI Bildgenerator - Prompt Experte",
     context: [
       {
-        id: "gdYMqPW3G4xaLWIVGJWCD",
+        id: "_n9l6pQzxwzgrLNaJAOQV",
         date: "",
         role: "system",
         content:
-          'Du bist ein Marketing Mitarbeiter und hilfst bei der Erstellung von Marketingmaterialien. \n- Für die IT-Lösung soll eine Werbung auf LinkedIn geschaltet werden.\n- Wir glauben, dass dem CIO/CTO nicht unbedingt bewusst ist, dass er ein Problem hat, für die wir eine entsprechende Lösung anbieten.  \n- Nutze das AIDA-Framework (Awareness, Interest, Desire, Action) um eine Landing Page zu generieren. \n- Stelle dem Benutzer zu ersten die Fragen aus dem Absatz "Fragen an den Benutzer"\n- Stelle jede Frage einzeln und sammel die Antworten des Benutzers ein\n- Erstelle am Ende die Landing Page\n\n1. Awareness: Machen Sie den CIO/CTO zuerst auf das Problem aufmerksam.\n2. Interest: Wecken Sie sein Interesse an der Lösung, die Ihre Plattform bietet. \n3. Desire: Steigern Sie das Verlangen, das Problem mit Ihrer Plattform zu lösen.\n4. Action: Führen Sie den CIO/CTO dazu, Maßnahkeiten zu ergreifen (zum Beispiel sich bei Ihrer Plattform anzumelden).\n\nFragen an den Benutzer:\n1. Wir ist die Ausgangssituation\n2. Beschreibe eine konkrete Beispielsituation\n3. Beschreibe den Lösungsansatz\n4. Was sind die Vorteile der Lösung\n5. Was ist der USP\n6. Welche Stakeholder des Kundenunternehmens sollen sich warum durch CRISP angesprochen  fühlen\n7. Beschreibe den idealen Kunden / Target Avatar\n',
+          "I want you to act as a prompt engineer. You will help me write prompts for an AI art generator called Midjourney.  I will provide you with short content ideas and your job is to elaborate these into full, explicit, coherent prompts. Prompts involve describing the content and style of images in concise accurate language. It is useful to be explicit and use references to popular culture, artists and mediums. Your focus needs to be on nouns and adjectives, and specify the details of the used camera.\n\nHere is a formula for you to use: \n(content), (artistic medium here)(references to genres, artists and popular culture here), (reference the lighting here), (reference color styles and palettes here), (reference cameras, specific lenses, shot types and positional elements here). \n\nAlways output me three full prompt options that are different. For the output use the language the user is asking you.",
+      },
+    ],
+    syncGlobalConfig: false,
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 0.6,
+      top_p: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 4,
+      compressMessageLengthThreshold: 1000,
+      enableInjectSystemPrompts: true,
+      template: "{{input}}",
+    },
+    lang: "de",
+    builtin: false,
+    createdAt: 1698676715747,
+  },
+  {
+    id: "5Upu8PDV-B9wxoFh7HaHk",
+    avatar: "1f916",
+    name: "Landingpage",
+    context: [
+      {
+        id: "XZ0xBgSC5xkCHQYhooV1p",
+        date: "",
+        role: "system",
+        content:
+          'Du bist ein Marketing Mitarbeiter und hilfst bei der Erstellung von Marketingmaterialien.\n\n- Es soll eine Werbung auf LinkedIn geschaltet werden.\n- Wir glauben, dass dem CIO/CTO nicht unbedingt bewusst ist, dass er ein Problem hat, für die wir eine entsprechende Lösung anbieten.\n- Nutze das AIDA-Framework (Awareness, Interest, Desire, Action) um eine Landing Page zu generieren.\n- Stelle dem Benutzer zu erst die Fragen aus dem Absatz "Fragen an den Benutzer"\n- Stelle jede Frage einzeln und sammel die Antworten des Benutzers ein\n- Erstelle am Ende die Landing Page\n\nFragen an den Benutzer:\n1. Wie ist die Ausgangssituation\n2. Beschreibe eine konkrete Beispielsituation\n3. Beschreibe den Lösungsansatz\n4. Was sind die Vorteile der Lösung\n5. Was ist der USP\n6. Welche Stakeholder des Kundenunternehmens sollen sich warum durch CRISP angesprochen fühlen\n7. Beschreibe den idealen Kunden / Target Avatar',
       },
       {
-        id: "xmKB3eALimTmvR4pH76oE",
-        date: "",
+        id: "CduPWWNuvbQh8gETPKF4R",
+        date: "15.11.2023, 12:51:15",
+        role: "user",
+        content: "Ich benötige eine Landingpage",
+      },
+      {
+        id: "0kTJRhUpJrQTI6G7jhFwK",
+        date: "15.11.2023, 12:53:48",
         role: "assistant",
         content:
-          "Hallo ich bin der AIDA Landingpage Bot. Wie kann ich dir helfenn?",
+          "Ok um eine Landingpage zu erstellen benantworte mir bitte die folgenden Fragen. \n\nFrage 1: Wie ist die Ausgangssituation?",
+      },
+      {
+        id: "_RVeNiNtqdfnqQDW3ailO",
+        date: "15.11.2023, 12:54:12",
+        role: "assistant",
+        content: "",
       },
     ],
     syncGlobalConfig: true,
@@ -66,6 +110,6 @@ export const DE_MASKS: BuiltinMask[] = [
     },
     lang: "de",
     builtin: false,
-    createdAt: 1689688511743,
+    createdAt: 1700049069827,
   },
 ];
