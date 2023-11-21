4c9ae67528e3a9400da22c637e92f1c51aaf29b1
羚羊公子 <lygz030110@gmail.com>
2023-11-21 18:10:31 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -213,7 +213,7 @@ export function SideBar(props: { className?: string }) {
           </div>
           <div className={styles["sidebar-action"]}>
             <Link to={Path.Settings}>
-              <IconButton icon={<SettingsIcon />} shadow />设置
+              <IconButton icon={<SettingsIcon />} shadow />
             </Link>
           </div>
 
