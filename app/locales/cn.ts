87854a7d342fb06b3d3c69777f2ab535ef24435a
羚羊公子 <lygz030110@gmail.com>
2023-11-21 17:27:13 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -12,9 +12,9 @@ const cn = {
   },
   Auth: {
     Title: "需要密码访问",
-    Tips: "管理员开启了密码验证，请在下方填入访问码",
+    Tips: "管理员开启了密码验证，请在下方填入邀请码",
     SubTips: "或者输入你购买的 OpenAI API 密钥（APIkey)",
-    Input: "在此处填写访问码",
+    Input: "在此处填写邀请码",
     Confirm: "确认",
     Later: "稍后再说",
   },
