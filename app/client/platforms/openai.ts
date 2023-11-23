ea0e0f8cec308664c3bbf44fe61a158f1f1dece6
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-11-23 13:25:57 +0100

    getServerSession

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -28,27 +28,6 @@ import { incrementAPICallCount } from '../../utils/cloud/redisRestClient';
 
 
 
-
-
-export default async function handler(req, res) {
-  const session = await getServerSession(req, res, authOptions)
-
-  if (!session) {
-    res.status(401).json({ message: "You must be logged in." });
-    return;
-  }
-
-  console.log("session within openai.ts:", session);
-
-  return res.json({
-    message: 'Success',
-  })
-}
-
-
-
-
-
 export interface OpenAIListModelResponse {
   object: string;
   data: Array<{
