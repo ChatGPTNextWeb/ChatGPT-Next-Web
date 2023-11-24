2a7e1490e50fe57bb703834b0057e3ec52ede56a
alittlejuly <73336734+alittlejuly@users.noreply.github.com>
2023-11-25 02:12:30 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -342,7 +342,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "找宇哥干嘛？",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
