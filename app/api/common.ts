f9d916925eec45424bab3592e5eafca69e062a98
H0llyW00dzZ <priv8@btz.pm>
2023-11-25 10:23:16 +0700

    Fixup Api Common [Server Side] [Console Log]
    - [+] fix(common.ts): fix condition to check if serverConfig.openaiOrgId is not undefined

--- app/api/common.ts
+++ app/api/common.ts
@@ -31,7 +31,7 @@ export async function requestOpenai(req: NextRequest) {
   console.log("[Proxy] ", path);
   console.log("[Base Url]", baseUrl);
   // this fix [Org ID] undefined in server side if not using custom point
-  if (serverConfig.openaiOrgId) {
+  if (serverConfig.openaiOrgId !== undefined) {
     console.log("[Org ID]", serverConfig.openaiOrgId);
   }
 
