d920f2b0e4e0d70c92a16f7dd9b30d27ab72babc
yshishenya <yshishenya@gmail.com>
2023-11-30 14:30:59 +0500

    logo

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          Профессионалы 4.0
+         GPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          GPT
+         Профессионалы 4.0
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
@@ -215,11 +215,6 @@ export function SideBar(props: { className?: string }) {
             <Link to={Path.Settings}>
               <IconButton icon={<SettingsIcon />} shadow />
             </Link>
-          </div>
-          <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank" rel="noopener noreferrer">
-              <IconButton icon={<GithubIcon />} shadow />
-            </a>
           </div>
         </div>
         <div>
