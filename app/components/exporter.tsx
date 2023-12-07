d4c3ad8f93def0214ab7d91818a4b638fd33128e
Kyalpha <hostage007@gmail.com>
2023-12-07 15:23:38 +0800

    更新导出的文字描述

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -532,7 +532,7 @@ export function ImagePreviewer(props: {
           <div>
             <div className={styles["main-title"]}>ChatGPT Next Web</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              GPT.Changtsi.com
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
