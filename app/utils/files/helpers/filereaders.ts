6605b7a24b65829fa9410c0de5a0346ed42c7439
iamrazo <bestwishtous@gmail.com>
2023-12-11 23:30:11 +0800

    refactor: Refactor filereaders.ts

--- app/utils/files/helpers/filereaders.ts
+++ app/utils/files/helpers/filereaders.ts
@@ -1,55 +1,68 @@
-const readImageFiles = async (file: File | Blob): Promise<string> => {
-  return new Promise<string>(async (resolve, reject) => {
-    try {
-      const { createWorker } = await import("tesseract.js");
-      const worker = await createWorker("eng+chi_sim");
-      await worker.load();
-      const {
-        data: { text },
-      } = await worker.recognize(file);
-      await worker.terminate();
-      resolve(text);
-    } catch (error) {
-      reject(error);
-    }
+const readFileAsArrayBuffer = (file: File | Blob): Promise<ArrayBuffer> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      resolve(event.target?.result as ArrayBuffer);
+    };
+    reader.onerror = (event) => {
+      reject(event.target?.error);
+    };
+    reader.readAsArrayBuffer(file);
   });
 };
 
-function readWordFile(file: File | Blob): Promise<string> {
-  return new Promise<string>((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = async (event: ProgressEvent<FileReader>) => {
-      const arrayBuffer = event.target?.result as ArrayBuffer;
-      const { default: JSZip } = await import("jszip");
-      const zip = await JSZip.loadAsync(arrayBuffer);
-      const content = await zip.file("word/document.xml")?.async("text");
+const readWordFile = async (file: File | Blob): Promise<string> => {
+  const arrayBuffer = await readFileAsArrayBuffer(file);
+  const { default: JSZip } = await import("jszip");
+  const zip = await JSZip.loadAsync(arrayBuffer);
+  const content = await zip.file("word/document.xml")?.async("text");
+  return content
+    ? extractTextFromWordXML(content)
+    : Promise.reject("Failed to read Word file content");
+};
 
-      if (content) {
-        const extractedText = extractTextFromWordXML(content);
-        resolve(extractedText);
-      } else {
-        reject("Failed to read Word file content");
-      }
-    };
-    reader.onerror = (event: ProgressEvent<FileReader>) => {
-      reject(event.target?.error);
-    };
-    reader.readAsArrayBuffer(file);
-  });
-}
+const readExcelFile = async (file: File | Blob): Promise<string> => {
+  const arrayBuffer = await readFileAsArrayBuffer(file);
+  const { read, utils } = await import("xlsx");
+  const data = new Uint8Array(arrayBuffer);
+  const workbook = read(data, { type: "array" });
+  const sheetNames = workbook.SheetNames;
+  const extractedTextArray: string[] = [];
+  for (const sheetName of sheetNames) {
+    extractedTextArray.push("Sheet: " + sheetName + "\n");
+    const worksheet = workbook.Sheets[sheetName];
+    const jsonData = utils.sheet_to_json(worksheet, { header: 1 });
+    extractedTextArray.push(extractTextFromExcelData(jsonData as any[][]));
+  }
+  return extractedTextArray.join("\n");
+};
 
-const readZIPFileAsArrayBuffer = (file: File): Promise<ArrayBuffer> => {
-  return new Promise<ArrayBuffer>((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = (event: ProgressEvent<FileReader>) => {
-      const arrayBuffer = event.target?.result as ArrayBuffer;
-      resolve(arrayBuffer);
-    };
-    reader.onerror = (event: ProgressEvent<FileReader>) => {
-      reject(event.target?.error);
-    };
-    reader.readAsArrayBuffer(file);
-  });
+const readPdfFile = async (file: File | Blob): Promise<string> => {
+  const arrayBuffer = await readFileAsArrayBuffer(file);
+  const pdfjsLib = await import("pdfjs-dist");
+  pdfjsLib.GlobalWorkerOptions.workerSrc = require("pdfjs-dist/build/pdf.worker.min.mjs");
+  const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise;
+
+  let textContent = "";
+  for (let i = 1; i <= pdf.numPages; i++) {
+    const page = await pdf.getPage(i);
+    const text = await page.getTextContent();
+    textContent += text.items.map((item: any) => item.str).join(" ");
+  }
+
+  pdf.destroy();
+  return textContent;
+};
+
+const readImageFiles = async (file: File): Promise<string> => {
+  const { createWorker } = await import("tesseract.js");
+  const worker = await createWorker("eng+chi_sim");
+  await worker.load();
+  const {
+    data: { text },
+  } = await worker.recognize(file);
+  await worker.terminate();
+  return text;
 };
 
 const readFilesFromZIPFile = async (
@@ -58,14 +71,13 @@ const readFilesFromZIPFile = async (
   ignoreExtensions: string[],
 ): Promise<string> => {
   const files = new Map<string, string>();
-  const zipData = await readZIPFileAsArrayBuffer(zipFile);
+  const zipData = await readFileAsArrayBuffer(zipFile);
   const { default: JSZip } = await import("jszip");
   const zip = await JSZip.loadAsync(zipData);
 
   await Promise.allSettled(
     Object.values(zip.files).map(async (file) => {
       const fileName = file.name;
-
       const fileExtension =
         "." + fileName.split(".").pop()?.toLowerCase() || "";
 
@@ -78,6 +90,7 @@ const readFilesFromZIPFile = async (
         let fileContent = "";
         const fileContentArrayBuffer = await file.async("arraybuffer");
         const fileContentAsBlob = new Blob([fileContentArrayBuffer]);
+
         if (fileExtension === ".pdf") {
           fileContent = await readPdfFile(fileContentAsBlob);
         } else if (fileExtension === ".docx") {
@@ -87,6 +100,7 @@ const readFilesFromZIPFile = async (
         } else {
           fileContent = await file.async("string");
         }
+
         files.set(file.name, fileContent);
       }
     }),
@@ -95,7 +109,7 @@ const readFilesFromZIPFile = async (
   let outputText = "";
 
   for (const [filePath, fileContent] of files.entries()) {
-    outputText += `\nFile: ${filePath}/\n`;
+    outputText += `\nFile: ${filePath}\n`;
     outputText += `${fileContent}\n\n`;
   }
 
@@ -103,49 +117,16 @@ const readFilesFromZIPFile = async (
 };
 
 function extractTextFromWordXML(xmlContent: string) {
-  try {
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(xmlContent, "text/xml");
-    const textNodes = xmlDoc.getElementsByTagName("w:t");
-    let extractedText = "";
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(xmlContent, "text/xml");
+  const textNodes = xmlDoc.getElementsByTagName("w:t");
+  let extractedText = "";
 
-    for (let i = 0; i < textNodes.length; i++) {
-      extractedText += textNodes[i].textContent + " \n";
-    }
-
-    return extractedText;
-  } catch (error) {
-    console.error(error);
-    return "";
+  for (let i = 0; i < textNodes.length; i++) {
+    extractedText += textNodes[i].textContent + " \n";
   }
-}
 
-function readExcelFile(file: File | Blob): Promise<string> {
-  return new Promise<string>((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = async (event: ProgressEvent<FileReader>) => {
-      const { read, utils } = await import("xlsx");
-      const arrayBuffer = event.target?.result as ArrayBuffer;
-      const data = new Uint8Array(arrayBuffer);
-      const workbook = read(data, { type: "array" });
-      const sheetNames = workbook.SheetNames;
-      const extractedTextArray: string[] = [];
-
-      for (const sheetName of sheetNames) {
-        extractedTextArray.push("Sheet: " + sheetName + "\n");
-        const worksheet = workbook.Sheets[sheetName];
-        const jsonData = utils.sheet_to_json(worksheet, { header: 1 });
-        const extractedText = extractTextFromExcelData(jsonData as any[][]);
-        extractedTextArray.push(extractedText);
-      }
-      const joinedText = extractedTextArray.join("\n");
-      resolve(joinedText);
-    };
-    reader.onerror = (event: ProgressEvent<FileReader>) => {
-      reject(event.target?.error);
-    };
-    reader.readAsArrayBuffer(file);
-  });
+  return extractedText;
 }
 
 function extractTextFromExcelData(data: any[][]): string {
@@ -162,46 +143,6 @@ function extractTextFromExcelData(data: any[][]): string {
 
   return extractedText;
 }
-
-const readPdfFile = async (file: File | Blob): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = async (event: ProgressEvent<FileReader>) => {
-      if (event.target?.result) {
-        try {
-          const pdfjsLib = await import("pdfjs-dist");
-          pdfjsLib.GlobalWorkerOptions.workerSrc = require("pdfjs-dist/build/pdf.worker.min.mjs");
-          const pdf = await pdfjsLib.getDocument({ data: event.target.result })
-            .promise;
-
-          let textContent = "";
-          for (let i = 1; i <= pdf.numPages; i++) {
-            try {
-              const page = await pdf.getPage(i);
-              const text = await page.getTextContent();
-              textContent += text.items.map((item: any) => item.str).join(" ");
-            } catch (error) {
-              console.log(`Error occurred while reading page ${i}: ${error}`);
-              continue;
-            }
-          }
-
-          pdf.destroy();
-
-          resolve(textContent);
-        } catch (error) {
-          reject(`Error occurred while reading PDF file: ${error}`);
-        }
-      } else {
-        reject("No result found");
-      }
-    };
-    reader.onerror = () => {
-      reject(`Error occurred while reading file: ${reader.error}`);
-    };
-    reader.readAsArrayBuffer(file);
-  });
-};
 
 export {
   readFilesFromZIPFile,
