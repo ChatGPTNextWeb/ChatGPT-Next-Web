7cfa8d81833fc89137bc67d0a24fc58a1b46184d
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-12-21 11:40:57 +0100

    iOS

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "AdExGPT Mobile",
+    title: "AdExGPT iOS",
     statusBarStyle: "default",
   },
 };
