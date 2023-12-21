20211b28b4d9db91185f5d1a881e7c98e2f8d473
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-12-21 11:44:08 +0100

    ChatGPT-Next-Web-AdEx iOS

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "AdExGPT iOS",
+    title: "AdExGPT App iOS",
     statusBarStyle: "default",
   },
 };
