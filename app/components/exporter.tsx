b65ae21e5f4909fc776323ef283dc597822703d0
Fred Liang <info@fredliang.cn>
2023-12-23 16:48:16 +0800

    refactor: update product name

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -530,7 +530,7 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>NextChat</div>
             <div className={styles["sub-title"]}>
               github.com/Yidadaa/ChatGPT-Next-Web
             </div>
