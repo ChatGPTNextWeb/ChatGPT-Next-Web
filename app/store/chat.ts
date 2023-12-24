1ac85f5ec50af3da19a32c82ee60ab60e32281b5
H0llyW00dzZ <priv8@btz.pm>
2023-12-25 04:12:05 +0700

    Fix [UI/UX Page] Known issue
    - [+] fix(chat.ts): fix known issue where summarize is not using the current model selected
    - [+] feat(chat.ts): add support for user-selected model in getSummarizeModel function
    - [+] feat(builder.sh): add script to modify tauri.conf.json file and build Tauri application

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -81,9 +81,10 @@ function createEmptySession(): ChatSession {
   };
 }
 
-function getSummarizeModel(currentModel: string) {
-  // if it is using gpt-* models, force to use 3.5 to summarize
-  return currentModel.startsWith("gpt") ? SUMMARIZE_MODEL : currentModel;
+// fix known issue where summarize is not using the current model selected
+function getSummarizeModel(currentModel: string, modelConfig: ModelConfig) {
+  // should be depends of user selected
+  return currentModel ? modelConfig.model : currentModel;
 }
 
 function countMessages(msgs: ChatMessage[]) {
@@ -508,10 +509,12 @@ export const useChatStore = createPersistStore(
               content: Locale.Store.Prompt.Topic,
             }),
           );
+          const sessionModelConfig = this.currentSession().mask.modelConfig;
+          const topicModel = getSummarizeModel(session.mask.modelConfig.model, sessionModelConfig);
           api.llm.chat({
             messages: topicMessages,
             config: {
-              model: getSummarizeModel(session.mask.modelConfig.model),
+              model: topicModel,
             },
             onFinish(message) {
               get().updateCurrentSession(
@@ -555,6 +558,8 @@ export const useChatStore = createPersistStore(
           historyMsgLength > modelConfig.compressMessageLengthThreshold &&
           modelConfig.sendMemory
         ) {
+          const sessionModelConfig = this.currentSession().mask.modelConfig;
+          const summarizeModel  = getSummarizeModel(session.mask.modelConfig.model, sessionModelConfig);
           api.llm.chat({
             messages: toBeSummarizedMsgs.concat(
               createMessage({
@@ -566,7 +571,7 @@ export const useChatStore = createPersistStore(
             config: {
               ...modelConfig,
               stream: true,
-              model: getSummarizeModel(session.mask.modelConfig.model),
+              model: summarizeModel,
             },
             onUpdate(message) {
               session.memoryPrompt = message;
