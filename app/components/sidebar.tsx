9ae4767603360d19ed7342b099b322d58c4a63c4
Ben11029 <107007336+Ben11029@users.noreply.github.com>
2023-12-26 08:48:06 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           Pingtao-ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          为防止滥用，本站会定期修改登录密码；请私聊站主获取最新密码。
+          为防滥用会定期改密码请私站主获最新。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
