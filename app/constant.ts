a2d749a62e75de15d5b592dcdca543e197b48245
Nicolas <gml229251267@gmail.com>
2023-12-27 17:03:03 +0800

    Update constant.ts
    修改模型默认系统设置

--- app/constant.ts
+++ app/constant.ts
@@ -95,14 +95,7 @@ export const Google = {
 };
 
 export const DEFAULT_INPUT_TEMPLATE = `{{input}}`; // input / time / model / lang
-export const DEFAULT_SYSTEM_TEMPLATE = `
-You are ChatGPT, a large language model trained by OpenAI.
-Knowledge cutoff: {{cutoff}}
-Current model: {{model}}
-Current time: {{time}}
-Latex inline: $x^2$ 
-Latex block: $$e=mc^2$$
-`;
+export const DEFAULT_SYSTEM_TEMPLATE = "";
 
 export const SUMMARIZE_MODEL = "gpt-3.5-turbo";
 
