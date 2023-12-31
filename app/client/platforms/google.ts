9eaf492d5b72d252c9dcaa55d5d99dab87bf19e0
Fred Liang <info@fredliang.cn>
2023-12-31 19:44:51 +0800

    chore: low the google safety setting to avoid unexpected blocking

--- app/client/platforms/google.ts
+++ app/client/platforms/google.ts
@@ -59,6 +59,24 @@ export class GeminiProApi implements LLMApi {
         topP: modelConfig.top_p,
         // "topK": modelConfig.top_k,
       },
+      safetySettings: [
+        {
+          category: "HARM_CATEGORY_HARASSMENT",
+          threshold: "BLOCK_ONLY_HIGH",
+        },
+        {
+          category: "HARM_CATEGORY_HATE_SPEECH",
+          threshold: "BLOCK_ONLY_HIGH",
+        },
+        {
+          category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+          threshold: "BLOCK_ONLY_HIGH",
+        },
+        {
+          category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+          threshold: "BLOCK_ONLY_HIGH",
+        },
+      ],
     };
 
     console.log("[Request] google payload: ", requestPayload);
