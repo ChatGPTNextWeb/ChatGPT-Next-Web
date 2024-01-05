37260ed194e643445116981170a6319b3bf13aad
Fred Liang <info@fredliang.cn>
2024-01-05 21:16:49 +0800

    fix: import language issue

--- app/locales/index.ts
+++ app/locales/index.ts
@@ -16,7 +16,7 @@ import cs from "./cs";
 import ko from "./ko";
 import ar from "./ar";
 import bn from "./bn";
-import bn from "./sk";
+import sk from "./sk";
 import { merge } from "../utils/merge";
 
 import type { LocaleType } from "./cn";
@@ -67,7 +67,7 @@ export const ALL_LANG_OPTIONS: Record<Lang, string> = {
   no: "Nynorsk",
   ar: "العربية",
   bn: "বাংলা",
-  sk: "Slovensky"
+  sk: "Slovensky",
 };
 
 const LANG_KEY = "lang";
