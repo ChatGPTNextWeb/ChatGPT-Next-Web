2040a319563eef475aeabf4a32e30b66706d0957
sunbin1229 <36294205+sunbin1229@users.noreply.github.com>
2024-01-05 10:34:02 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT By Guanghui
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          山东光辉人力资源科技有限公司
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
