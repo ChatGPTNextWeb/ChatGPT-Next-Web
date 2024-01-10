223ff8d1c49e8338476627abdf7a260e2368ff3e
Alexey Bogomolov <mail@abogomolov.com>
2024-01-11 02:17:34 +0300

    autoscroll enabled by default

--- app/store/config.ts
+++ app/store/config.ts
@@ -34,6 +34,7 @@ export const DEFAULT_CONFIG = {
   theme: Theme.Auto as Theme,
   tightBorder: !!getClientConfig()?.isApp,
   sendPreviewBubble: true,
+  autoScrollMessage: true,
   enableAutoGenerateTitle: true,
   sidebarWidth: DEFAULT_SIDEBAR_WIDTH,
 
