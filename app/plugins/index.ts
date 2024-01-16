a54a4bc31d6705d9cb589d00cafebc488ddfb39f
JonMagon <jonmagon@gmail.com>
2024-01-16 13:40:20 +0300

    Plugins Russian localization

--- app/plugins/index.ts
+++ app/plugins/index.ts
@@ -1,6 +1,7 @@
 import { Plugin } from "../store/plugin";
 import { CN_PLUGINS } from "./cn";
 import { EN_PLUGINS } from "./en";
+import { RU_PLUGINS } from "./ru";
 
 import { type BuiltinPlugin } from "./typing";
 export { type BuiltinPlugin } from "./typing";
@@ -24,4 +25,5 @@ export const BUILTIN_PLUGIN_STORE = {
 export const BUILTIN_PLUGINS: BuiltinPlugin[] = [
   ...CN_PLUGINS,
   ...EN_PLUGINS,
+  ...RU_PLUGINS,
 ].map((m) => BUILTIN_PLUGIN_STORE.add(m));
