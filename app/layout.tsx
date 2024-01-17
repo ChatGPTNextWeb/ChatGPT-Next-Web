6dc47620f2b1fc5b1f0b0a9e0b80bf0f355b9c83
chenjiarong123 <145646383+chenjiarong123@users.noreply.github.com>
2024-01-17 11:23:12 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { getServerSideConfig } from "./config/server";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "GBPIChat",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "GBPIChat",
     statusBarStyle: "default",
   },
 };
