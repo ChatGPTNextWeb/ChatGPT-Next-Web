83d29753028dc466a4b50533c4d4e092160dc3ac
Qiulin <20410040207@stu.xust.edu.cn>
2024-01-26 17:15:47 +0800

    google

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           ImagineChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          始于兴趣是最好的学习方式！
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
