eda187282eb8333e6465eeee96d3a4e018f96018
Q.A.zh <40236765+QAbot-zh@users.noreply.github.com>
2024-01-26 10:12:31 +0800

    Update config.ts (use const gpt-3.5-turbo to generate title and summerize history)

--- app/store/config.ts
+++ app/store/config.ts
@@ -34,7 +34,7 @@ export const DEFAULT_CONFIG = {
   theme: Theme.Auto as Theme,
   tightBorder: !!getClientConfig()?.isApp,
   sendPreviewBubble: true,
-  enableAutoGenerateTitle: false,
+  enableAutoGenerateTitle: true,
   sidebarWidth: DEFAULT_SIDEBAR_WIDTH,
 
   disablePromptHint: false,
