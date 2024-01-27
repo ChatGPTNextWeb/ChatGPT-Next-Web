f9d2830eda48c89e6f8c45e30463fda97294cdf3
sijinhui <sijinhui@qq.com>
2024-01-28 01:23:20 +0800

    2

--- start.sh
+++ start.sh
@@ -20,4 +20,4 @@ rsync -az --delete ./.next/server/ ${OUT_DIR}/.next/server
 rsync -az --delete ./.next/server/ ${OUT_DIR}/.next/server
 rsync -az --delete "./node_modules/tiktoken/" ${OUT_DIR}/node_modules/tiktoken
 
-docker network create chatgpt-ns &> /dev/null
+docker network create chatgpt-ns
