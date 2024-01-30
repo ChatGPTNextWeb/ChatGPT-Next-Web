738e9d01fcc4e9616304f97efe6273db02709f51
Qiulin <20410040207@stu.xust.edu.cn>
2024-01-30 21:13:13 +0800

    export const Models

--- app/client/api.ts
+++ app/client/api.ts
@@ -11,7 +11,7 @@ import { GeminiProApi } from "./platforms/google";
 export const ROLES = ["system", "user", "assistant"] as const;
 export type MessageRole = (typeof ROLES)[number];
 
-export const Models = ["gpt-3.5-turbo", "gpt-4"] as const;
+export const Models = ["gemini-pro", "gemini-pro"] as const;
 export type ChatModel = ModelType;
 
 export interface RequestMessage {
