08cf1b7f83e17ed715c036e2f3b1d1f97e89555a
Qiulin <20410040207@stu.xust.edu.cn>
2024-01-30 20:01:08 +0800

    Modify modelConfig

--- app/store/config.ts
+++ app/store/config.ts
@@ -46,10 +46,10 @@ export const DEFAULT_CONFIG = {
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
-    model: "gpt-3.5-turbo" as ModelType,
+    model: "gemini-pro" as ModelType,
     temperature: 0.5,
     top_p: 1,
-    max_tokens: 4000,
+    max_tokens: 4090,
     presence_penalty: 0,
     frequency_penalty: 0,
     sendMemory: true,
