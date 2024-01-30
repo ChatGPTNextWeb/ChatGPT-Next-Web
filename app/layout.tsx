b921407dab0394c99ac6721b1ac8b066a2d75513
Dylan Shi <dylanshipf@hotmail.com>
2024-01-30 09:11:56 +0000

    add the google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,8 +35,30 @@ export default function RootLayout({
         <meta name="version" content={buildConfig.commitId} />
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
+        {/* 插入 GTM 的 <head> 部分代码 */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PLMWKRSN');
+            `,
+          }}
+        ></script>
       </head>
-      <body>{children}</body>
+      <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PLMWKRSN"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        ></noscript>
+        {children}
+      </body>
     </html>
   );
 }
