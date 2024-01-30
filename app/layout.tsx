dab3bd664775187c664554d69a695128e3fefee3
Doiiars <doiiars@qq.com>
2024-01-31 01:37:34 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,13 @@ export default function RootLayout({
         <script src="/serviceWorkerRegister.js" defer></script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6765970453610684" crossOrigin="anonymous"></script>
         <GoogleAnalytics gaId="G-KQB25CC08Q" />
+        <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "kq9ueuu3wo");
+        </script>
       </head>
       <body>
         {children}
