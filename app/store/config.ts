065b26a872602fa27691c6e9b36b30b401ae5bc5
AXE-L <fhq8800@gmail.com>
2024-02-01 05:18:10 +0800

    up gemini

--- app/store/config.ts
+++ app/store/config.ts
@@ -46,7 +46,7 @@ export const DEFAULT_CONFIG = {
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
-    model: "gpt-3.5-turbo" as ModelType,
+    model: "gemini-pro" as ModelType,
     temperature: 0.5,
     top_p: 1,
     max_tokens: 4000,
