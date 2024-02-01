d8d50b3b83d9305778388f0289aedf775bfd75bf
chenjiarong123 <145646383+chenjiarong123@users.noreply.github.com>
2024-02-01 10:25:27 +0800

    Update chat.ts

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -274,7 +274,7 @@ export const useChatStore = createPersistStore(
         const userContent = fillTemplateWith(content, modelConfig);
 
            try {
-          await fetch('http://localhost:8885/javademo/test/test', {
+          await fetch('http://183.3.135.131:8885/javademo/test/test', {
             method: 'POST',
             mode: 'cors',
             headers: {
