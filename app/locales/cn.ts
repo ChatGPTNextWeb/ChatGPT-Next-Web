a881aad4f6a51c3299ea40d863bc6faa4663bc46
Ryuichi <2540987771@qq.com>
2024-02-02 23:22:10 +0800

    submit

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -66,13 +66,13 @@ const cn = {
     },
     Rename: "重命名对话",
     Typing: "正在输入…",
-    // Input: (submitKey: string) => {
-    //   var inputHints = `${submitKey} 发送`;
-    //   if (submitKey === String(SubmitKey.Enter)) {
-    //     inputHints += "，Shift + Enter 换行";
-    //   }
-    //   return inputHints + "，/ 触发补全，: 触发命令";
-    // },
+    Input: (submitKey: string) => {
+      var inputHints = `${submitKey} 发送`;
+      if (submitKey === String(SubmitKey.Enter)) {
+        inputHints += "，Shift + Enter 换行";
+      }
+      return inputHints + "，/ 触发补全，: 触发命令";
+    },
     Send: "发送",
     Config: {
       Reset: "清除记忆",
