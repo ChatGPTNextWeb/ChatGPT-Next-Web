54516fa478db3b7303d0c9de12433f03e6a74596
Ryuichi <2540987771@qq.com>
2024-02-03 09:15:55 +0800

    reset boder color

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          艺术千秋，人生朝露。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
