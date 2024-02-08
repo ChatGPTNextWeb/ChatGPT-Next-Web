f45f96704a881a74c41cdc64d84c19ebf3d337ba
songh3 <songh3@ziroom.com>
2024-02-09 00:15:58 +0800

    commit

--- app/components/ltt.ts
+++ app/components/ltt.ts
@@ -2,23 +2,25 @@ import * as sdk from "microsoft-cognitiveservices-speech-sdk";
 import { getServerSideConfig } from "@/app/config/server";
 const config = getServerSideConfig();
 
-let subscriptionKey = config.azurekey;
+let subscriptionKey = config.baseUrl;
 const serviceRegion = "eastus"; // 例如 "westus"
-if (typeof subscriptionKey !== "string") {
-  throw new Error("The SUBSCRIPTION_KEY environment variable is not set.");
-}
+// if (typeof subscriptionKey !== "string") {
+//   console.error("The subscriptionKey is not a string."+ subscriptionKey);
+//   throw new Error("The SUBSCRIPTION_KEY environment variable is not set.");
+// }
 // 创建语音识别器
-const speechConfig = sdk.SpeechConfig.fromSubscription(
-  subscriptionKey,
-  serviceRegion,
-);
-
-// 设置语音识别的语言
-speechConfig.speechRecognitionLanguage = "zh-CN";
-
-const audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput();
 
 export default function recognizeSpeech(): Promise<string> {
+  let subscriptionKey = process.env.OPENAI_ORG_ID;
+  const speechConfig = sdk.SpeechConfig.fromSubscription(
+    subscriptionKey,
+    serviceRegion,
+  );
+
+  // 设置语音识别的语言
+  speechConfig.speechRecognitionLanguage = "zh-CN";
+
+  const audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput();
   return new Promise<string>((resolve, reject) => {
     const recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
     console.log("开始语音识别，请说话...");
--- app/components/tts.ts
+++ app/components/tts.ts
@@ -2,19 +2,18 @@ import * as sdk from "microsoft-cognitiveservices-speech-sdk";
 import { getServerSideConfig } from "@/app/config/server";
 const config = getServerSideConfig();
 
-let subscriptionKey = config.azurekey;
+let subscriptionKey = config.baseUrl;
 
 // 这些信息应该从环境变量或配置文件中获取，不应硬编码在脚本中。
 const serviceRegion = "eastus"; // 例如 "westus"
-if (typeof subscriptionKey !== "string") {
-  throw new Error("The SUBSCRIPTION_KEY environment variable is not set.");
-}
-// 创建语音识别器
-const speechConfig = sdk.SpeechConfig.fromSubscription(
-  subscriptionKey,
-  serviceRegion,
-);
+
 export default function convertTextToSpeech(text: string) {
+  let subscriptionKey = process.env.OPENAI_ORG_ID || "";
+  // 创建语音识别器
+  const speechConfig = sdk.SpeechConfig.fromSubscription(
+    subscriptionKey,
+    serviceRegion,
+  );
   return new Promise((resolve, reject) => {
     speechConfig.speechSynthesisVoiceName = "zh-CN-XiaoshuangNeural"; // 使用 Azure 提供的声音名列表，选择合适的声音
 
