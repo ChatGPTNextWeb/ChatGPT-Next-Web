4d8a9bf652f1303e5ba3727d195b1133ba438fa5
离心称 <130938456+lxc00215@users.noreply.github.com>
2024-02-10 01:07:50 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          本技术由 <a href="https://api.leebay.asia">信成AI</a>提供支持
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
