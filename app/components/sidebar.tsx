a56232b53e67c36390e4f428f6b05f1692f1b408
Tianshi Xu <994876761@qq.com>
2024-02-11 05:25:17 +0000

    .

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,11 +155,9 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          SEC-GPT
         </div>
-        <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
-        </div>
+        <div className={styles["sidebar-sub-title"]}>ChatGPT for SEC!</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
