f4f92608762584cfd1796ebe583e784f33ec84c5
Tianshi Xu <994876761@qq.com>
2024-02-14 13:37:37 +0000

    .

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          SEC-GPT
+          SEC-Chat
         </div>
         <div className={styles["sidebar-sub-title"]}>ChatGPT for SEC Lab !</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
