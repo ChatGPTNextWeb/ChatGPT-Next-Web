4a52951900336e1ff5c0d1fe171c5912876ae63c
FengLi666 <kaishibianshen@gmail.com>
2024-02-18 11:23:26 +0800

    gemini BLOCK_NONE

--- app/client/platforms/google.ts
+++ app/client/platforms/google.ts
@@ -62,19 +62,19 @@ export class GeminiProApi implements LLMApi {
       safetySettings: [
         {
           category: "HARM_CATEGORY_HARASSMENT",
-          threshold: "BLOCK_ONLY_HIGH",
+          threshold: "BLOCK_NONE",
         },
         {
           category: "HARM_CATEGORY_HATE_SPEECH",
-          threshold: "BLOCK_ONLY_HIGH",
+          threshold: "BLOCK_NONE",
         },
         {
           category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-          threshold: "BLOCK_ONLY_HIGH",
+          threshold: "BLOCK_NONE",
         },
         {
           category: "HARM_CATEGORY_DANGEROUS_CONTENT",
-          threshold: "BLOCK_ONLY_HIGH",
+          threshold: "BLOCK_NONE",
         },
       ],
     };
