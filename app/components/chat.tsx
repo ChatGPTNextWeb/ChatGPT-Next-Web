e287e5470065c6f0531d8e6e44ed460794582fba
Algorithm5838 <108630393+Algorithm5838@users.noreply.github.com>
2024-03-09 22:17:25 +0300

    Update chat.tsx

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -387,25 +387,24 @@ function useScrollToBottom(
   scrollRef: RefObject<HTMLDivElement>,
   detach: boolean = false,
 ) {
-  // for auto-scroll
-
   const [autoScroll, setAutoScroll] = useState(true);
-  function scrollDomToBottom() {
-    const dom = scrollRef.current;
-    if (dom) {
-      requestAnimationFrame(() => {
-        setAutoScroll(true);
-        dom.scrollTo(0, dom.scrollHeight);
-      });
-    }
-  }
 
-  // auto scroll
   useEffect(() => {
     if (autoScroll && !detach) {
-      scrollDomToBottom();
+      const dom = scrollRef.current;
+      if (dom) {
+        dom.scrollTo(0, dom.scrollHeight);
+      }
     }
-  });
+  }, [autoScroll, detach, scrollRef]);
+
+  function scrollDomToBottom() {
+    setAutoScroll(true);
+    const dom = scrollRef.current;
+    if (dom) {
+      dom.scrollTo(0, dom.scrollHeight);
+    }
+  }
 
   return {
     scrollRef,
