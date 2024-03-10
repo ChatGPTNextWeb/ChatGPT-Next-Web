66fd135cfba6ac767f02571de99c6066cf6b5fb7
zhangchenguang01 <zhangchenguang01@58.com>
2024-03-10 15:29:40 +0800

    修改链接报错

--- app/constant.ts
+++ app/constant.ts
@@ -1,5 +1,4 @@
 export const OWNER = "Yidadaa";
-export const 7li7li_URL = `https://www.7li7li.cn`;
 export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
