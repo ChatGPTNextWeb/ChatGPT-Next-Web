7fde6efefe21deb53d89b8592253d60cedbaafc1
BlazarWinGX <133990625+BlazarWinGX@users.noreply.github.com>
2024-03-11 20:17:58 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -32,7 +32,7 @@ export const DEFAULT_CONFIG = {
   avatar: "1f603",
   fontSize: 14,
   theme: Theme.Auto as Theme,
-  tightBorder: !!getClientConfig()?.isApp,
+  tightBorder: true,
   sendPreviewBubble: true,
   enableAutoGenerateTitle: true,
   sidebarWidth: DEFAULT_SIDEBAR_WIDTH,
