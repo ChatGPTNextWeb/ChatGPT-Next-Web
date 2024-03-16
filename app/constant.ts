fa2eb230cd43f9d8148fa70b10335f35b9b71656
Cloudbeer <19568205@qq.com>
2024-03-16 22:01:53 +0800

    fetch models from remote url

--- app/constant.ts
+++ app/constant.ts
@@ -127,6 +127,7 @@ export const DEFAULT_MODELS = [
     available: true,
     modelId: "anthropic.claude-3-sonnet-20240229-v1:0",
     anthropic_version: "bedrock-2023-05-31",
+    displayName: "Claude3 sonnet",
     provider: {
       id: "aws",
       providerName: "AWS",
@@ -138,6 +139,7 @@ export const DEFAULT_MODELS = [
     available: true,
     modelId: "anthropic.claude-3-haiku-20240307-v1:0",
     anthropic_version: "bedrock-2023-05-31",
+    displayName: "Claude3 haiku",
     provider: {
       id: "aws",
       providerName: "AWS",
