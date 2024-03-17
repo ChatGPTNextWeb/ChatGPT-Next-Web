742c85d8e0b5108219309c935af24ab1bc6f1f7c
zhihuz123 <117065911+zhihuz123@users.noreply.github.com>
2024-03-17 10:40:20 +0800

    set default avator

--- app/store/config.ts
+++ app/store/config.ts
@@ -29,7 +29,7 @@ export const DEFAULT_CONFIG = {
   lastUpdate: Date.now(), // timestamp, to merge state
 
   submitKey: SubmitKey.Enter,
-  avatar: "1f603",
+  avatar: "1f98a",
   fontSize: 14,
   theme: Theme.Auto as Theme,
   tightBorder: !!getClientConfig()?.isApp,
