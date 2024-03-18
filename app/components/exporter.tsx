2d286ded68bb736822ee32fb7b9b5a9a5c542a89
laputa7 <liujiachang777@gmail.com>
2024-03-18 20:24:40 +0800

    update copy & icon

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -546,9 +546,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
+            <div className={styles["main-title"]}>BRClient</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              github.com/DamonDeng/BRClient
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
