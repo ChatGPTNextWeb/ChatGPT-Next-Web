d5fde4a49cc05ac73ae2db3bdf2c74ecd3ff4650
LiLeeLing <158540825+LiLeeLing@users.noreply.github.com>
2024-03-21 23:20:09 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -546,7 +546,7 @@ export function ImagePreviewer(props: {
           <div>
             <div className={styles["main-title"]}>NextChat</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              lolis.dev
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
