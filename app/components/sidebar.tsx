22ab45b452566ba1a5ab770e056fbe36a5844c07
youya <132335365+yushizhuo@users.noreply.github.com>
2024-03-27 19:55:22 +0000

    Pending changes exported from your codespace

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          龙年大吉.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
