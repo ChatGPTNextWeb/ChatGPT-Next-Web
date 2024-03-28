4435893ceed98c680d49652c4800e6abd0a9300f
kpfefer <15963543+kpfefer@users.noreply.github.com>
2024-03-28 21:43:05 +0100

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -87,7 +87,7 @@ export const Azure = {
 
 export const Google = {
   ExampleEndpoint: "https://generativelanguage.googleapis.com/",
-  ChatPath: "v1beta/models/gemini-pro:generateContent",
+  ChatPath: "v1beta/models/gemini-1.5-pro-latest:generateContent",
   VisionChatPath: "v1beta/models/gemini-pro-vision:generateContent",
 
   // /api/openai/v1/chat/completions
