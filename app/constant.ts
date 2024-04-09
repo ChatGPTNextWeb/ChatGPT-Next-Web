d50812745211f6ef043a7fad8d50f3178e5a2290
butterfly <butterfly@bogon.com>
2024-04-09 10:45:09 +0800

    feat: fix system prompt

--- app/constant.ts
+++ app/constant.ts
@@ -118,7 +118,7 @@ You are ChatGPT, a large language model trained by {{ServiceProvider}}.
 Knowledge cutoff: {{cutoff}}
 Current model: {{model}}
 Current time: {{time}}
-Latex inline: \(x^2\) 
+Latex inline: \\(x^2\\) 
 Latex block: $$e=mc^2$$
 `;
 
