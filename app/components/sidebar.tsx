8b78d180238e464f10294eb85dfb0f0fe5c69797
cliveskinner <clive.skinner@marketcast.com>
2024-04-12 12:27:07 +0100

    Updated sidebar text and icon

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          ChatMC
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          MarketCast's ChatGPT Service
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
