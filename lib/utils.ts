3b36628bc17656d3a137fd85705f3058dbaaf2d4
sijinhui <sijinhui@qq.com>
2024-04-13 11:38:38 +0800

    update package

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,9 +1,9 @@
 import bcrypt from "bcryptjs";
 import { get_encoding } from "tiktoken";
 
+const encoding = get_encoding("cl100k_base");
 
 export function getTokenLength(input: string): number {
-  const encoding = get_encoding("cl100k_base");
   return encoding.encode(input).length;
 }
 
