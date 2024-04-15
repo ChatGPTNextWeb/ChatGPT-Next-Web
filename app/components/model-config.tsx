8a6a58d33824c9744afaec30b1f2909c41643e12
黑云白土 <Li_Yucong@outlook.com>
2024-04-15 19:38:40 +0800

    Update model-config.tsx

--- app/components/model-config.tsx
+++ app/components/model-config.tsx
@@ -84,7 +84,7 @@ export function ModelConfigList(props: {
         <input
           type="number"
           min={0}
-          max={maxOutToken}
+          max={props.modelConfig.max_tokens}
           value={props.modelConfig.max_tokens}
           onChange={(e) =>
             props.updateConfig(
