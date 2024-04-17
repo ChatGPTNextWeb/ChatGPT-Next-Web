f94938810e9c4e4ce20d703946213c16629f6584
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2024-04-17 17:22:18 +0200

    constant

--- app/constant.ts
+++ app/constant.ts
@@ -127,12 +127,10 @@ export const KnowledgeCutOffDate: Record<string, string> = {
   default: "2021-09",
   "gpt-4-turbo": "2023-12",
   "gpt-4-turbo-2024-04-09": "2023-12",
-  "gpt-4-turbo": "2023-12",
   "gpt-4-turbo-preview": "2023-12",
   "gpt-4-1106-preview": "2023-04",
   "gpt-4-0125-preview": "2023-12",
   "gpt-4-vision-preview": "2023-04",
-  "gpt-4-turbo-2024-04-09": "2023-12",
   
   // After improvements,
   // it's now easier to add "KnowledgeCutOffDate" instead of stupid hardcoding it, as was done previously.
