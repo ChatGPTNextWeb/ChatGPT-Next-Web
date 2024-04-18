7fc930a54c463d8bb991d41af02e668dcac6ccb2
rasheed <thcmofskot@gmail.com>
2024-04-18 09:56:31 +0100

    Refactor code to use arrow functions and proper promise handling with .catch

--- public/serviceWorkerRegister.js
+++ public/serviceWorkerRegister.js
@@ -1,9 +1,7 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/serviceWorker.js').then(function (registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function (err) {
-      console.error('ServiceWorker registration failed: ', err);
-    });
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/serviceWorker.js')
+      .then((registration) => console.log('ServiceWorker registration successful with scope: ', registration.scope))
+      .catch((err) => console.error('ServiceWorker registration failed: ', err));
   });
 }
