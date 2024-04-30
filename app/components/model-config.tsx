7da2b56dbd62d95184e3cddbb4034d6768be1912
Tiiiiiida <im_jy@outlook.com>
2024-04-30 15:59:15 +0800

    fix maxtoken parameter for larger models

--- app/components/model-config.tsx
+++ app/components/model-config.tsx
@@ -79,7 +79,7 @@ export function ModelConfigList(props: {
         <input
           type="number"
           min={1024}
-          max={512000}
+          max={1048576}
           value={props.modelConfig.max_tokens}
           onChange={(e) =>
             props.updateConfig(
--- app/store/config.ts
+++ app/store/config.ts
@@ -82,7 +82,7 @@ export const ModalConfigValidator = {
     return x as ModelType;
   },
   max_tokens(x: number) {
-    return limitNumber(x, 0, 512000, 1024);
+    return limitNumber(x, 0, 1048576, 1024);
   },
   presence_penalty(x: number) {
     return limitNumber(x, -2, 2, 0);
