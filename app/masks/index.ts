58d4a5632cdd309b172dffaf10997b966086d233
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2024-05-03 17:56:53 +0200

    en masks and prompts

--- app/masks/index.ts
+++ app/masks/index.ts
@@ -22,6 +22,6 @@ export const BUILTIN_MASK_STORE = {
   },
 };
 
-export const BUILTIN_MASKS: BuiltinMask[] = [...CN_MASKS, ...TW_MASKS, ...EN_MASKS].map(
+export const BUILTIN_MASKS: BuiltinMask[] = [...EN_MASKS].map(
   (m) => BUILTIN_MASK_STORE.add(m),
 );
