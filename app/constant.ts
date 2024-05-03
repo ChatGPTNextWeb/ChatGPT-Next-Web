332d4fcbcfa7df85d66e2b42a97a7d3d92d305d0
星野 Hoshino <93378160+HXH2002@users.noreply.github.com>
2024-05-03 21:51:24 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -111,7 +111,7 @@ export const DEFAULT_INPUT_TEMPLATE = `{{input}}`; // input / time / model / lan
 // Latex block: $$e=mc^2$$
 // `;
 export const DEFAULT_SYSTEM_TEMPLATE = `
-You are ChatGPT, a large language model trained by {{ServiceProvider}}.
+You are ChatXH(星环大模型), a large language model trained by {{ServiceProvider}}.
 Knowledge cutoff: {{cutoff}}
 Current model: {{model}}
 Current time: {{time}}
