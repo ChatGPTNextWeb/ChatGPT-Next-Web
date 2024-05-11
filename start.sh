5b2a9ad3ba2a28713a21c4c413a519c725754e45
sijinhui <sijinhui@qq.com>
2024-05-11 17:58:11 +0800

    test build

--- start.sh
+++ start.sh
@@ -1,37 +1,30 @@
 #!/bin/bash
 set -e
-# 加速
-yarn config set registry 'https://registry.npmmirror.com/'
-yarn config set sharp_binary_host "https://npm.taobao.org/mirrors/sharp"
-yarn config set sharp_libvips_binary_host "https://npm.taobao.org/mirrors/sharp-libvips"
-# 官方
-#yarn config delete registry
-#yarn config delete sharp_binary_host
-#yarn config delete sharp_libvips_binary_host
-
-yarn cache clean
-yarn install
-#if command -v cnpm &> /dev/null
-#then
-#    echo "cnpm 已安装"
-#else
-#    npm i cnpm
-#fi
+## 加速
+#yarn config set registry 'https://registry.npmmirror.com/'
+#yarn config set sharp_binary_host "https://npm.taobao.org/mirrors/sharp"
+#yarn config set sharp_libvips_binary_host "https://npm.taobao.org/mirrors/sharp-libvips"
+## 官方
+##yarn config delete registry
+##yarn config delete sharp_binary_host
+##yarn config delete sharp_libvips_binary_host
 #
-#cnpm i
-yarn run build
-
-mkdir -p "./node_modules/tiktoken"
-export OUT_DIR="out"
-
-mkdir -p ${OUT_DIR}
-
-rsync -az --delete ./.next/standalone/ ${OUT_DIR}
-rsync -az --delete ./public/ ${OUT_DIR}/public
-rsync -az --delete ./.next/static/ ${OUT_DIR}/.next/static
-rsync -az --delete ./.next/server/ ${OUT_DIR}/.next/server
-rsync -az --delete ./.next/server/ ${OUT_DIR}/.next/server
-rsync -az --delete "./node_modules/tiktoken/" ${OUT_DIR}/node_modules/tiktoken
+#yarn cache clean
+#yarn install
+#
+#yarn run build
+#
+#mkdir -p "./node_modules/tiktoken"
+#export OUT_DIR="out"
+#
+#mkdir -p ${OUT_DIR}
+#
+#rsync -az --delete ./.next/standalone/ ${OUT_DIR}
+#rsync -az --delete ./public/ ${OUT_DIR}/public
+#rsync -az --delete ./.next/static/ ${OUT_DIR}/.next/static
+#rsync -az --delete ./.next/server/ ${OUT_DIR}/.next/server
+#rsync -az --delete ./.next/server/ ${OUT_DIR}/.next/server
+#rsync -az --delete "./node_modules/tiktoken/" ${OUT_DIR}/node_modules/tiktoken
 
 docker network ls | grep -qw chatgpt-ns || docker network create chatgpt-ns
 
