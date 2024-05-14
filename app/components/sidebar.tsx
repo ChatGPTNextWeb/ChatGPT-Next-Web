cf65f6cac8f608b38b02ba56bdf5db093fb98494
青云依兮 <1422756921@qq.com>
2024-05-14 21:30:28 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          ChatGPT-青云依兮
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          构建自己的人工智能助手。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
