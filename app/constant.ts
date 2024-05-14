969dfbdb30c0456cdf5a691501712901b82ef0d3
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2024-05-14 14:38:43 +0200

    anthropicModels

--- app/constant.ts
+++ app/constant.ts
@@ -160,7 +160,7 @@ const anthropicModels = [
   // "claude-2.0",
   // "claude-2.1",
   // "claude-3-sonnet-20240229",
-  // "claude-3-opus-20240229",
+  "claude-3-opus-20240229",
   // "claude-3-haiku-20240307",
 ];
 
