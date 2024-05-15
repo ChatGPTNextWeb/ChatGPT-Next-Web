425f2d9d37f8b0ae2689bf065b33a152d66a2070
Woodwater <69405831+kevins-96@users.noreply.github.com>
2024-05-15 17:21:19 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -159,12 +159,12 @@ const googleModels = [
 ];
 
 const anthropicModels = [
-  "claude-instant-1.2",
-  "claude-2.0",
-  "claude-2.1",
-  "claude-3-sonnet-20240229",
-  "claude-3-opus-20240229",
-  "claude-3-haiku-20240307",
+  // "claude-instant-1.2",
+  // "claude-2.0",
+  // "claude-2.1",
+  // "claude-3-sonnet-20240229",
+  // "claude-3-opus-20240229",
+  // "claude-3-haiku-20240307",
 ];
 
 export const DEFAULT_MODELS = [
