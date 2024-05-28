79d2d6db1578ef76671ccfebfbab799b52c06a57
opvip888 <118167189+opvip888@users.noreply.github.com>
2024-05-28 20:20:55 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          Mercedes-Benz
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          禁止违规，违法内容.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
