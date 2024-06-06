0470a60ae5fa10c296caa5a44b260f6bdc9d6e43
Igroshka <86719527+Igroshka@users.noreply.github.com>
2024-06-06 22:09:10 +0700

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -83,7 +83,7 @@ export const ModalConfigValidator = {
     return x as ModelType;
   },
   max_tokens(x: number) {
-    return limitNumber(x, 0, 512000, 1024);
+    return limitNumber(x, 0, 1056768, 1024);
   },
   presence_penalty(x: number) {
     return limitNumber(x, -2, 2, 0);
