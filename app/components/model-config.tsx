8a0bd28474996299512486f2cee60f9b87428add
Igroshka <86719527+Igroshka@users.noreply.github.com>
2024-06-06 21:47:21 +0700

    Update model-config.tsx Max Tokens 1,056,768

--- app/components/model-config.tsx
+++ app/components/model-config.tsx
@@ -79,7 +79,7 @@ export function ModelConfigList(props: {
         <input
           type="number"
           min={1024}
-          max={512000}
+          max={1056768}
           value={props.modelConfig.max_tokens}
           onChange={(e) =>
             props.updateConfig(
