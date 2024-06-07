5443eaf217a7c0347581d117b116a8b2d3d802b0
Igroshka <86719527+Igroshka@users.noreply.github.com>
2024-06-07 15:17:09 +0700

    Update settings.tsx

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -616,7 +616,6 @@ export function Settings() {
   const showUsage = accessStore.isAuthorized();
   useEffect(() => {
     // checks per minutes
-    checkUpdate();
     showUsage && checkUsage();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
