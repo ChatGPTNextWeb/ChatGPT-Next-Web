463cc963ab15e20fab64453a7fa89e977083826f
h-lakshman <lakshman.vaibhav007@gmail.com>
2024-06-18 02:17:45 +0530

    Removed the spinner and used the existing loading component

--- app/auth_callback/page.tsx
+++ app/auth_callback/page.tsx
@@ -2,19 +2,18 @@
 import { Suspense, useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useAccessStore } from "@/app/store";
-import Spinner from "@/app/components/spinner";
+import { Loading } from "../components/ui-lib";
 
 const AuthCallbackPage = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
+  console.log(token);
 
   useEffect(() => {
     if (typeof window !== "undefined" && token) {
       // Update the state with the token and isLoggedin to true
       useAccessStore.setState({ openaiApiKey: `${token}`, isLoggedin: true });
-
-      console.log("User is logged in:", useAccessStore.getState().isLoggedin);
 
       router.push("/");
     }
@@ -24,7 +23,9 @@ const AuthCallbackPage = () => {
 };
 const AuthCallbackPageWrapper = () => {
   return (
-    <Suspense fallback={<Spinner />}>
+    <Suspense>
+      fallback=
+      {<Loading />}
       <AuthCallbackPage />
     </Suspense>
   );
