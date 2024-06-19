aedda563e8aad5b04204dc1f4d9fad8314b97dcc
h-lakshman <lakshman.vaibhav007@gmail.com>
2024-06-19 19:57:36 +0530

    By default goes to Login page if user is not logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
+"use client";
 import { Analytics } from "@vercel/analytics/react";
-
 import { Home } from "./components/home";
-
 import { getServerSideConfig } from "./config/server";
-
+import { useEffect } from "react";
+import { useAccessStore } from "./store";
+import { Path } from "./constant";
+import { useRouter } from "next/navigation";
 const serverConfig = getServerSideConfig();
 
 export default async function App() {
+  const router = useRouter();
+  const isLoggedin = useAccessStore.getState().isLoggedin;
+  useEffect(() => {
+    if (!isLoggedin) {
+      if (process.env.NODE_ENV === "development") {
+        router.push(Path.LoginDev);
+      } else {
+        router.push(Path.Login);
+      }
+    }
+  }, []);
   return (
     <>
       <Home />
