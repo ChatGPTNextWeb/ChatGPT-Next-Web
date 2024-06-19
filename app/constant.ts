334a746b6754048cb4cbf7731db9d9be371fbf37
h-lakshman <lakshman.vaibhav007@gmail.com>
2024-06-19 16:13:26 +0530

    Added new openai models

--- app/constant.ts
+++ app/constant.ts
@@ -141,18 +141,17 @@ export const KnowledgeCutOffDate: Record<string, string> = {
 
 const openaiModels = [
   "gpt-3.5-turbo",
-  "gpt-3.5-turbo-1106",
-  "gpt-3.5-turbo-0125",
   "gpt-4",
   "gpt-4-0613",
-  "gpt-4-32k",
-  "gpt-4-32k-0613",
+  "gpt-4-0314",
+  "gpt-4-0125-preview",
   "gpt-4-turbo",
   "gpt-4-turbo-preview",
+  "gpt-4-turbo-2024-04-09",
+  "gpt-4-1106-preview",
+  "gpt-4-vision-preview",
   "gpt-4o",
   "gpt-4o-2024-05-13",
-  "gpt-4-vision-preview",
-  "gpt-4-turbo-2024-04-09",
 ];
 
 const googleModels = [
@@ -181,24 +180,24 @@ export const DEFAULT_MODELS = [
       providerType: "openai",
     },
   })),
-  ...googleModels.map((name) => ({
-    name,
-    available: true,
-    provider: {
-      id: "google",
-      providerName: "Google",
-      providerType: "google",
-    },
-  })),
-  ...anthropicModels.map((name) => ({
-    name,
-    available: true,
-    provider: {
-      id: "anthropic",
-      providerName: "Anthropic",
-      providerType: "anthropic",
-    },
-  })),
+  // ...googleModels.map((name) => ({
+  //   name,
+  //   available: true,
+  //   provider: {
+  //     id: "google",
+  //     providerName: "Google",
+  //     providerType: "google",
+  //   },
+  // })),
+  // ...anthropicModels.map((name) => ({
+  //   name,
+  //   available: true,
+  //   provider: {
+  //     id: "anthropic",
+  //     providerName: "Anthropic",
+  //     providerType: "anthropic",
+  //   },
+  // })),
 ] as const;
 
 export const CHAT_PAGE_SIZE = 15;
