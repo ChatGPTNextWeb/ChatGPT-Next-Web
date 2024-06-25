fec3fdbe4527c423af483e120c4b1c0c3c0af95d
anujsingharvato <anuj.singh@bertelsmann.de>
2024-06-25 09:04:12 +0000

    Added Entra ID provider authentication to Next JS

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth'
+import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id"
 import Okta from 'next-auth/providers/okta'
 
 export const authOptions = {
@@ -9,6 +10,12 @@ export const authOptions = {
             clientSecret: process.env.OKTA_OAUTH2_CLIENT_SECRET as string,
             issuer: process.env.OKTA_OAUTH2_ISSUER as string,
         }),
+
+        MicrosoftEntraID({
+      clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
+      clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
+      tenantId: process.env.AUTH_MICROSOFT_ENTRA_ID_TENANT_ID,
+    }),
     ],
     secret: process.env.SECRET as string
 }
