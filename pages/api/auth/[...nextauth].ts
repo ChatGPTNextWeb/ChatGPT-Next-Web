f1bac6f6a7356988a170f755900332035a116028
anujsingharvato <anuj.singh@bertelsmann.de>
2024-06-27 08:32:26 +0000

    Changed next auth version in package manager

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,23 +1,21 @@
-import NextAuth from 'next-auth'
+import NextAuth from "next-auth"
 import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id"
-import Okta from 'next-auth/providers/okta'
+import Okta from "next-auth/providers/okta"
 
-export const authOptions = {
-    // Configure one or more authentication providers
+const options = {
     providers: [
         Okta({
             clientId: process.env.OKTA_OAUTH2_CLIENT_ID as string,
             clientSecret: process.env.OKTA_OAUTH2_CLIENT_SECRET as string,
             issuer: process.env.OKTA_OAUTH2_ISSUER as string,
         }),
-
         MicrosoftEntraID({
-      clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
-      clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
-      tenantId: process.env.AUTH_MICROSOFT_ENTRA_ID_TENANT_ID,
-    }),
+            clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID as string,
+            clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET as string,
+            tenantId: process.env.AUTH_MICROSOFT_ENTRA_ID_TENANT_ID as string,
+        }),
     ],
-    secret: process.env.SECRET as string
+    secret: process.env.SECRET as string,
 }
 
-export default NextAuth(authOptions)
+export default NextAuth(options)
