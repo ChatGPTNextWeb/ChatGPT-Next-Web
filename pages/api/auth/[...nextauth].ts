fa5477d0c3b335fb2bc75d7d4a0e84d8f54a341e
anujsingharvato <anuj.singh@bertelsmann.de>
2024-06-27 09:03:49 +0000

    Added entraid integration and updated package manager

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth'
+import MicrosoftEntraID from "next-auth/providers/microsoft-entra-id"
 import Okta from 'next-auth/providers/okta'
 
 export const authOptions = {
@@ -8,6 +9,11 @@ export const authOptions = {
             clientId: process.env.OKTA_OAUTH2_CLIENT_ID as string,
             clientSecret: process.env.OKTA_OAUTH2_CLIENT_SECRET as string,
             issuer: process.env.OKTA_OAUTH2_ISSUER as string,
+        }),
+        MicrosoftEntraID({
+            clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID as string,
+            clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET as string,
+            tenantId: process.env.AUTH_MICROSOFT_ENTRA_ID_TENANT_ID as string,
         }),
     ],
     secret: process.env.SECRET as string
