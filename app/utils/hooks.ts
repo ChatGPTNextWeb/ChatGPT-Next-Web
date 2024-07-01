7603b21cbcbfbaeef7556fea2d3472da02d5abf9
sijinhui <sijinhui@qq.com>
2024-07-01 17:32:49 +0800

    test

--- app/utils/hooks.ts
+++ app/utils/hooks.ts
@@ -1,10 +1,13 @@
 import { useMemo } from "react";
 import { useAccessStore, useAppConfig } from "../store";
 import { collectModels, collectModelsWithDefaultModel } from "./model";
+import { useSession } from "next-auth/react";
 
 export function useAllModels() {
   const accessStore = useAccessStore();
   const configStore = useAppConfig();
+  const { data: session, status } = useSession();
+  console.log("-sssssssssss", session, status);
   const models = useMemo(() => {
     return collectModelsWithDefaultModel(
       configStore.models,
