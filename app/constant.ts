2cae61652d0a563d08549d1cbe75d1daa4dc4cf5
ayman-makki <manaykimak@gmail.com>
2024-07-02 22:26:46 +0100

    removing models

--- app/constant.ts
+++ app/constant.ts
@@ -137,23 +137,23 @@ export const KnowledgeCutOffDate: Record<string, string> = {
 };
 
 const openaiModels = [
-  "gpt-3.5-turbo",
+  // "gpt-3.5-turbo",
   // "gpt-3.5-turbo-1106",
   // "gpt-3.5-turbo-0125",
   "gpt-4",
   // "gpt-4-0613",
   // "gpt-4-32k",
   // "gpt-4-32k-0613",
-  "gpt-4-turbo",
+  // "gpt-4-turbo",
   // "gpt-4-turbo-preview",
   "gpt-4o",
-  "gpt-4o-2024-05-13",
-  "gpt-4-vision-preview",
-  "gpt-4-turbo-2024-04-09",
+  // "gpt-4o-2024-05-13",
+  // "gpt-4-vision-preview",
+  // "gpt-4-turbo-2024-04-09",
 ];
 
 const googleModels = [
-  "gemini-1.0-pro",
+  // "gemini-1.0-pro",
   "gemini-1.5-pro-latest",
   "gemini-1.5-flash-latest",
   // "gemini-pro-vision",
@@ -161,9 +161,9 @@ const googleModels = [
 
 const anthropicModels = [
   // "claude-instant-1.2",
-  "claude-2.0",
-  "claude-2.1",
-  "claude-3-sonnet-20240229",
+  // "claude-2.0",
+  // "claude-2.1",
+  // "claude-3-sonnet-20240229",
   "claude-3-opus-20240229",
   "claude-3-haiku-20240307",
   "claude-3-5-sonnet-20240620",
