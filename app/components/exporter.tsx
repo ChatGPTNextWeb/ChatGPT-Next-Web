403f3009ea37a234eb076e9b76965bbbb85a75d5
zhangkuo <769329489@qq.com>
2024-07-13 18:58:58 +0800

    logo名称调整

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -539,7 +539,7 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
+            <div className={styles["main-title"]}>BuouChat</div>
             <div className={styles["sub-title"]}>
               github.com/Yidadaa/ChatGPT-Next-Web
             </div>
