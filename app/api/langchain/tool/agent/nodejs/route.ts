cca5a1dbc856084d2100ee6bbfc2a4d04e59f859
Hk-Gosuto <yominogosuto@gmail.com>
2024-07-17 16:35:51 +0800

    fix: vercel 403 country region

--- app/api/langchain/tool/agent/nodejs/route.ts
+++ app/api/langchain/tool/agent/nodejs/route.ts
@@ -99,3 +99,22 @@ export const GET = handle;
 export const POST = handle;
 
 export const runtime = "nodejs";
+export const preferredRegion = [
+  "arn1",
+  "bom1",
+  "cdg1",
+  "cle1",
+  "cpt1",
+  "dub1",
+  "fra1",
+  "gru1",
+  "hnd1",
+  "iad1",
+  "icn1",
+  "kix1",
+  "lhr1",
+  "pdx1",
+  "sfo1",
+  "sin1",
+  "syd1",
+];
