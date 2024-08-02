8498cadae8f394c680be6addf35a489e75d33954
李超 <lichao@sanzhiyang.cn>
2024-08-02 21:05:21 +0800

    fix: Fixed an issue where the sample of the reply content was displayed out of order

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -1470,6 +1470,7 @@ function _Chat() {
                   )}
                   <div className={styles["chat-message-item"]}>
                     <Markdown
+                      key={message.streaming ? "loading" : "done"}
                       content={getMessageTextContent(message)}
                       loading={
                         (message.preview || message.streaming) &&
