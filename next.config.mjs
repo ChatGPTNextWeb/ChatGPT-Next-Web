2f7883dcdad7359f3b0a2cc307e5af7818b68e32
Hk-Gosuto <yominogosuto@gmail.com>
2024-08-28 00:09:33 +0800

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,11 +6,14 @@ console.log("[Next] build mode", mode);
 const disableChunk = !!process.env.DISABLE_CHUNK || mode === "export";
 console.log("[Next] build with chunk: ", !disableChunk);
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   env: {
     LANGCHAIN_CALLBACKS_BACKGROUND: true,
   },
+};
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
