cb6576ec04257ba7a2896cf95749cf9f164ba081
river <laolei@forkway.cn>
2024-09-24 21:19:45 +0800

    feat: simple cargo

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,33 +1,15 @@
-use tauri_specta::Event;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    #[cfg(debug_assertions)]
-    let devtools = tauri_plugin_devtools::init();
-    let mut builder = tauri::Builder::default();
-
-    let specta_builder = tauri_specta::Builder::<tauri::Wry>::new();
-
-    #[cfg(debug_assertions)]
-    {
-        builder = builder.plugin(devtools);
-    }
-
-    #[cfg(all(debug_assertions, not(mobile)))]
-    specta_builder
-        .export(
-            specta_typescript::Typescript::default()
-                .formatter(specta_typescript::formatter::prettier),
-            "../src/bindings.ts",
-        )
-        .expect("failed to export typescript bindings");
-
-    builder
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_dialog::init())
-        .invoke_handler(specta_builder.invoke_handler())
-        .setup(move |app| {
-            specta_builder.mount_events(app);
+    tauri::Builder::default()
+        .plugin(tauri_plugin_http::init())
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
             Ok(())
         })
         .run(tauri::generate_context!())
