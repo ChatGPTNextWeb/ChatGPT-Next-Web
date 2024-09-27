b141f7db6de2d5475968fa1f3f8157b39048e51a
eviswu <eviswu@tutamail.com>
2024-09-27 16:17:11 +0800

    Update auth.tsx

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -45,7 +45,6 @@ export function AuthPage() {
 
   return (
     <div className={styles["auth-page"]}>
-      <TopBanner></TopBanner>
       <div className={styles["auth-header"]}>
         <IconButton
           icon={<LeftIcon />}
@@ -104,12 +103,6 @@ export function AuthPage() {
           text={Locale.Auth.Confirm}
           type="primary"
           onClick={goChat}
-        />
-        <IconButton
-          text={Locale.Auth.SaasTips}
-          onClick={() => {
-            goSaas();
-          }}
         />
       </div>
     </div>
