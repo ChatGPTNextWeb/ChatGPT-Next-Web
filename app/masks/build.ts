10311d7bb962a82186b579fd4619c1fdd61caf4a
Joxp <joxp233@163.com>
2024-10-22 23:03:25 +0800

    更改SaaS

--- app/masks/build.ts
+++ app/masks/build.ts
@@ -1,14 +1,12 @@
 import fs from "fs";
 import path from "path";
 import { CN_MASKS } from "./cn";
-import { TW_MASKS } from "./tw";
 import { EN_MASKS } from "./en";
 
 import { type BuiltinMask } from "./typing";
 
 const BUILTIN_MASKS: Record<string, BuiltinMask[]> = {
   cn: CN_MASKS,
-  tw: TW_MASKS,
   en: EN_MASKS,
 };
 
