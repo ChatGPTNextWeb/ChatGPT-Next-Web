8ea4ee9f4627a8e2f0e64b3b199677cba86401f4
Joxp <joxp233@163.com>
2024-10-22 22:43:13 +0800

    更改SaaS

--- app/constant.ts
+++ app/constant.ts
@@ -501,5 +501,5 @@ export const PLUGINS = [
   { name: "Search Chat", path: Path.SearchChat },
 ];
 
-export const SAAS_CHAT_URL = "https://nextchat.dev/chat";
-export const SAAS_CHAT_UTM_URL = "https://nextchat.dev/chat?utm=github";
+export const SAAS_CHAT_URL = "https://bot.howlike.cn/";
+export const SAAS_CHAT_UTM_URL = "https://bot.howlike.cn/";
