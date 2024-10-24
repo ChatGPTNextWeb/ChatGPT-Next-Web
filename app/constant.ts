cec4ffb5caed6b0adfa826d1ee15e0a8d77a0150
Banu-teja <bhanuteja.pappala@gmail.com>
2024-10-24 18:16:23 +0000

    feat: add new claude model

--- app/constant.ts
+++ app/constant.ts
@@ -319,6 +319,8 @@ const anthropicModels = [
   "claude-3-opus-20240229",
   "claude-3-haiku-20240307",
   "claude-3-5-sonnet-20240620",
+  "claude-3-5-sonnet-20241022",  // aws model
+  "claude-3-5-sonnet-v2@20241022",  // gcp model
 ];
 
 const baiduModels = [
