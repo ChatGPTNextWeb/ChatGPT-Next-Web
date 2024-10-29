736cbdbdd12d340e9b08b69724f9a1321befd645
hyiip <hyiip@link.cuhk.edu.hk>
2024-10-30 02:18:41 +0800

    add constant to claude 3.5 sonnet 20241022

--- app/constant.ts
+++ app/constant.ts
@@ -319,6 +319,7 @@ const anthropicModels = [
   "claude-3-opus-20240229",
   "claude-3-haiku-20240307",
   "claude-3-5-sonnet-20240620",
+  "claude-3-5-sonnet-20241022",
 ];
 
 const baiduModels = [
