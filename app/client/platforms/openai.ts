a60ee08a07255fe606ff8be1d0a66f26ff77acd1
YINGXINGHUA <127013150+YINGXINGHUA@users.noreply.github.com>
2024-11-03 05:20:28 +0800

    Update openai.ts

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -229,7 +229,7 @@ export class ChatGPTApi implements LLMApi {
         presence_penalty: !isO1 ? modelConfig.presence_penalty : 0,
         frequency_penalty: !isO1 ? modelConfig.frequency_penalty : 0,
         top_p: !isO1 ? modelConfig.top_p : 1,
-        // max_tokens: Math.max(modelConfig.max_tokens, 1024),
+        max_tokens: Math.max(modelConfig.max_tokens, 1024),
         // Please do not ask me why not send max_tokens, no reason, this param is just shit, I dont want to explain anymore.
       };
 
