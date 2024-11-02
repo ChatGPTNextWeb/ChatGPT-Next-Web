89e3a6bb2baea98665b4571170982c8180204911
joezhn2020 <61100767+joezhn2020@users.noreply.github.com>
2024-11-02 11:35:50 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "舟joeWeb Chat",
+  description: "你的私人聊天机器人。",
   appleWebApp: {
-    title: "NextChat",
+    title: "舟joeWeb Chat",
     statusBarStyle: "default",
   },
 };
