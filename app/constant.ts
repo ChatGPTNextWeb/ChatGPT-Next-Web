3086a2fa77d2815af05236bae4a13a4da387730b
opchips <opchips@gmail.com>
2024-11-06 12:56:24 +0800

    add claude35haiku not vision

--- app/constant.ts
+++ app/constant.ts
@@ -328,6 +328,7 @@ const anthropicModels = [
   "claude-3-sonnet-20240229",
   "claude-3-opus-20240229",
   "claude-3-haiku-20240307",
+  "claude-3-5-haiku-20241022",
   "claude-3-5-sonnet-20240620",
   "claude-3-5-sonnet-20241022",
   "claude-3-5-sonnet-latest",
