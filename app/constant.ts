64aa760e58f31ab45ce720b988c859dd53c491ca
opchips <opchips@gmail.com>
2024-11-06 19:18:05 +0800

    update claude rank

--- app/constant.ts
+++ app/constant.ts
@@ -330,10 +330,10 @@ const anthropicModels = [
   "claude-3-opus-latest",
   "claude-3-haiku-20240307",
   "claude-3-5-haiku-20241022",
+  "claude-3-5-haiku-latest",
   "claude-3-5-sonnet-20240620",
   "claude-3-5-sonnet-20241022",
   "claude-3-5-sonnet-latest",
-  "claude-3-5-haiku-latest",
 ];
 
 const baiduModels = [
