fb94120702453c624b6248b3c3acf966bb77f54a
Codeliu <lzlalpha@gmail.com>
2024-11-17 08:22:19 +0800

    Update build.ts

--- app/masks/build.ts
+++ app/masks/build.ts
@@ -6,10 +6,14 @@ import { EN_MASKS } from "./en";
 
 import { type BuiltinMask } from "./typing";
 
+// const BUILTIN_MASKS: Record<string, BuiltinMask[]> = {
+//   cn: CN_MASKS,
+//   tw: TW_MASKS,
+//   en: EN_MASKS,
+// };
+
 const BUILTIN_MASKS: Record<string, BuiltinMask[]> = {
   cn: CN_MASKS,
-  tw: TW_MASKS,
-  en: EN_MASKS,
 };
 
 const dirname = path.dirname(__filename);
