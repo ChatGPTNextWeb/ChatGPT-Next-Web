5a23a4d217da2904841e5a783ff8fe00810255e8
Hk-Gosuto <yominogosuto@gmail.com>
2024-11-19 02:32:56 +0000

    fix: search special character error

--- app/components/search-bar.tsx
+++ app/components/search-bar.tsx
@@ -37,15 +37,19 @@ export interface SearchInputRef {
   inputElement: HTMLInputElement | null;
 }
 
+function escapeRegExp(search: string) {
+  return search.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
 function highlightAndShorten(str: string, search: string) {
   const index = str.toLowerCase().indexOf(search.toLowerCase());
   const head = Math.max(0, index - 10);
   const tail = Math.min(str.length, index + search.length + 40);
   // Remove code block syntax
   let result = str.slice(head, tail);
-
+  const safeSearch = escapeRegExp(search);
   // Use ** to highlight the search result
-  result = result.replace(new RegExp(`(${search})`), "**$1**");
+  result = result.replace(new RegExp(`(${safeSearch})`), "**$1**");
 
   if (head > 0) {
     result = "..." + result;
