c3dc54dd9133e9267163899acd7bc36f4e0540e6
afred <afred@macbookpro-afred.local>
2024-12-11 09:44:36 +0800

    remove original url

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -9,13 +9,10 @@ const en: LocaleType = {
   WIP: "Coming Soon...",
   Error: {
     Unauthorized: isApp
-      ? `😆 Oops, there's an issue. No worries:
-     \\ 1️⃣ New here? [Click to start chatting now 🚀](${SAAS_CHAT_UTM_URL})
-     \\ 2️⃣ Want to use your own OpenAI resources? [Click here](/#/settings) to change settings ⚙️`
+      ? `😆 Oops, there's an issue. No worries:`
       : `😆 Oops, there's an issue. Let's fix it:
-     \ 1️⃣ New here? [Click to start chatting now 🚀](${SAAS_CHAT_UTM_URL})
-     \ 2️⃣ Using a private setup? [Click here](/#/auth) to enter your key 🔑
-     \ 3️⃣ Want to use your own OpenAI resources? [Click here](/#/settings) to change settings ⚙️
+     \ 2️⃣  [Click here](/#/auth) to enter your password 🔑
+
      `,
   },
   Auth: {
