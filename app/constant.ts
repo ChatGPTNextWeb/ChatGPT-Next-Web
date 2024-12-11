40310e64bcf5ff956bc5f76788b830a839d31cde
afred <afred@macbookpro-afred.local>
2024-12-11 09:48:48 +0800

    change url

--- app/constant.ts
+++ app/constant.ts
@@ -566,5 +566,5 @@ export const PLUGINS = [
   { name: "Search Chat", path: Path.SearchChat },
 ];
 
-export const SAAS_CHAT_URL = "https://nextchat.dev/chat";
-export const SAAS_CHAT_UTM_URL = "https://nextchat.dev/chat?utm=github";
+export const SAAS_CHAT_URL = "https://kaiyanmedical.com";
+export const SAAS_CHAT_UTM_URL = "https://kaiyanmedical.com/chat?utm=github";
