29f7a9c663563a10cc6d4ab3f17d8e4e93086ebb
afred <afred@macbookpro-afred.local>
2024-12-11 14:02:33 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { Home } from "./components/home";
 import { getServerSideConfig } from "./config/server";
 import Hotjar from '@hotjar/browser';
-const siteId = 3414362;
+const siteId = 4245983;
 const hotjarVersion = 6;
 
 Hotjar.init(siteId, hotjarVersion);
