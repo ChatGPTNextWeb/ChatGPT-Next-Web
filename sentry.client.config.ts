4a3edaf3d7dd7acee29aa480d0caac01083a0bf9
afred <afred@macbookpro-afred.local>
2024-12-11 14:51:22 +0800

    test

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -22,5 +22,5 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: true,
 });
