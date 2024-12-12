58f103eaf69dc5ce2c37de70244414f34c7747ff
04041b <65881086+04041b@users.noreply.github.com>
2024-12-11 21:49:19 -0800

    fix invoke in stream.ts, upgrade package

--- app/global.d.ts
+++ app/global.d.ts
@@ -12,32 +12,34 @@ declare module "*.svg";
 
 declare interface Window {
   __TAURI__?: {
-    writeText(text: string): Promise<void>;
-    invoke(command: string, payload?: Record<string, unknown>): Promise<any>;
-    dialog: {
-      save(options?: Record<string, unknown>): Promise<string | null>;
-    };
-    fs: {
-      writeBinaryFile(path: string, data: Uint8Array): Promise<void>;
-      writeTextFile(path: string, data: string): Promise<void>;
-    };
-    notification: {
-      requestPermission(): Promise<Permission>;
-      isPermissionGranted(): Promise<boolean>;
-      sendNotification(options: string | Options): void;
-    };
-    updater: {
-      checkUpdate(): Promise<UpdateResult>;
-      installUpdate(): Promise<void>;
-      onUpdaterEvent(
-        handler: (status: UpdateStatusResult) => void,
-      ): Promise<UnlistenFn>;
-    };
-    http: {
-      fetch<T>(
-        url: string,
-        options?: Record<string, unknown>,
-      ): Promise<Response<T>>;
+    core: {
+      writeText(text: string): Promise<void>;
+      invoke(command: string, payload?: Record<string, unknown>): Promise<any>;
+      dialog: {
+        save(options?: Record<string, unknown>): Promise<string | null>;
+      };
+      fs: {
+        writeBinaryFile(path: string, data: Uint8Array): Promise<void>;
+        writeTextFile(path: string, data: string): Promise<void>;
+      };
+      notification: {
+        requestPermission(): Promise<Permission>;
+        isPermissionGranted(): Promise<boolean>;
+        sendNotification(options: string | Options): void;
+      };
+      updater: {
+        checkUpdate(): Promise<UpdateResult>;
+        installUpdate(): Promise<void>;
+        onUpdaterEvent(
+          handler: (status: UpdateStatusResult) => void,
+        ): Promise<UnlistenFn>;
+      };
+      http: {
+        fetch<T>(
+          url: string,
+          options?: Record<string, unknown>,
+        ): Promise<Response<T>>;
+      };
     };
   };
 }
