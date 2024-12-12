cfaa0b5501d6f3a36a293380d3c50f7c012d8d9b
04041b <65881086+04041b@users.noreply.github.com>
2024-12-11 19:27:53 -0800

    wip commit

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,11 +9,26 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .invoke_handler(tauri::generate_handler![stream::stream_fetch])
-        .plugin(tauri_plugin_window_state::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+// pub fn run() {
+//     tauri::Builder::default()
+//         .plugin(tauri_plugin_http::init())
+//         .plugin(tauri_plugin_shell::init())
+//         .plugin(tauri_plugin_notification::init())
+//         .plugin(tauri_plugin_dialog::init())
+//         not compatible with mobile
+//         .plugin(tauri_plugin_updater::Builder::new().build())
+//         .plugin(tauri_plugin_fs::init())
+//         .plugin(tauri_plugin_clipboard_manager::init())
+//         .invoke_handler(tauri::generate_handler![stream::stream_fetch])
+//         not compatible with mobile
+//         .plugin(tauri_plugin_window_state::Builder::default().build())
+//         .run(tauri::generate_context!())
+//         .expect("error while running tauri application");
+// }
