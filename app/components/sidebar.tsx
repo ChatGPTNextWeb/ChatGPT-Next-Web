a1fe280b292cacdd1c93e7c88cf8845883c8f783
Qingyun Yixi <1422756921@qq.com>
2024-12-13 10:14:15 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT-青云依兮
+          NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          构建自己的人工智能助手。
+          Build your own AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
