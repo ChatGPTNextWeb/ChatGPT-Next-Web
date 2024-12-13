f176fd8fb7740f5612da1c3eff1fd5c77b9a0327
Qingyun Yixi <1422756921@qq.com>
2024-12-13 10:24:29 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -377,7 +377,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-   BotHello: "有什么可以帮你的吗",
+    BotHello: "有什么可以帮你的吗",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
