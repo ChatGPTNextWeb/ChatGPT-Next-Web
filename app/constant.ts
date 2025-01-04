25cfe053188535e19570133429e9419da845c012
afred <afred@macbookpro-afred.local>
2025-01-04 15:22:46 +0800

    remove openai tts

--- app/constant.ts
+++ app/constant.ts
@@ -292,7 +292,7 @@ export const KnowledgeCutOffDate: Record<string, string> = {
 };
 
 export const DEFAULT_TTS_ENGINE = "Edge-TTS";
-export const DEFAULT_TTS_ENGINES = ["Edge-TTS", "OpenAI-TTS"];
+export const DEFAULT_TTS_ENGINES = "Edge-TTS";
 export const DEFAULT_TTS_MODEL = "tts-1";
 export const DEFAULT_TTS_VOICE = "alloy";
 export const DEFAULT_TTS_MODELS = ["tts-1", "tts-1-hd"];
