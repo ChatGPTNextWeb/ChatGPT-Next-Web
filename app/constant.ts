8f12beb8f0b65d9b3de009348b0a8b2397e5574c
Sky <retiredqq@gmail.com>
2025-02-02 21:43:30 +0000

    support o3-mini

--- app/constant.ts
+++ app/constant.ts
@@ -410,6 +410,7 @@ export const KnowledgeCutOffDate: Record<string, string> = {
   "gpt-4-vision-preview": "2023-04",
   "o1-mini": "2023-10",
   "o1-preview": "2023-10",
+  "o3-mini": "2023-10",
   // After improvements,
   // it's now easier to add "KnowledgeCutOffDate" instead of stupid hardcoding it, as was done previously.
   "gemini-pro": "2023-12",
